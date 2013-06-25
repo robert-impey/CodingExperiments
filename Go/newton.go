e1d7a559a51a241176525469dc729e75364f00b9
Robert Impey <robert@impey.info>
2013-06-25 23:02:56 +0100

    Error handling in sqrt code.
    http://tour.golang.org/#55

--- Go/newton.go
+++ Go/newton.go
@@ -1,4 +1,5 @@
 // See http://tour.golang.org/#23
+// and http://tour.golang.org/#55
 
 package main
 
@@ -7,7 +8,16 @@ import (
 	"math"
 )
 
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprint("Cannot Sqrt negative numbers: ", float64(e))
+}
+
 func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return math.NaN(), ErrNegativeSqrt(x)
+	}
 	z := 1.0
 	for i := 0; i < 10; i++ {
 		z = z - ((z*z - x) / (2 * x))
@@ -17,11 +27,15 @@ func Sqrt(x float64) (float64, error) {
 
 func main() {
 	for i := -2; i <= 10; i++ {
-		mine, _ := Sqrt(float64(i))
-		builtIn := math.Sqrt(float64(i))
+		mine, sqrtError := Sqrt(float64(i))
+		if sqrtError == nil {
+			builtIn := math.Sqrt(float64(i))
 
-		error := math.Abs(mine - builtIn)
+			error := math.Abs(mine - builtIn)
 
-		fmt.Printf("%d,%f,%f,%f\n", i, mine, builtIn, error)
+			fmt.Printf("%d,%f,%f,%f\n", i, mine, builtIn, error)
+		} else {
+			fmt.Println(sqrtError.Error())
+		}
 	}
 }
