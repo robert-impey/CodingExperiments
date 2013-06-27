bc35e8fb684fe48a45284dea3756d3cc1d600514
Robert Impey <robert@impey.info>
2013-06-27 23:06:30 +0100

    Floyd's Triangle in VB.Net.

---
+++ VB.Net/FloydsTriangle/FloydsTriangle/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Rows = 10
+        Dim CurrentNumber = 0
+
+        For Row = 1 To Rows
+            Dim Columns = Row
+
+            For Column = 1 To Columns
+                CurrentNumber += 1
+                Console.Write(CurrentNumber & " ")
+            Next
+            Console.WriteLine()
+        Next
+
+    End Sub
+
+End Module
