711356460bc0f0d0eca6d3c74c59fcd93ff7311d
Robert Impey <robert@impey.info>
2013-07-04 22:00:37 +0100

    Copied in file from elsewhere.

---
+++ Java/BinarySearch/src/binarysearch/BinarySearch.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binarysearch;
+
+/**
+ *
+ * @author Robert
+ */
+public class BinarySearch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final int max = 10;
+        final int[] soughtNumbers = {-1, 0, 5, 9, 10};
+        
+        int[] as = new int[max];
+        int i, j, k;
+        
+        for (i = 0; i < max; i++) {
+            as[i] = i;
+        }
+        
+        for (int soughtNumber : soughtNumbers) {
+            System.out.printf("Seeking: %d\n", soughtNumber);
+            
+            // Simple Search
+            i = 0;
+            while (!((i == max - 1) || (as[i] == soughtNumber))) {
+                i++;
+            }
+
+            if (as[i] == soughtNumber) {
+                System.out.println("Found!");
+            } else {
+                System.out.println("Not found!");
+            }
+
+            // Binary Search
+            i = 0;
+            j = max - 1;
+            do {
+                k = (i + j) / 2;
+                if (soughtNumber > as[k]) {
+                    i = k + 1;
+                } else {
+                    j = k - 1;
+                }
+            } while (!((as[k] == soughtNumber) || (i >= j)));
+
+            if (as[i] == soughtNumber) {
+                System.out.println("Found!");
+            } else {
+                System.out.println("Not found!");
+            }
+        }
+    }
+}
