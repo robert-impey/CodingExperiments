0b358d70659a6e9778d05ef0ef70db8ab4d92611
Robert Impey <robert@impey.info>
2013-07-24 09:09:24 +0100

    Sum of first 1000 numbers.

---
+++ Python/HelloPython/sum1000.py
@@ -0,0 +1,8 @@
+sum_of_numbers = 0
+max_number = 1000
+
+for i in range(1, max_number + 1):
+    print "Adding %d to %d" % (i, sum_of_numbers)
+    sum_of_numbers += i
+
+print "The sum_of_numbers: %d" % sum_of_numbers
