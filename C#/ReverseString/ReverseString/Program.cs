8cc68d891ed125f983102709bc2d7469ffabf933
Robert Impey <robert@impey.info>
2013-08-25 17:44:29 +0100

    String reversing in C# using a stack.

---
+++ C#/ReverseString/ReverseString/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                var stringToReverse = args[0];
+
+                var reversedString = ReverseString(stringToReverse);
+
+                Console.WriteLine(reversedString);
+            }
+            else
+            {
+                Console.WriteLine("Give me a string to work with!");
+            }
+        }
+
+        private static string ReverseString(string stringToReverse)
+        {
+            var tempStack = new Stack<char>();
+            
+            foreach (var c in stringToReverse)
+            {
+                tempStack.Push(c);
+            }
+
+            var reversedString = new StringBuilder();
+
+            foreach (var c in tempStack)
+            {
+                reversedString.Append(c);
+            }
+
+            return reversedString.ToString();
+        }
+    }
+}
