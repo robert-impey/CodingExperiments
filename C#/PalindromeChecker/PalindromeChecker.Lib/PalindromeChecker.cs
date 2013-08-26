5af62fd58539191217712d3ca8d0a55cdd3530ca
Robert Impey <robert@impey.info>
2013-08-26 17:58:12 +0100

    Code to check if strings are palindromes.

---
+++ C#/PalindromeChecker/PalindromeChecker.Lib/PalindromeChecker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeChecker.Lib
+{
+    public class PalindromeChecker
+    {
+        public static bool IsPalindrome(string possiblePalindrome)
+        {
+            if (string.IsNullOrWhiteSpace(possiblePalindrome))
+                return false;
+
+            var tempStack = new Stack<char>();
+
+            var i = 0;
+            for (; i < (int)(possiblePalindrome.Length / 2); i++)
+            {
+                tempStack.Push(possiblePalindrome[i]);
+            }
+
+            if (possiblePalindrome.Length % 2 == 1)
+                i++;
+
+            for (; i < possiblePalindrome.Length; i++)
+            {
+                if (possiblePalindrome[i] != tempStack.Pop())
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ C#/PalindromeChecker/PalindromeChecker.Tests/PalindromeCheckerTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PalindromeChecker.Lib;
+
+namespace PalindromeChecker.Tests
+{
+    [TestClass]
+    public class PalindromeCheckerTest
+    {
+        [TestMethod]
+        public void EmptyString()
+        {
+            Assert.IsFalse(PalindromeChecker.Lib.PalindromeChecker.IsPalindrome(""));
+        }
+
+        [TestMethod]
+        public void SingleLetter()
+        {
+            Assert.IsTrue(PalindromeChecker.Lib.PalindromeChecker.IsPalindrome("a"));
+        }
+
+        [TestMethod]
+        public void TwoLetterPalindrome()
+        {
+            Assert.IsTrue(PalindromeChecker.Lib.PalindromeChecker.IsPalindrome("aa"));
+        }
+
+        [TestMethod]
+        public void TwoDifferentLetters()
+        {
+            Assert.IsFalse(PalindromeChecker.Lib.PalindromeChecker.IsPalindrome("ab"));
+        }
+
+        [TestMethod]
+        public void ThreeLetterPalindrome()
+        {
+            Assert.IsTrue(PalindromeChecker.Lib.PalindromeChecker.IsPalindrome("aba"));
+        }
+
+        [TestMethod]
+        public void FourLetterPalindrome()
+        {
+            Assert.IsTrue(PalindromeChecker.Lib.PalindromeChecker.IsPalindrome("abba"));
+        }
+
+        [TestMethod]
+        public void FourLetterRepeats()
+        {
+            Assert.IsFalse(PalindromeChecker.Lib.PalindromeChecker.IsPalindrome("abab"));
+        }
+    }
+}
