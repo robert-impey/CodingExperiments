2c792e438db6039f95983ef920ff580c501596ca
Robert Impey <robert@impey.info>
2013-08-26 18:23:02 +0100

    Allow reading a dictionary file.

--- C#/PalindromeChecker/PalindromeChecker/Program.cs
+++ C#/PalindromeChecker/PalindromeChecker/Program.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PalindromeChecker.Lib;
+
+/**
+ * For searching a dictionary file for palindromes.
+ * 
+ * e.g. Civic Eve did a gig. Wow, madam! Pop solos deified a toot.
+ */
 
 namespace PalindromeChecker
 {
@@ -10,7 +18,24 @@ namespace PalindromeChecker
     {
         static void Main(string[] args)
         {
+            foreach (var wordFileName in args)
+            {
+                using (var wordFileReader = File.OpenText(wordFileName))
+                {
+                    while (true)
+                    {
+                        var line = wordFileReader.ReadLine();
 
+                        if (line == null)
+                            break;
+
+                        var wordToCheck = line.Trim();
+
+                        if (PalindromeChecker.Lib.PalindromeChecker.IsPalindrome(wordToCheck))
+                            Console.WriteLine(wordToCheck);
+                    }
+                }
+            }
         }
     }
 }
