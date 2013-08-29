ea507f53bcfa2e375b7683d9f6389d923aa852d9
Robert Impey <robert@impey.info>
2013-08-29 10:17:29 +0100

    Generic version of the algorithm.

--- C#/PairCount/PairCount.Lib/PairCounter.cs
+++ C#/PairCount/PairCount.Lib/PairCounter.cs
@@ -4,13 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/**
+ * Finds the count of pairs of elements in a
+ * that have a sum equal to k.
+ */
 namespace PairCount.Lib
 {
     public class PairCounter
     {
         public static int Count(int k, int[] a)
         {
-            var elementCounts = new Dictionary<int, int>();
+            return Count(k, a, (kParam, aParam) => kParam - aParam);
+        }
+
+        /// <summary>
+        /// Generic version of the counting function.
+        /// </summary>
+        /// <typeparam name="K"></typeparam>
+        /// <typeparam name="A"></typeparam>
+        /// <param name="k"></param>
+        /// <param name="a"></param>
+        /// <param name="inverse">The inverse of the function that maps (ai, aj) to K.</param>
+        /// <returns></returns>
+        public static int Count<K, A>(K k, A[] a, Func<K, A, A> inverse)
+        {
+            var elementCounts = new Dictionary<A, int>();
 
             var groups = from element in a
                          group element by element;
@@ -23,7 +41,7 @@ namespace PairCount.Lib
             var pairs = 0;
             foreach (var element in elementCounts.Keys)
             {
-                var difference = k - element;
+                var difference = inverse(k, element);
 
                 if (elementCounts.ContainsKey(difference))
                 {
