7b918444bab73a21e13f67302ac3747cf7ce58db
Robert Impey <robert@impey.info>
2013-08-29 09:06:50 +0100

    Init.

---
+++ C#/PairCount/PairCount.Test/PairCounterTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PairCount.Test
+{
+    [TestClass]
+    public class PairCounterTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
