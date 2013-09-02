7b9ee14e7e83b8605bba9c777b80a10536dd99ec
Robert Impey <robert@impey.info>
2013-09-02 10:40:36 +0100

    Using a partially applied function for printing.

--- F#/Loose/ListVsSeq.fs
+++ F#/Loose/ListVsSeq.fs
@@ -1,12 +1,12 @@
 // Comparing a list to a seq
 // Robert Impey 2013-08-21
 
-let max = pown 10 6
+let max = pown 10 3
 
 let p x = printfn "%d" x 
 
 let firstUpToMaxList = [1..max]
-List.iter p firstUpToMaxList
+List.iter (printfn "%d") firstUpToMaxList
 
 let firstUpToMaxSeq = seq { for i in 1 .. max do yield i }
 Seq.iter p firstUpToMaxSeq
