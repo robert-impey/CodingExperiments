ecc63fd896933429135621f34f1392cb7a010abd
Robert Impey <robert@impey.info>
2013-12-03 21:16:08 +0000

    Tests for the query.

--- F-Sharp/Selection/src/Tests/Selection.Tests/Tests.fs
+++ F-Sharp/Selection/src/Tests/Selection.Tests/Tests.fs
@@ -6,11 +6,37 @@ open FsUnit
 
 [<TestFixture>]
 type ``Given a selection helper and list of numbers`` () =
+    let getItems () =
+        [|10 .. 20|]
+
     [<Test>]
-    member x.``When a number less than the lowest item is sent then the lowest element is returned.`` () =
-//        let items = [|10 .. 20|]
-//        let bar = 5
-//        let found = SelectionHelper.getLowestOrLowestItemGreaterThan bar items
-//
-//        found |> should equal 10
-        1 |> should equal 1
+    member x.``When a number less than the lowest item is sent Then the lowest element should be returned.`` () =
+        let items = getItems ()
+        let bar = 5
+        let found = SelectionHelper.getLowestOrLowestItemGreaterThan bar items
+
+        found |> should equal 10
+
+    [<Test>]
+    member x.``When a number in range is sent Then the lowest item greater than that item should be returned`` () =
+        let items = getItems ()
+        let bar = 11
+        let found = SelectionHelper.getLowestOrLowestItemGreaterThan bar items
+
+        found |> should equal 12
+
+    [<Test>]
+    member x.``When a number equal to the max is sent Then none should be returned`` () =
+        let items = getItems ()
+        let bar = 20
+        let found = SelectionHelper.getLowestOrLowestItemGreaterThan bar items
+
+        found |> should be Null
+
+    [<Test>]
+    member x.``When a number greater than the max is sent Then none should be returned`` () =
+        let items = getItems ()
+        let bar = 30
+        let found = SelectionHelper.getLowestOrLowestItemGreaterThan bar items
+
+        found |> should be Null
