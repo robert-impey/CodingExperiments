293b216ce901d9ec250d0ce200fd2a4424311b50
Robert Impey <robert@impey.info>
2014-02-22 12:09:10 +0000

    Updates to the configuration of the depot project.

--- ruby/depot/config/application.rb
+++ ruby/depot/config/application.rb
@@ -32,6 +32,7 @@ module Depot
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.enforce_available_locales = true
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -44,5 +45,7 @@ module Depot
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.secret_key_base = 'qi9R8eAxIYu5S3TMh7Iv'
   end
 end
