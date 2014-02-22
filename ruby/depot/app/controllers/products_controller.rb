a6e5ce984eaef456fa7729a3d9104e915a38b86f
Robert Impey <robert@impey.info>
2014-02-22 13:25:11 +0000

    Fixed product params issue.
    http://weblog.rubyonrails.org/2012/3/21/strong-parameters/

--- ruby/depot/app/controllers/products_controller.rb
+++ ruby/depot/app/controllers/products_controller.rb
@@ -40,7 +40,7 @@ class ProductsController < ApplicationController
   # POST /products
   # POST /products.json
   def create
-    @product = Product.new(params[:product])
+    @product = Product.new(product_params)
 
     respond_to do |format|
       if @product.save
@@ -59,7 +59,7 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
 
     respond_to do |format|
-      if @product.update_attributes(params[:product])
+      if @product.update_attributes(product_params)
         format.html { redirect_to @product, notice: 'Product was successfully updated.' }
         format.json { head :ok }
       else
@@ -79,5 +79,11 @@ class ProductsController < ApplicationController
       format.html { redirect_to products_url }
       format.json { head :ok }
     end
+  end
+  
+  private
+  
+  def product_params
+    params.require(:product).permit(:title, :description, :image_url, :price)
   end
 end
