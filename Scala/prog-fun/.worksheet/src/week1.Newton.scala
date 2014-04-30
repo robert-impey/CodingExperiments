b1583fd734aad25542642a177ad95a47aaef6b33
Robert Impey <robert@impey.info>
2014-04-30 23:01:23 +0100

    Scala: Relative check for epsilon check in Newton's method.

--- Scala/prog-fun/.worksheet/src/week1.Newton.scala
+++ Scala/prog-fun/.worksheet/src/week1.Newton.scala
@@ -2,9 +2,9 @@ package week1
 
 object Newton {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(39); val res$0 = 
   1 + 2;System.out.println("""res0: Int(3) = """ + $show(res$0));$skip(52); 
-  def abs(x: Double): Double = if (x < 0) -x else x;System.out.println("""abs: (x: Double)Double""");$skip(90); 
+  def abs(x: Double): Double = if (x < 0) -x else x;System.out.println("""abs: (x: Double)Double""");$skip(94); 
   def isGoodEnough(guess: Double, x: Double): Boolean =
-    abs(guess * guess - x) < 1e-3;System.out.println("""isGoodEnough: (guess: Double, x: Double)Boolean""");$skip(80); 
+    abs(guess * guess - x) / x < 1e-3;System.out.println("""isGoodEnough: (guess: Double, x: Double)Boolean""");$skip(80); 
   def improve(guess: Double, x: Double): Double =
     (guess + (x / guess)) / 2;System.out.println("""improve: (guess: Double, x: Double)Double""");$skip(131); 
   def sqrtIter(guess: Double, x: Double): Double =
@@ -14,6 +14,6 @@ object Newton {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._
 
   sqrt(2);System.out.println("""res1: Double = """ + $show(res$1));$skip(10); val res$2 = 
   sqrt(4);System.out.println("""res2: Double = """ + $show(res$2));$skip(13); val res$3 = 
-  sqrt(1e-6);System.out.println("""res3: Double = """ + $show(res$3))}
-  //sqrt(1e60)
+  sqrt(1e-6);System.out.println("""res3: Double = """ + $show(res$3));$skip(13); val res$4 = 
+  sqrt(1e60);System.out.println("""res4: Double = """ + $show(res$4))}
 }
--- Scala/prog-fun/src/week1/Newton.sc
+++ Scala/prog-fun/src/week1/Newton.sc
@@ -5,7 +5,7 @@ object Newton {
   def abs(x: Double): Double = if (x < 0) -x else x
                                                   //> abs: (x: Double)Double
   def isGoodEnough(guess: Double, x: Double): Boolean =
-    abs(guess * guess - x) < 1e-3                 //> isGoodEnough: (guess: Double, x: Double)Boolean
+    abs(guess * guess - x) / x < 1e-3             //> isGoodEnough: (guess: Double, x: Double)Boolean
   def improve(guess: Double, x: Double): Double =
     (guess + (x / guess)) / 2                     //> improve: (guess: Double, x: Double)Double
   def sqrtIter(guess: Double, x: Double): Double =
@@ -14,7 +14,7 @@ object Newton {
   def sqrt(x: Double): Double = sqrtIter(1, x)    //> sqrt: (x: Double)Double
 
   sqrt(2)                                         //> res1: Double = 1.4142156862745097
-  sqrt(4)                                         //> res2: Double = 2.0000000929222947
-  sqrt(1e-6)                                      //> res3: Double = 0.031260655525445276
-  //sqrt(1e60)
+  sqrt(4)                                         //> res2: Double = 2.000609756097561
+  sqrt(1e-6)                                      //> res3: Double = 0.0010000001533016628
+  sqrt(1e60)                                      //> res4: Double = 1.0000788456669446E30
 }
