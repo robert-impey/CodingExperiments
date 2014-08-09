7b8beb23bbd0e330b1a0d53d232439c0445aad3e
Robert Impey <robert@impey.info>
2014-08-09 19:44:43 +0200

    C++: extra code to look at for loops

--- C++/HelloWorld/HelloLoops/HelloLoops.cpp
+++ C++/HelloWorld/HelloLoops/HelloLoops.cpp
@@ -1,11 +1,33 @@
 #include <stdio.h>
 
+int j;
+
+void init()
+{
+	j = 0;
+}
+
+bool check()
+{
+	return j < 10;
+}
+
+void incr()
+{
+	j++;
+}
+
 int main()
 {
 	int i;
 
 	for (i = 0; i < 10; i++) {
-		printf("%d\n", i);
+		printf("i - %d\n", i);
+	}
+
+	for (init(); check(); incr())
+	{
+		printf("j - %d\n", j);
 	}
 
 	return 0;
