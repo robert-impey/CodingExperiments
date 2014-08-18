d03578762e53389f828dfdcf645de8cb74aaed64
Robert Impey <robert@impey.info>
2014-08-18 14:37:18 +0100

    F#: data contract serializer example

---
+++ F-Sharp/AttributeTargets/AttributeTargets/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open System.IO
+open System.Runtime.Serialization
+open System.Text
+open System.Xml
+
+[<DataContract>]
+type Foo = 
+    { [<field:DataMember>]
+      Bar : string }
+
+[<EntryPoint>]
+let main argv = 
+    let foo = { Bar = "Baz" }
+    let serializer = DataContractSerializer(typeof<Foo>)
+    let stringBuilder = new StringBuilder()
+    let writer = new XmlTextWriter(new StringWriter(stringBuilder))
+    serializer.WriteObject(writer, foo)
+    printfn "%s" <| stringBuilder.ToString()
+    0
