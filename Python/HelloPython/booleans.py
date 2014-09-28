47a006b1bb2affa044bbeb5edace0ee4e6a8ebb8
Robert Impey <robert@impey.info>
2014-09-28 15:26:29 +0100

    Python: boolean experiment 2 -> 3

--- Python/HelloPython/booleans.py
+++ Python/HelloPython/booleans.py
@@ -1,22 +1,21 @@
 def i_say_which_function_i_am(order):
-    print "I'm function number ", order
+    print("I'm function number ", order)
 
 def tell_me_true(order):
     i_say_which_function_i_am(order)
-    print "I'm going to return True!"
+    print("I'm going to return True!")
     return True
 
 def tell_me_false(order):
     i_say_which_function_i_am(order)
-    print "I'm going to return False!"
+    print("I'm going to return False!")
     return False
 
 def i_say_what_i_get(boolean_expression):
     if boolean_expression:
-        print "I got a true expression!"
+        print("I got a true expression!")
     else:
-        print "I got a false expression!"
-    print
+        print("I got a false expression!")
 
 i_say_what_i_get(tell_me_false(1) and not tell_me_true(2) or tell_me_false(3))
 i_say_what_i_get(tell_me_false(1) or tell_me_false(2))
