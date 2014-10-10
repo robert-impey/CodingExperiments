666d29be8c410431c3371d142801122bbea2cd02
Robert Impey <robert@impey.info>
2014-10-10 23:21:44 +0100

    F#: closure in recursion example.

--- F-Sharp/Loose/Recursion.fsx
+++ F-Sharp/Loose/Recursion.fsx
@@ -1,11 +1,11 @@
 let doSomething times =
-    let rec doSomething' times timesDone =
+    let rec doSomething' timesDone =
         printfn "%d and %d" times timesDone
         if times = timesDone then
             printfn "Done!\n"
         else
             printfn "Doing it again!\n"
-            doSomething' times (timesDone + 1)
-    doSomething' times 0
+            doSomething' (timesDone + 1)
+    doSomething' 0
 
 doSomething 5
