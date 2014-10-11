41a1c04593e26400706ff59e5b97f268aec52f24
Robert Impey <robert@impey.info>
2014-10-11 10:31:06 +0100

    F#: example of number checker in Fizz Buzz.

--- F-Sharp/Loose/FizzBuzz.fsx
+++ F-Sharp/Loose/FizzBuzz.fsx
@@ -1,5 +1,17 @@
 type NumberChecker = int -> string option
 
+let isEven x = 
+    if x % 2 = 0 then Some "That number's even!"
+    else None
+
+let numberCheckerPrinter (numberChecker : NumberChecker) number =
+    match numberChecker number with
+    | Some message -> printfn "%s" message
+    | None -> printfn "%d" number
+
+numberCheckerPrinter isEven 2
+numberCheckerPrinter isEven 3
+
 let numberCheckerMaker (divisor, message) number = 
     if number % divisor = 0 then
         Some message
