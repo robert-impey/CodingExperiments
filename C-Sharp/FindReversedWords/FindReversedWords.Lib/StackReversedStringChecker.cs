39debbc9681a3a92e7fde97d35a5d2a1f4c77dbf
Robert Impey <robert@impey.info>
2014-11-23 13:56:28 +0000

    C#: finding reversed strings
    Checker for 2 strings- is one the reverse of the other?

---
+++ C-Sharp/FindReversedWords/FindReversedWords.Lib/StackReversedStringChecker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindReversedWords.Lib
+{
+    public class StackReversedStringChecker : IReversedStringChecker
+    {
+        public bool AreReversedPair(string firstString, string secondString)
+        {
+            if (firstString.Length != secondString.Length)
+            {
+                return false;
+            }
+
+            var firstStringStack = new Stack<char>();
+
+            foreach (var c in firstString)
+            {
+                firstStringStack.Push(c);
+            }
+
+            foreach (var currentCharFromSecond in secondString)
+            {
+                var currentCharFromFirst = firstStringStack.Pop();
+
+                if (currentCharFromFirst != currentCharFromSecond)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ C-Sharp/FindReversedWords/FindReversedWords.Tests/StackReversedStringCheckerTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FindReversedWords.Lib;
+
+namespace FindReversedWords.Tests
+{
+    [TestClass]
+    public class StackReversedStringCheckerTests
+    {
+        private IReversedStringChecker reversedStringChecker;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            reversedStringChecker = new StackReversedStringChecker();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            reversedStringChecker = null;
+        }
+
+        [TestMethod]
+        public void EmptyStrings()
+        {
+            Assert.IsTrue(reversedStringChecker.AreReversedPair(string.Empty, string.Empty));
+        }
+
+        [TestMethod]
+        public void SameSingleCharStrings()
+        {
+            Assert.IsTrue(reversedStringChecker.AreReversedPair("a", "a"));
+        }
+
+        [TestMethod]
+        public void DifferentSingleCharStrings()
+        {
+            Assert.IsFalse(reversedStringChecker.AreReversedPair("a", "b"));
+        }
+
+        [TestMethod]
+        public void SameMultiCharStrings()
+        {
+            Assert.IsFalse(reversedStringChecker.AreReversedPair("ab", "ab"));
+        }
+
+        [TestMethod]
+        public void ReversedMultiCharsStrings()
+        {
+            Assert.IsTrue(reversedStringChecker.AreReversedPair("ab", "ba"));
+        }
+
+        [TestMethod]
+        public void ReversedMultiCharsStringsWithExtraChar()
+        {
+            Assert.IsFalse(reversedStringChecker.AreReversedPair("ab", "baa"));
+        }
+    }
+}
