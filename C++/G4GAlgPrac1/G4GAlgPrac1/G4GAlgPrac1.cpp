265f6f94ebf985cbcd411f153b63e1a503a1c410
Robert Impey <robert@impey.info>
2014-12-08 22:13:33 +0000

    C++: power of 2 - bit shifting

--- C++/G4GAlgPrac1/G4GAlgPrac1/G4GAlgPrac1.cpp
+++ C++/G4GAlgPrac1/G4GAlgPrac1/G4GAlgPrac1.cpp
@@ -33,18 +33,24 @@ unsigned fun2(unsigned n)
 	return n % 2 == 0 ? tmp * tmp : 2 * tmp * tmp;
 }
 
+unsigned fun3(unsigned n)
+{
+	return 1 << n;
+}
+
 int main(array<System::String ^> ^args)
 {
-	Console::WriteLine("i,fun0(i),fun1(i),fun2(i)");
+	Console::WriteLine("i,fun0(i),fun1(i),fun2(i),fun3(i)");
 
 	for (int i = 0; i <= 10; i++)
 	{
 		printf(
-			"%d,%d,%d,%d\n", 
+			"%d,%d,%d,%d,%d\n", 
 			i, 
 			fun0(i),
 			fun1(i),
-			fun2(i)
+			fun2(i),
+			fun3(i)
 		);
 	}
 
