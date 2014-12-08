5645c3d1bb1bbe80dc1c655c94a53f852dd0a374
Robert Impey <robert@impey.info>
2014-12-08 22:04:49 +0000

    C++:  power of 2 - alt implementation

--- C++/G4GAlgPrac1/G4GAlgPrac1/G4GAlgPrac1.cpp
+++ C++/G4GAlgPrac1/G4GAlgPrac1/G4GAlgPrac1.cpp
@@ -16,11 +16,26 @@ unsigned fun0 (unsigned n)
 	return fun0(n - 1) + fun0(n - 1);
 }
 
+unsigned fun1(unsigned n)
+{	
+	if (n == 0) return 1;
+	if (n == 1) return 2;
+
+	return 2 * fun1(n - 1);
+}
+
 int main(array<System::String ^> ^args)
 {
+	Console::WriteLine("i,fun0(i),fun1(i)");
+
 	for (int i = 0; i <= 10; i++)
 	{
-		printf("fun0(%d) - %d\n", i, fun0(i));
+		printf(
+			"%d,%d,%d\n", 
+			i, 
+			fun0(i),
+			fun1(i)
+		);
 	}
 
 	return 0;
