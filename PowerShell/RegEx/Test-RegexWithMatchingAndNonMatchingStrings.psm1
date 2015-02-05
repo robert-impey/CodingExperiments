b51e74389b683167a7dcfdd3c69f9941f936a9fd
Robert Impey <robert@impey.info>
2015-02-05 16:58:33 +0000

    PowerShell: variable length lookbehind in regexen

---
+++ PowerShell/RegEx/Test-RegexWithMatchingAndNonMatchingStrings.psm1
@@ -0,0 +1,39 @@
+﻿Function Test-Regex
+{
+    param($target, $regex, $strings)
+
+    foreach ($string in $strings)
+    {
+        $actual = $string -match $regex
+
+        if ($actual -ne $target)
+        {
+            Write-Host -NoNewline 'not '
+        }
+
+        Write-Host -NoNewline 'ok'
+
+        Write-Host -NoNewline " # $($string) "
+
+        if ($actual) 
+        {
+            Write-Host 'matches'
+        }
+        else
+        {
+            Write-Host 'does not match'
+        }
+    }
+}
+
+Function Test-RegexWithMatchingAndNonMatchingStrings
+{
+    param($regex, $matching, $nonMatching)
+
+    Write-Host "# Regex: $($regex)"
+
+    Test-Regex $true $regex $matching
+    Test-Regex $false $regex $nonMatching
+}
+
+Export-ModuleMember Test-RegexWithMatchingAndNonMatchingStrings
---
+++ PowerShell/RegEx/duplicates-in-json-array.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module .\Test-RegexWithMatchingAndNonMatchingStrings.psm1
+
+$matching = @('["WA"]', '["WA", "ME"]', '["ME", "WA"]');
+
+$nonMatching = @('[]', '["WA", "WA"]', '["WA", "ME", "WA"]', '["ME", "ME"]');
+
+$regex = '(?<!WA.*)WA(?!.*WA)';
+
+Test-RegexWithMatchingAndNonMatchingStrings $regex $matching $nonMatching
+
+Remove-Module Test-RegexWithMatchingAndNonMatchingStrings
