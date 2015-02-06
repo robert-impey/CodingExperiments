201ed61921819ba903d964d1ee3970a3153a474c
Robert Impey <robert@impey.info>
2015-02-06 08:31:14 +0000

    Perl: Regex test using variable length lookbehind.

--- Perl/regex/duplicates-in-json-list.pl
+++ Perl/regex/duplicates-in-json-list.pl
@@ -7,6 +7,6 @@ my @matching = ('["WA"]', '["WA", "ME"]', '["ME", "WA"]');
 
 my @non_matching = ('[]', '["WA", "WA"]', '["WA", "ME", "WA"]', '["ME", "ME"]');
 
-my $regex = 'WA(?!.*WA)';
+my $regex = '(?<!WA.*)WA(?!.*WA)';
 
 test_matching_and_non_matching($regex, \@matching, \@non_matching);
