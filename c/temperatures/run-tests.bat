e9055785443d7c5e321fe67cc7dce5efc956298f
Robert Impey <robert@impey.info>
2015-07-23 15:29:42 +0900

    C: removed unused test running scripts from temp exp.

--- c/temperatures/run-tests.bat
+++
@@ -1,2 +0,0 @@
-make
-perl ..\..\Perl\run-cli-tests\run-cli-tests.pl -d . -t ..\..\Perl\run-cli-tests\test-data
--- c/temperatures/run-tests.sh
+++
@@ -1,3 +0,0 @@
-make
-perl ../../Perl/run-cli-tests/run-cli-tests.pl -d . -t ../../Perl/run-cli-tests/test-data
-
