3d248cbed5578557ba06c1cff291619955b48460
Robert Impey <robert@impey.info>
2015-08-08 08:49:14 +0900

    Py: swimming lengths calculator

---
+++ Python/swimming.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+
+sets = int(sys.argv[1])
+total = 0
+
+for current_set in range(1, sets + 1):
+    print("Current set:", current_set)
+    
+    if (current_set <= sets / 2):
+        exponent = current_set - 1
+    else:
+        exponent = sets - current_set
+
+    lengths = pow(2, exponent)
+    print("Lengths in set:", lengths)
+
+    total += lengths
+    
+print("Total lengths:", total)
+
