6f64730065e1a2d271dbd8f50045fe9b5b8df783
Robert Impey <robert@impey.info>
2015-08-14 20:44:34 +0100

    R: q 5 from week 2 quiz

--- r/JH Coursera/rprog-031/week2.R
+++ r/JH Coursera/rprog-031/week2.R
@@ -1,3 +1,5 @@
+# Question 3
+
 f <- function(x) {
   g <- function(y) {
     y + z
@@ -10,10 +12,28 @@ z <- 10
 
 f(3)
 
+# Question 4
+
 x <- 5
 
 y <- if (x < 5) {
   NA
 } else {
   10
+}
+
+# Question 5
+
+h <- function(x, y = NULL, d = 3L) {
+  z <- cbind(x, d)
+  if (!is.null(y)) {
+    z <- z + y
+  } else {
+    z <- z + f
+  }
+  g <- x + y / z
+  if (d == 3L)
+    return(g)
+  g <- g + 10
+  g
 }
