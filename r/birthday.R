7f5be3c1d5761389145e03cca2576796dc49d574
Robert Impey <robert@impey.info>
2015-08-30 19:07:37 +0100

    R: Birthday clashes

---
+++ r/birthday.R
@@ -0,0 +1,36 @@
+# Experiment to look at the likelihood of a birthday clash at a party
+# Robert Impey 2015-08-30
+
+count_birthdays_with_clash <- function(party_size, number_of_parties = 10, days = c(1:365))
+{
+  number_of_clashes <- 0
+  for (i in 1:number_of_parties) 
+  {
+    guests_birthdays <- sample(days, party_size, replace=TRUE)    
+    if (party_has_clash(guests_birthdays))
+    {
+      number_of_clashes <- number_of_clashes + 1
+    }
+  }
+  data.frame(number_of_parties = number_of_parties, number_of_clashes = number_of_clashes)
+}
+
+party_has_clash <- function(birthdays)
+{
+  length(birthdays) > length(unique(birthdays)) 
+}
+
+number_of_parties <- 3000
+
+max_party_size <- 50
+clash_ratios <- c()
+for (i in 2:max_party_size)
+{
+  clash_counts <- count_birthdays_with_clash(i, number_of_parties = number_of_parties)
+  
+  clash_ratio <- clash_counts$number_of_clashes / clash_counts$number_of_parties
+  
+  clash_ratios <- append(clash_ratios, clash_ratio)
+}
+
+plot(clash_ratios)
