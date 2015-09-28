d60b305acf8161fe15dad970a31f1c49419926fa
Robert Impey <robert@impey.info>
2015-09-28 08:58:33 +0100

    C#: Removed unused usings from Tube station finder.

--- C-Sharp/NearestTube/NearestTube.Lib/TubeStation.cs
+++ C-Sharp/NearestTube/NearestTube.Lib/TubeStation.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace NearestTube.Lib
+﻿namespace NearestTube.Lib
 {
     public class TubeStation
     {
