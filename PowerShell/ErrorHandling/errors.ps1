6f1334e368eb812c7410883a3dbb26692af0980c
Robert Impey <robert@impey.info>
2015-10-27 14:31:09 +0000

    PS: Looking at error handling

---
+++ PowerShell/ErrorHandling/errors.ps1
@@ -0,0 +1,13 @@
+﻿# See http://powershell.com/cs/blogs/tobias/archive/2008/09/29/trapping-errors.aspx
+
+1 / $null # Errors before the trap are still caught
+
+trap { 
+    'Something terrible happened!' 
+    continue # Surpresses the original error message
+    #exit # Stop at the first error
+}
+
+1 / $null
+
+1 / 2 # Legal code is still run if we continue
