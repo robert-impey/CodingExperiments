5626a3e56381110789ee98210a6941a7961cb366
Robert Impey <robert@impey.info>
2015-12-10 21:45:44 +0000

    PS: Refactored program logic for substring checker to unit.

---
+++ pascal/SubstringChecker/substringchecking.pas
@@ -0,0 +1,45 @@
+unit SubstringChecking;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+function Search(Haystack, Needle: string): boolean;
+
+implementation
+
+function Search(Haystack, Needle: string): boolean;
+var
+  HaystackIndex, NeedleIndex: integer;
+  Found: boolean;
+begin
+  HaystackIndex := 0;
+  Found := False;
+
+  if (Length(Haystack) >= Length(Needle)) and (Length(Needle) > 0) then
+  begin
+    while (HaystackIndex <= Length(Haystack)) and (not Found) do
+    begin
+      Inc(HaystackIndex);
+      NeedleIndex := 1;
+      while (Haystack[HaystackIndex + NeedleIndex - 1] = Needle[NeedleIndex]) and
+        (not Found) do
+      begin
+        if NeedleIndex = Length(Needle) then
+        begin
+          Found := True;
+        end
+        else
+        begin
+          Inc(NeedleIndex);
+        end;
+      end;
+    end;
+  end;
+  Result := Found;
+end;
+
+end.
