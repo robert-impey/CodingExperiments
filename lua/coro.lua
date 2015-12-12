6ba30fbcad6c5d8f90e84bd507d373377827ae59
Robert Impey <robert@impey.info>
2015-12-12 10:18:46 +0000

    LU: Tidied shebangs

--- lua/coro.lua
+++ lua/coro.lua
@@ -1,18 +1,20 @@
-function foo (a)
-   print("foo", a)
-   return coroutine.yield(2*a)
- end
+#!/usr/bin/env lua
 
- co = coroutine.create(function (a,b)
+function foo (a)
+  print("foo", a)
+  return coroutine.yield(2*a)
+end
+
+co = coroutine.create(function (a,b)
 	   print("co-body", a, b)
 	   local r = foo(a+1)
 	   print("co-body", r)
 	   local r, s = coroutine.yield(a+b, a-b)
 	   print("co-body", r, s)
 	   return b, "end"
- end)
+end)
 
- print("main", coroutine.resume(co, 1, 10))
- print("main", coroutine.resume(co, "r"))
- print("main", coroutine.resume(co, "x", "y"))
- print("main", coroutine.resume(co, "x", "y"))
+print("main", coroutine.resume(co, 1, 10))
+print("main", coroutine.resume(co, "r"))
+print("main", coroutine.resume(co, "x", "y"))
+print("main", coroutine.resume(co, "x", "y"))
