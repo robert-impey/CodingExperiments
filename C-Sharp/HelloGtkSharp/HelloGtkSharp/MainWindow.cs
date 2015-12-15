4f3b81888307a3b100b5dd42f2913a0e7d2ba7b8
Robert Impey <robert@impey.info>
2015-12-15 20:18:43 +0000

    CS: Intro to GTK programming.

---
+++ C-Sharp/HelloGtkSharp/HelloGtkSharp/MainWindow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+
+		goFetchButton.Clicked += new EventHandler (DisplayWebsiteLength);
+
+		outputTextView.Buffer.Text = "Hello, GTK#!";
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected async void DisplayWebsiteLength (object sender, EventArgs e)
+	{
+		outputTextView.Buffer.Text = "Fetching...";
+		using (var client = new HttpClient ()) {
+			outputTextView.Buffer.Text = await client.GetStringAsync (inputTextView.Buffer.Text);
+		}
+	}
+}
