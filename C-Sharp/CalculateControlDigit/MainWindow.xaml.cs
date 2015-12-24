1a577307e3c81e6656f6e9712b53eeac985e9ce0
Robert Impey <robert@impey.info>
2015-12-24 14:18:13 +0000

    CS: Algorithm for calculating control digit from PluralSight.

---
+++ C-Sharp/CalculateControlDigit/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculateControlDigit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            var controlDigitTuple = ControlDigitCalculator.ParseAndCalculate(inputTextBox.Text);
+
+            if (controlDigitTuple.Item1.HasValue)
+            {
+                errorLabel.Content = "";
+                controlDigitTextBox.Text = controlDigitTuple.Item1.Value.ToString();
+            }
+            else
+            {
+                errorLabel.Content = controlDigitTuple.Item2;
+            }
+        }
+    }
+}
