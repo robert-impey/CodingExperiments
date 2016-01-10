394060c4a2ef8913939209b7ac551b9fd1a4b9fb
Robert Impey <robert@impey.info>
2016-01-10 17:58:19 +0000

    PR: Create file name for sorted files.

--- Perl/sorting/test-sorted-lists.pl
+++ Perl/sorting/test-sorted-lists.pl
@@ -6,25 +6,27 @@ use Getopt::Long;
 my ($sorted_lists_file);
 
 GetOptions(
-	's|sorted-lists-file=s' => \$sorted_lists_file
+    's|sorted-lists-file=s' => \$sorted_lists_file
 );
 
 open SORT, '<', $sorted_lists_file or die $!;
 my $in_order = 1;
 sort_file: while (<SORT>) {
-	chomp;
-	my @hopefully_sorted_list_of_integers = split /,/;
-	
-	foreach (1..(scalar(@hopefully_sorted_list_of_integers) - 2)) {
-		if ($hopefully_sorted_list_of_integers[$_] > $hopefully_sorted_list_of_integers[$_ + 1]) {
-			$in_order = 0;
-			last sort_file;
-		}
-	}
+    chomp;
+    unless (/^#/) {
+        my @hopefully_sorted_list_of_integers = split /,/;
+
+        foreach (1..(scalar(@hopefully_sorted_list_of_integers) - 2)) {
+            if ($hopefully_sorted_list_of_integers[$_] > $hopefully_sorted_list_of_integers[$_ + 1]) {
+                $in_order = 0;
+                last sort_file;
+            }
+        }
+    }
 }
 
 if ($in_order) {
-	print "In order.\n";
+    print "In order.\n";
 } else {
-	print "Not in order.\n";
+    print "Not in order.\n";
 }
