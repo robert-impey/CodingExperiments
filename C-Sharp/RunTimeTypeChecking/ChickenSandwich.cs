12f18b533d6176f65ae979653b394c5f1f71617e
Robert Impey <robert@impey.info>
2016-01-19 18:25:29 +0000

    C#:  Performance of different ways to check types

---
+++ C-Sharp/RunTimeTypeChecking/ChickenSandwich.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunTimeTypeChecking
+{
+    class ChickenSandwich : Food
+    {
+        internal ChickenSandwich(string flavour) : base (flavour)
+        {
+
+        }
+
+        internal override string DescribeFlavour()
+        {
+            return string.Format("A chicken sandwich's flavour is {0}.", Flavour);
+        }
+    }
+}
