b259c0c2a467603bfedbac8a6f78380e4fbd2699
Robert Impey <a-roimpe@microsoft.com>
2016-01-25 18:06:18 +0000

    C#: Comparing list access to array access

---
+++ C-Sharp/ArrayAndListAccess/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using static System.Console;
+
+namespace ArrayAndListAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                int sizeOfSequence;
+                if (int.TryParse(arg, out sizeOfSequence))
+                {
+                    WriteLine($"Experimenting with a sequence of {sizeOfSequence} numbers.");
+
+                    TestArray(sizeOfSequence);
+                    TestList(sizeOfSequence);
+                }
+            }
+        }
+
+        private static void TestList(int sizeOfSequence)
+        {
+            WriteLine("Testing the list");
+
+            var list = new List<int>();
+
+            for (var i = 0; i < sizeOfSequence; i++)
+            {
+                list.Add(i);
+            }
+
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            var rand = new Random();
+
+            for (var i = 0; i < sizeOfSequence; i++)
+            {
+                var index = rand.Next(sizeOfSequence);
+
+                var ele = list[index];
+            }
+
+            stopwatch.Stop();
+
+            WriteLine($"Accessed in {stopwatch.ElapsedMilliseconds} ms.");
+        }
+
+        private static void TestArray(int sizeOfSequence)
+        {
+            WriteLine("Testing the array");
+
+            var ary = new int[sizeOfSequence];
+
+            for (var i = 0; i < sizeOfSequence; i++)
+            {
+                ary[i] = i;
+            }
+
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            var rand = new Random();
+
+            for (var i = 0; i < sizeOfSequence; i++)
+            {
+                var index = rand.Next(sizeOfSequence);
+
+                var ele = ary[index];
+            }
+
+            stopwatch.Stop();
+
+            WriteLine($"Accessed in {stopwatch.ElapsedMilliseconds} ms.");
+        }
+    }
+}
