f167624bd4c4c11c5a1c4b4f7bf5c3ec70f5e671
Robert Impey <robert@impey.info>
2016-03-20 12:23:53 +0000

    JS: Simple node.js web app.

---
+++ JavaScript/TheBoard/server.js
@@ -0,0 +1,9 @@
+﻿var http = require("http");
+
+var server = http.createServer(function (req, res) {
+	console.log(req.url);
+	res.write("<html><body>" + req.url + "</body></html>");
+	res.end();
+});
+
+server.listen(3000);
