96aceca8d5c6961434e9cd66b0d59b14a36800d3
Robert Impey <robert@impey.info>
2016-04-04 08:57:38 +0100

    R: Labels for plog in birthday clashes

--- r/birthday.R
+++ r/birthday.R
@@ -20,17 +20,23 @@ party_has_clash <- function(birthdays)
   length(birthdays) > length(unique(birthdays)) 
 }
 
-number_of_parties <- 3000
+number_of_parties <- 4000
 
 max_party_size <- 50
 clash_ratios <- c()
-for (i in 2:max_party_size)
+for (party_size in 2:max_party_size)
 {
-  clash_counts <- count_birthdays_with_clash(i, number_of_parties = number_of_parties)
+  clash_counts <- count_birthdays_with_clash(
+    party_size, 
+    number_of_parties = number_of_parties)
   
   clash_ratio <- clash_counts$number_of_clashes / clash_counts$number_of_parties
   
   clash_ratios <- append(clash_ratios, clash_ratio)
 }
 
-plot(clash_ratios)
+plot(
+  clash_ratios, 
+  main = "Party Size vs Clash Ratio",
+  xlab = "Party Size", 
+  ylab = "Clash Ratio")
