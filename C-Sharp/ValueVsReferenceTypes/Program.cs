9de148e896134bcf9b78f17efc3de165cfef56a7
Robert Impey <robert@impey.info>
2016-04-12 21:58:03 +0100

    CS: The difference between structs and classes.

---
+++ C-Sharp/ValueVsReferenceTypes/Program.cs
@@ -0,0 +1,55 @@
+﻿using static System.Console;
+
+namespace ValueVsReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Initialize the class and struct");
+            var myClass = new MyClass { MyInt = 5 };
+            WriteLine($"myClass.MyInt: {myClass.MyInt}");
+
+            var myStruct = new MyStruct { MyInt = 7 };
+            WriteLine($"myStruct.MyInt: {myStruct.MyInt}");
+
+            WriteLine("Assign myClass a 2nd time");
+            var my2ndClass = myClass;
+
+            WriteLine("Change the value in myClass");
+            myClass.MyInt = 10;
+            WriteLine($"myClass.MyInt: {myClass.MyInt}");
+            WriteLine($"my2ndClass.MyInt: {my2ndClass.MyInt}");
+
+            WriteLine("Assign myStruct a 2nd time");
+            var my2ndStruct = myStruct;
+
+            WriteLine("Change the value in myStruct");
+            myStruct.MyInt = 14;
+            WriteLine($"myStruct.MyInt: {myStruct.MyInt}");
+            WriteLine($"my2ndStruct.MyInt: {my2ndStruct.MyInt}");
+
+            MyMutatingProcedure(myClass);
+            WriteLine($"myClass.MyInt: {myClass.MyInt}");
+            WriteLine($"my2ndClass.MyInt: {my2ndClass.MyInt}");
+
+            MyMutatingProcedure(myStruct);
+            WriteLine($"myStruct.MyInt: {myStruct.MyInt}");
+            WriteLine($"my2ndStruct.MyInt: {my2ndStruct.MyInt}");
+        }
+
+        static void MyMutatingProcedure(MyClass myClass)
+        {
+            WriteLine("Inside MyMutatingProcedure(MyClass myClass)");
+            myClass.MyInt = 15;
+            WriteLine($"myClass.MyInt: {myClass.MyInt}");
+        }
+
+        static void MyMutatingProcedure(MyStruct myStruct)
+        {
+            WriteLine("Inside MyMutatingProcedure(MyStruct myStruct)");
+            myStruct.MyInt = 21;
+            WriteLine($"myStruct.MyInt: {myStruct.MyInt}");
+        }
+    }
+}
