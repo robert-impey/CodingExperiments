74d6d06b7068d2979a12b2000763c3088b8c3119
Robert Impey <robert@impey.info>
2016-04-16 12:42:22 +0100

    CS: Book finding imperative and declarative.

---
+++ C-Sharp/BookFinding/BookFinding/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace BookFinding
+{
+    class Book
+    {
+        internal Book(string name, int numberOfPages, Colour colour)
+        {
+            Name = name;
+            NumberOfPages = numberOfPages;
+            Colour = colour;
+        }
+
+        internal string Name { get; }
+        internal int NumberOfPages { get; }
+        internal Colour Colour { get; }
+    }
+}
