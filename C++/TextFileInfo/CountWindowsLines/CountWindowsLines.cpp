4a63ef91ea9474bb8319f8491ad44917b9c2087a
Robert Impey <robert@impey.info>
2016-04-24 17:44:26 +0100

    C++: Trying to count lines in Windows and on a old Mac.

--- C++/TextFileInfo/CountWindowsLines/CountWindowsLines.cpp
+++ C++/TextFileInfo/CountWindowsLines/CountWindowsLines.cpp
@@ -4,13 +4,34 @@ int main()
 {
 	int c, nl;
 
+	bool in_line_ending, error_found;
+	in_line_ending = error_found = false;
+
 	nl = 0;
 
 	while ((c = getchar()) != EOF) {
-		if (c == '\n') {
-			nl++;
+		if (in_line_ending) {
+			in_line_ending = false;
+
+			if (c == '\n') {
+				nl++;
+			}
+			else {
+				error_found = true;
+				break;
+			}
+		}
+		else {
+			if (c == '\r') {
+				in_line_ending = true;
+			}
 		}
 	}
 
-	printf("%d\n", nl);
+	if (error_found) {
+		printf("This file has non-Windows line endings!");
+	}
+	else {
+		printf("Windows line endings found: %d\n", nl);
+	}
 }
