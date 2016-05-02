bd0db178984e8529062de999a99a5799bc0bce1b
Robert Impey <robert@impey.info>
2016-05-02 10:30:02 +0100

    C++: Moved vowel counting to separate namespace.

--- C++/TryGet/Main.cpp
+++ C++/TryGet/Main.cpp
@@ -1,62 +1,12 @@
 #include <iostream>
 #include <string>
+#include "VowelCounting.h"
 
 using namespace std;
 
-bool isVowel(char c)
-{
-	return c == 'a'
-		|| c == 'e'
-		|| c == 'i'
-		|| c == 'o'
-		|| c == 'u';
-}
-
-bool isDigit(char c)
-{
-	return c == '0'
-		|| c == '1'
-		|| c == '2'
-		|| c == '3'
-		|| c == '4'
-		|| c == '5'
-		|| c == '6'
-		|| c == '7'
-		|| c == '8'
-		|| c == '9';
-}
-
-int countVowels(string word)
-{
-	int count = 0;
-
-	string::iterator it;
-	for (it = word.begin(); it < word.end(); it++)
-	{
-		if (isVowel(*it))
-		{
-			count++;
-		}
-
-		if (isdigit(*it))
-		{
-			return -1;
-		}
-	}
-
-	return count;
-}
-
-bool tryCountVowels(string word, int &count)
-{
-	count = countVowels(word);
-
-	return count >= 0;
-}
-
 void printCount(string word)
 {
-	int count = countVowels(word);
+	int count = VowelCounting::countVowels(word);
 
 	cout << "I found " << count << " vowels in '" << word << "'." << endl;
 }
@@ -64,7 +14,7 @@ void printCount(string word)
 void printValidatedCount(string word)
 {
 	int attemptedCount;
-	if (tryCountVowels(word, attemptedCount))
+	if (VowelCounting::tryCountVowels(word, attemptedCount))
 	{
 		cout << "I found " << attemptedCount << " vowels in '" << word << "'." << endl;
 	}
