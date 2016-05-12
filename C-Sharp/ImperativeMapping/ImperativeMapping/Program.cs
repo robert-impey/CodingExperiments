456bc118dff2307ce31d3f6c014f8fa3f1c17468
Robert Impey <robert@impey.info>
2016-05-12 23:29:19 +0100

    F# and C#: Comparing mapping strategies.

---
+++ C-Sharp/ImperativeMapping/ImperativeMapping/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using static System.Console;
+
+namespace ImperativeMapping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int Size = 10 * 1000 * 1000;
+
+            var ys = new int[Size];
+
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            for (var i = 0; i < Size; i++)
+            {
+                ys[i] = i + i;
+            }
+            stopWatch.Stop();
+            WriteLine($"Time to run one loop with many iterations: {stopWatch.ElapsedMilliseconds}");
+
+            var zs = new int[10];
+
+            stopWatch.Restart();
+            for (var i = 0; i < Size; i++)
+            {
+                for (var j = 0; j < zs.Length; j++)
+                {
+                    zs[j] = j + j;
+                }
+            }
+            stopWatch.Stop();
+            WriteLine($"Time to run many loops with few iterations: {stopWatch.ElapsedMilliseconds}");
+        }
+    }
+}
---
+++ F-Sharp/FunctionalMapping/FunctionalMapping/Program.fs
@@ -0,0 +1,28 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open System.Diagnostics
+
+[<EntryPoint>]
+let main argv = 
+    let size = pown 10 7
+    let f x = x + x
+
+    let stopWatch = Stopwatch()
+
+    stopWatch.Start()
+    let xs = [| 1 ..  size |]
+    Array.map f xs |> ignore
+    stopWatch.Stop()
+
+    printfn "Time to run one loop with many iterations: %d" stopWatch.ElapsedMilliseconds
+
+    stopWatch.Restart()
+    let ys = [| 0 .. 9 |]
+    for _ in 1 .. size do
+        Array.map f ys |> ignore
+    stopWatch.Stop()
+
+    printfn "Time to run many loops with few iterations: %d" stopWatch.ElapsedMilliseconds
+    
+    0 // return an integer exit code
