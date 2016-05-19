975583754840dde803687b2d79d8adc66e6a71da
Robert Impey <robert@impey.info>
2016-05-19 22:45:50 +0100

    VB: Parsing XML literals.

---
+++ VB.Net/XmlLiterals/XmlLiterals/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim fruits =
+                <?xml version="1.0"?>
+                <fruits>
+                    <fruit>Apple</fruit>
+                    <fruit>Banana</fruit>
+                    <fruit>Coconut</fruit>
+                </fruits>
+
+        For Each fruit In fruits.Element("fruits").Elements()
+            Console.WriteLine(fruit.Value)
+        Next
+
+    End Sub
+
+End Module
