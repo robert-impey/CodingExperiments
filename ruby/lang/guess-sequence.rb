fe01b07bc43b4b801c62d1423f5c56c58a468a7f
Robert Impey <robert@impey.info>
2016-05-22 17:20:53 +0100

    RB: Refactored sequence guessing
    Moved validation and sequence testing logic to functions.

--- ruby/lang/guess-sequence.rb
+++ ruby/lang/guess-sequence.rb
@@ -1,26 +1,15 @@
 #!/usr/bin/env ruby
 
-puts <<HLP
-Try to guess the sequence.
+def string_is_number(candidate)
+	candidate =~ /^\s*[-+]?[0-9]+(?:\.[0-9]+)?\s*$/
+end
 
-I am thinking of an infinite sequence of numbers.
-
-You need to try to guess the sequence.
-
-You can find out about the sequence by giving me lists of 3 numbers.
-
-I will say "Yes" or "No" depending on whether the numbers appear
-in the sequence in the same order.
-
-Press "q" to quit.
-
-To get you started, I will give you these three numbers:
-
-2, 4, 6
-HLP
+def number_is_integer(candidate)
+	candidate =~ /^\s*[-+]?[0-9]+\s*$/
+end
 
 def are_in_sequence(list)
-	if list.all? {|cand| cand =~ /^\s*[-+]?[1-9]([0-9]*)?\s*$/ } then
+	if list.all? {|cand| number_is_integer(cand) } then
 		intList = list.map { |s| s.to_i }
 
 		if intList[0] < intList[1] and intList[1] < intList[2] then
@@ -33,7 +22,7 @@ end
 
 def validate(list)
 	if list.count == 3 then
-		if list.all? {|cand| cand =~ /^\s*[-+]?[1-9]([0-9]*)?(?:\.[0-9]+)?\s*$/ } then
+		if list.all? {|cand| string_is_number(cand) } then
 			return true
 		end
 	end
@@ -41,23 +30,46 @@ def validate(list)
 	return false
 end
 
-while true do
-	puts "Please give me the next candidate list: "
+if __FILE__ == $0 then
+	puts <<HLP
+Try to guess the sequence.
 
-	list = gets.chomp.split(",")
-	
-	if list[0].downcase == "q" then
-		puts "OK, bye!"
-		break
-	end
-	
-	if validate(list) then	
-		if are_in_sequence(list) then
-			puts "Yes"
-		else
-			puts "No"
+I am thinking of an infinite sequence of numbers.
+
+You need to try to guess the sequence.
+
+You can find out about the sequence by giving me lists of 3 numbers.
+
+I will say "Yes" or "No" depending on whether the numbers appear
+in the sequence in the same order.
+
+To get you started, I will give you these three numbers that meet
+the criteria for saying "Yes":
+
+2 4 6
+
+Press "q" to quit.
+
+HLP
+
+	while true do
+		puts "Please give me the next candidate list: "
+
+		list = gets.chomp.split(/\s+/)
+		
+		if list[0].downcase == "q" then
+			puts "OK, bye!"
+			break
 		end
-	else
-		puts "I asked for 3 numbers!"
+		
+		if validate(list) then	
+			if are_in_sequence(list) then
+				puts "Yes"
+			else
+				puts "No"
+			end
+		else
+			puts "I asked for 3 numbers!"
+		end
 	end
 end
