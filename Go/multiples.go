c470adcd023d75498d45e44717fc68795ed1b764
Robert Impey <robert@impey.info>
2016-09-06 22:57:12 +0100

    Go: Refactored multiples printing to func

--- Go/multiples.go
+++ Go/multiples.go
@@ -2,12 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
-	number := 7
-	currentTotal := number
+func printMultiples(number int, numberOfMultiples int) {
+	currentTotal := 0
 
-	for i := 0; i < 10; i++ {
-		fmt.Println(currentTotal)
+	for i := 0; i < numberOfMultiples; i++ {
 		currentTotal += number
+		fmt.Println(currentTotal)
 	}
+}
+
+func main() {
+	number := 5
+	numberOfMultiples := 7
+
+	printMultiples(number, numberOfMultiples)
 }
