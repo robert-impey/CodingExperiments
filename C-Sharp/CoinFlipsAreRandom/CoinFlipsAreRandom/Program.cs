cdf9f49da5d5e663edfd692b6eb1861af7d3ba20
Robert Impey <robert@impey.info>
2016-09-15 23:28:27 +0100

    CS: Command line parsing for coin flips.

--- C-Sharp/CoinFlipsAreRandom/CoinFlipsAreRandom/Program.cs
+++ C-Sharp/CoinFlipsAreRandom/CoinFlipsAreRandom/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CommandLine;
 using static System.Console;
 
 namespace CoinFlipsAreRandom
@@ -7,31 +8,47 @@ namespace CoinFlipsAreRandom
     {
         static void Main(string[] args)
         {
-            WriteLine("heads, tails, flips, heads share");
-
-            var runs = 1000;
-
-            // Taken from http://www.bbc.co.uk/news/politics/eu_referendum/results
-            var flips = 33551983;
-
-            var randomNumberGenerator = new Random();
-
-            for (var run = 0; run < runs; run++)
+            var commandLineArgs = new Options();
+            if (Parser.Default.ParseArguments(args, commandLineArgs))
             {
-                var heads = 0;
+                WriteLine("heads, tails, flips, heads share");
 
-                for (var coinToss = 0; coinToss < flips; coinToss++)
+                var runs = commandLineArgs.Runs;
+                var flips = commandLineArgs.CompetitionSize;
+
+                var randomNumberGenerator = new Random();
+
+                for (var run = 0; run < runs; run++)
                 {
-                    var toss = randomNumberGenerator.Next(2);
+                    var heads = 0;
 
-                    if (toss > 0)
+                    for (var coinToss = 0; coinToss < flips; coinToss++)
                     {
-                        heads++;
-                    }
-                }
+                        var toss = randomNumberGenerator.Next(2);
 
-                WriteLine($"{heads}, {flips - heads}, {flips}, {(0.0 + heads) / flips}");
+                        if (toss > 0)
+                        {
+                            heads++;
+                        }
+                    }
+
+                    WriteLine($"{heads}, {flips - heads}, {flips}, {(0.0 + heads) / flips}");
+                }
+            }
+            else
+            {
+                WriteLine("Unable to parse the command line arguments!");
             }
         }
+    }
+
+    class Options
+    {
+        // Taken from http://www.bbc.co.uk/news/politics/eu_referendum/results
+        [Option('s', "competitionSize", DefaultValue = 33551983, HelpText = "The size of each coin tossing competition.")]
+        public int CompetitionSize { get; set; }
+
+        [Option('r', "runs", DefaultValue = 1000, HelpText = "The number of runs of the competition")]
+        public int Runs { get; set; }
     }
 }
