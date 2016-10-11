3f69ca4ce9b497b6daa91b5868f5d6322da4d468
Robert Impey <robert@impey.info>
2016-10-11 15:08:00 +0100

    CS: WPF App to test formatting JSON

---
+++ C-Sharp/FormatJson/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FormatJson
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void formatButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var stringReader = new StringReader(textBox.Text))
+            using (var stringWriter = new StringWriter())
+            {
+                var jsonReader = new JsonTextReader(stringReader);
+                var jsonWriter = new JsonTextWriter(stringWriter) { Formatting = Formatting.Indented };
+                jsonWriter.WriteToken(jsonReader);
+                textBox.Text = stringWriter.ToString();
+            }
+
+        }
+    }
+}
