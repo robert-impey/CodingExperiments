853f78923064068bea389fc79f8db145565e7ef6
Robert Impey <robert@impey.info>
2016-11-14 21:48:19 +0000

    C: Building and traversing a singly linked list.

--- c/singly-linked-list/main.c
+++ c/singly-linked-list/main.c
@@ -17,12 +17,41 @@ struct Node
 
 int main(int argc, char **argv)
 {
-    int i;
-    for (i = 1; i < argc; i++) {
-        printf("%d - %d\n", i, atoi(argv[i]));
-    }
+    if (argc > 1) {
+        struct Node *head;
 
-    printf("\n");
+        int i;
+        for (i = argc - 1; i > 0; i--) {
+            int current_arg = atoi(argv[i]);
+
+            struct Node *temp;
+
+            temp = (struct Node *)malloc(sizeof (struct Node));
+            temp->Data = current_arg;
+
+            if (head == NULL) {
+                head = temp;
+                head->Next = NULL;
+            } else {
+                temp->Next = head;
+                head = temp;
+            }
+        }
+
+        struct Node *current;
+        current = head;
+        while (1) {
+            printf("%d", current->Data);
+            if (current->Next == NULL) {
+                printf("\n");
+                break;
+            } else {
+                printf(" ");
+                current = current->Next;
+            }
+        }
+    }
 
     return 0;
 }
+
