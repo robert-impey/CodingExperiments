f63e6456f999aee6844b43962b0025f328de547d
Robert Impey <robert@impey.info>
2017-11-05 22:43:06 +0000

    Go: Simplified echo with join.

--- Go/echo.go
+++ Go/echo.go
@@ -4,15 +4,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	var s, sep string
-
-	for _, arg := range os.Args[1:] {
-		s += sep + arg
-		sep = " "
-	}
-
-	fmt.Println(s)
+	fmt.Println(strings.Join(os.Args[1:], " "))
 }
