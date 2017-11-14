62e04ab42c2f9526effb78e61bbf5c03fd37a1e9
Robert Impey <robert@impey.info>
2017-11-14 22:45:59 +0000

    Go: Give the stop watch program something to do

--- Go/stop-watch.go
+++ Go/stop-watch.go
@@ -9,5 +9,14 @@ import (
 func main() {
 	start := time.Now()
 
+	max := 1000
+	total := 0
+	for i := 0; i < max; i++ {
+		total += i
+		for j := 0; j < max; j++ {
+			total += j
+		}
+	}
+
 	fmt.Printf("Elapsed nanoseconds %d\n", time.Since(start).Nanoseconds())
 }
