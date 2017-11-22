8a44253c3f1f9116595629a997b8b7f425622702
Robert Impey <robert@impey.info>
2017-11-22 21:23:06 +0000

    Perl: Copying files using other character systems.

--- Perl/read-utf8-files.pl
+++ Perl/read-utf8-files.pl
@@ -3,11 +3,31 @@
 use strict;
 use warnings;
 
+use File::Spec;
+use File::Remove 'remove';
+use File::Copy 'cp';
+
 my $korean_file_name = "한국어.txt";
 
 if (-f $korean_file_name){
-    print "I can see the file.\n"
+    print "I can see the file.\n";
 } else {
-    print "I can't see the file!\n"
+    print "I can't see the file!\n";
 }
+
+my $tmp_dir = File::Spec->tmpdir() . '/perl-utf-files-experiment';
+
+mkdir $tmp_dir;
+
+my $tmp_korean_file_name = "$tmp_dir/$korean_file_name";
+
+cp( $korean_file_name,  $tmp_korean_file_name);
+
+if (-f $tmp_korean_file_name) {
+    print "I can see the temporary file.\n";
+} else {
+    print "I can't see the temporary file!\n";
+}
+
+remove(\1, $tmp_dir);
 
