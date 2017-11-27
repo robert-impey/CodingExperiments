16f5bfef8aceb72755919e06942f788dfa42db39
Robert Impey <robert@impey.info>
2017-11-27 10:04:02 +0000

    Go: Lissajous to green on black

--- Go/lissajous.go
+++ Go/lissajous.go
@@ -11,11 +11,11 @@ import (
 	"os"
 )
 
-var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{color.Black, color.RGBA{0x00, 0xFF, 0x00, 0xFF}}
 
 const (
-	whiteIndex = 0 // first color in palette
-	blackIndex = 1 //  next color in palette
+	bgIndex = 0 // first color in palette
+	fgIndex = 1 //  next color in palette
 )
 
 func main() {
@@ -40,7 +40,7 @@ func lissajous(out io.Writer) {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				blackIndex)
+				fgIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
