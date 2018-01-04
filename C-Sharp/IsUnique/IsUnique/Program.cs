51072eaa2d5abfe2e9fe6e9b2f07d367a1452474
Robert Impey <robert@impey.info>
2018-01-04 13:17:31 +0000

    C#: IsUnique problem
    Is there a repeated character in a string?
    See Cracking the Coding Interview by Gayle Laakmann McDowell
    Problem 1.1

---
+++ C-Sharp/IsUnique/IsUnique/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IsUnique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine($"Is '{arg}' all unique? {IsUnique(arg)}");
+            }
+        }
+
+        private static bool IsUnique(string str)
+        {
+            var counts = new Dictionary<char, int>();
+
+            for (var i = 0; i < str.Length; i++)
+            {
+                var c = str[i];
+                if (counts.ContainsKey(c))
+                {
+                    counts[c]++;
+                }
+                else
+                {
+                    counts[c] = 1;
+                }
+            }
+
+            return !counts.Any(kv => kv.Value > 1);
+        }
+    }
+}
