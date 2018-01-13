d83155f1e2820c949eb172aec2c895d7c368f7d8
Robert Impey <robert@impey.info>
2018-01-13 13:14:47 +0000

    C#: Print an int as a binary string

---
+++ C-Sharp/PrintBinary/PrintBinary/Program.cs
@@ -0,0 +1,40 @@
+﻿using static System.Console;
+
+namespace PrintBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                if (int.TryParse(arg, out int n))
+                {
+                    PrintBinary(n);
+                }
+            }
+        }
+
+        private static void PrintBinary(int n)
+        {
+            var numBits = 32;
+            var checkBit = 0x80000000;
+
+            for (int i = 0; i < numBits; i++)
+            {
+                if ((checkBit & n) == 0)
+                {
+                    Write(0);
+                }
+                else
+                {
+                    Write(1);
+                }
+
+                n = n << 1;
+            }
+
+            WriteLine();
+        }
+    }
+}
