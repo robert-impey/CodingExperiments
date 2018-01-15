85afb3f4fcefc970960e788b2daca20d05ff7d7b
Robert Impey <robert@impey.info>
2018-01-15 12:00:31 +0000

    C#: Adding a field to an ASP page.

--- C-Sharp/RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
+++ C-Sharp/RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -13,5 +13,6 @@ namespace RazorPagesMovie.Models
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 }
