77cec9d471424068a14f5deb5ceccec7320bf5b8
Robert Impey <robert@impey.info>
2018-01-28 11:22:18 +0000

    C#: Added HW controller to MVC app.

---
+++ C-Sharp/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // /HelloWorld/Welcome/
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is {numTimes}");
+        }
+    }
+}
