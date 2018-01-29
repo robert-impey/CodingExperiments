6b4214a958673e591bcbbc223eece3b09af91d0b
Robert Impey <robert@impey.info>
2018-01-29 14:54:17 +0000

    C#: Added views to the MVC Movie tutorial.

--- C-Sharp/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ C-Sharp/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -12,15 +12,15 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     {
         // GET: /HelloWorld/
-        public string Index()
-        {
-            return "This is my default action...";
-        }
+        public IActionResult Index() => View();
 
         // /HelloWorld/Welcome/
-        public string Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is {numTimes}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
