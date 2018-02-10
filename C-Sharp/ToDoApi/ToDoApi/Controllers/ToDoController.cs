a4e3974887cfa57a916cafda56e1daefe3fb64d9
Robert Impey <robert@impey.info>
2018-02-10 14:21:21 +0000

    C#: Web Api tutorial

---
+++ C-Sharp/ToDoApi/ToDoApi/Controllers/ToDoController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using ToDoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ToDoController : Controller
+    {
+        private readonly ToDoContext _context;
+
+        public ToDoController(ToDoContext context)
+        {
+            _context = context;
+
+            if (_context.ToDoItems.Count() == 0)
+            {
+                _context.ToDoItems.Add(new ToDoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<ToDoItem> GetAll()
+        {
+            return _context.ToDoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.ToDoItems.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] ToDoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            _context.ToDoItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] ToDoItem item)
+        {
+            if (item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = _context.ToDoItems.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            todo.IsComplete = item.IsComplete;
+            todo.Name = item.Name;
+
+            _context.ToDoItems.Update(todo);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todo = _context.ToDoItems.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.ToDoItems.Remove(todo);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+    }
+}
