e340785c11794d3c0a03c83d17439b23531abc53
Robert Impey <robert-impey@users.noreply.github.com>
2018-06-28 22:43:15 +0100

    C#: Caller Info

---
+++ C-Sharp/CallerInfo/CallerInfo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+using static System.Console;
+
+namespace CallerInfo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrinterA();
+            PrinterB();
+        }
+
+        private static void PrintCaller(
+            [CallerMemberName] string memberName = "")
+        {
+            WriteLine("In PrintCaller");
+            WriteLine($"Called by {memberName}");
+        }
+
+        private static void PrinterA()
+        {
+            PrintCaller();
+        }
+
+        private static void PrinterB()
+        {
+            PrintCaller();
+        }
+    }
+}
