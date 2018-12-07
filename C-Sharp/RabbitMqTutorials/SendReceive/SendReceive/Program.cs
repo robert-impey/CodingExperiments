74f6f25103bf18fcebd81d2500e384372f1cfd29
Robert Impey <robert-impey@users.noreply.github.com>
2018-12-07 15:26:27 +0000

    C#: Send/ Receive tutorial for RabbitMQ

---
+++ C-Sharp/RabbitMqTutorials/SendReceive/SendReceive/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SendReceive
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("To demonstrate sending and receiving messages via RabbitMQ");
+        }
+    }
+}
