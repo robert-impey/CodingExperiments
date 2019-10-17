87401f9a726dde13102a8c70fcd9e0f73ff1c1d5
Robert Impey <robert.impey@gmail.com>
2019-10-17 11:59:47 +0100

    Go: Dup1 single fruit test

--- Go/src/dup1/dup1_test.go
+++ Go/src/dup1/dup1_test.go
@@ -14,3 +14,12 @@ func TestEmpty(t *testing.T) {
 		t.Errorf("Expected the empty file to return 0 counts, got %d", len(counts))
 	}
 }
+
+func TestSingleFruit(t *testing.T) {
+	f, _ := os.Open("single-fruit.txt")
+	counts := count(bufio.NewScanner(f))
+
+	if len(counts) != 0 {
+		t.Errorf("Expected the single fruits file to return 0 count, got %d", len(counts))
+	}
+}
