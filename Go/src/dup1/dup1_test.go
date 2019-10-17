3fb68758fddf2461003b62b7712920a2e1f400d6
Robert Impey <robert.impey@gmail.com>
2019-10-17 11:50:56 +0100

    Go: Added empty file dup1 test

---
+++ Go/src/dup1/dup1_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"testing"
+)
+
+func TestEmpty(t *testing.T) {
+	f, _ := os.Open("empty.txt")
+	counts := count(bufio.NewScanner(f))
+
+	if len(counts) != 0 {
+		t.Errorf("Expected the empty file to return 0 counts, got %d", len(counts))
+	}
+}
