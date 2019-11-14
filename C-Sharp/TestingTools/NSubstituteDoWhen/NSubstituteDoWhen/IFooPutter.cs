4fff76eac859789b516af9cf0acc81fada3d6113
Robert Impey <robert.impey@gmail.com>
2019-11-14 18:15:18 +0000

    C#: Testing NSub counting void method calls

---
+++ C-Sharp/TestingTools/NSubstituteDoWhen/NSubstituteDoWhen/IFooPutter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSubstituteDoWhen
+{
+    public interface IFooPutter
+    {
+        void Put(string name);
+    }
+}
---
+++ C-Sharp/TestingTools/NSubstituteDoWhen/NSubstituteDoWhen/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NSubstitute;
+
+namespace NSubstituteDoWhen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = 0;
+            var appleCounter = 0;
+
+            var fooPutter = Substitute.For<IFooPutter>();
+
+            fooPutter.When(x => x.Put(Arg.Any<string>())).Do(x => counter++);
+            fooPutter.When(x => x.Put("apples")).Do(x => appleCounter++);
+
+            fooPutter.Put("apples");
+            fooPutter.Put("bananas");
+
+            Console.WriteLine($"Counter: {counter}");
+            Console.WriteLine($"Apple Counter: {appleCounter}");
+        }
+    }
+}
