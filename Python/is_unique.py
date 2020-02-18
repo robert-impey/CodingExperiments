920fdbf76338063c3b3f04bc5a555767cf8bc22e
robert-impey <robert-impey@users.noreply.github.com>
2020-02-18 23:27:00 +0000

    Py: rec searching

--- Python/is_unique.py
+++ Python/is_unique.py
@@ -14,15 +14,28 @@ def is_unique_no_data_structure(input_str):
         for j in range(i):
             if input_str[j] == c:
                 return False
-                
+
+    return True
+
+def is_unique_rec(input_str, pivot = 0):
+    if (pivot < len(input_str)):
+        c = input_str[pivot]
+
+        for i in range(pivot):
+            if (input_str[i] == c):
+                return False
+        
+        return is_unique_rec(input_str, pivot + 1)
+
     return True
 
 def print_uniqueness(input_str):
     is_unique_result = is_unique(input_str)
     no_ds_result = is_unique_no_data_structure(input_str)
+    rec = is_unique_rec(input_str)
 
-    algos_match = is_unique_result == no_ds_result
-    print(f"'{input_str}', {is_unique_result}, {no_ds_result}, {algos_match}")
+    algos_match = is_unique_result == no_ds_result == rec
+    print(f"'{input_str}', {is_unique_result}, {no_ds_result}, {rec}, {algos_match}")
 
 print_uniqueness("")
 print_uniqueness("a")
