8571c0ffd183d9a24f759b3ad4bcfb555521c206
robert-impey <robert-impey@users.noreply.github.com>
2020-02-24 22:00:57 +0000

    Py: In order traversal of binary tree

---
+++ Python/binary_tree.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+class Node:
+    def __init__(self, datum, left = None, right = None):
+        self.datum = datum
+        self.left = left
+        self.right = right
+
+#      1
+#    /   \
+#   5     8
+#  / \     \
+# 4   3     0
+
+four = Node(4)
+three = Node(3)
+zero = Node(0)
+five = Node(5, four, three)
+eight = Node(8, None, zero)
+one = Node(1, five, eight)
+
+def in_order_traversal(tree):
+    def helper(tree, acc):
+        if tree.left is not None:
+            acc = helper(tree.left, acc)
+        acc.append(tree.datum)
+        if tree.right is not None:
+            acc = helper(tree.right, acc)
+        return acc
+    return helper(tree, [])
+
+def test_in_order_traversal():
+    traversed = in_order_traversal(one)
+    assert traversed == [4, 5, 3, 1, 8, 0]
