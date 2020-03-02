f69ab436cd0dfaea0ad06c3b3b6a777c22540b13
robert-impey <robert-impey@users.noreply.github.com>
2020-03-02 22:08:42 +0000

    Py: Solved sorted merge tests

--- Python/sorted_merge.py
+++ Python/sorted_merge.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python3
 
 def sorted_merge(l1, l2):
-    return []
+    if len(l1) == 0:
+        return l2
+    
+    if len(l2) == 0:
+        return l1
+        
+    merged = []
+    
+    index1 = index2 = 0
+    while True:
+        if index1 < len(l1) and index2 < len(l2):
+            if l1[index1] <= l2[index2]:
+                merged.append(l1[index1])
+                index1 += 1
+            else:
+                merged.append(l2[index2])
+                index2 += 1
+        elif index1 < len(l1):
+            merged.append(l1[index1])
+            index1 += 1
+        elif index2 < len(l2):
+            merged.append(l2[index2])
+            index2 += 1
+        else:
+            break
+    
+    return merged
     
 def test_sorted_merge():
     tests = [
@@ -9,7 +35,9 @@ def test_sorted_merge():
         ([1], [1], [1, 1]),
         ([1], [2], [1, 2]),
         ([2], [1], [1, 2]),
-        ([1, 3], [2], [1, 2, 3])
+        ([1, 3], [2], [1, 2, 3]),
+        ([1], [], [1]),
+        ([], [1], [1])
     ]
     
     for l1, l2, expectation in tests:
