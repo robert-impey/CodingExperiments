0e6ea00db942062086884ac46d106ae80a75ab42
robert-impey <robert-impey@users.noreply.github.com>
2020-03-15 21:13:27 +0000

    C#: Top down list merge sort

--- C-Sharp/Sorting/Sorting.Lib/Algorithms/TopDownListMergeSorter.cs
+++ C-Sharp/Sorting/Sorting.Lib/Algorithms/TopDownListMergeSorter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Sorting.Lib.Algorithms
 {
@@ -6,7 +7,39 @@ namespace Sorting.Lib.Algorithms
     {
         public int[] Sort(int[] items)
         {
-            throw new NotImplementedException();
+            if (items.Length <= 1)
+            {
+                return items;
+            }
+
+            return Sort(new LinkedList<int>(items)).ToArray();
+        }
+
+        private LinkedList<int> Sort(LinkedList<int> items)
+        {
+            if (items.Count <= 1)
+            {
+                return items;
+            }
+
+            var left = new LinkedList<int>();
+            var right = new LinkedList<int>();
+
+            var counter = 0;
+            foreach (var item in items)
+            {
+                if (counter < items.Count / 2)
+                {
+                    left.AddLast(item);
+                }
+                else
+                {
+                    right.AddLast(item);
+                }
+                counter++;
+            }
+
+            return SortedMerger.Merge(Sort(left), Sort(right));
         }
     }
 }
