215f0d82b2dd266f1c97e1206e21d5cc9618866a
Robert Impey <robert.impey@gmail.com>
2020-04-12 11:56:20 +0100

    C#: Incomplet LRU Cache with dictionary.

--- C-Sharp/ComputerScience/Cache/LruCache.cs
+++ C-Sharp/ComputerScience/Cache/LruCache.cs
@@ -1,24 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Cache
 {
     public class LruCache : IIntCache
     {
         private readonly int _capacity;
-
+        private readonly IDictionary<int, int> _values;
         public LruCache(int capacity)
         {
             _capacity = capacity;
+            _values = new Dictionary<int, int>();
         }
         
         public int Get(int key)
         {
-            throw new NotImplementedException();
+            if (!_values.ContainsKey(key))
+            {
+                return -1;
+            }
+
+            return _values[key];
         }
 
         public void Put(int key, int value)
         {
-            throw new NotImplementedException();
+            _values[key] = value;
         }
     }
 }
