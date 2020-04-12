a9b88ddac2d9ec0badefe5f027eaccbd1781ab0f
Robert Impey <robert.impey@gmail.com>
2020-04-12 16:30:34 +0100

    C#: Fifo with a dictionary

--- C-Sharp/ComputerScience/Cache/DictionaryFifoCache.cs
+++ C-Sharp/ComputerScience/Cache/DictionaryFifoCache.cs
@@ -1,16 +1,23 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Cache
 {
     public class DictionaryFifoCache : IFifoCache
     {
         private readonly IDictionary<int, int> _dictionary;
+        private readonly IDictionary<int, int> _additions;
+
+        private int _currentAddition;
         
         public DictionaryFifoCache(int capacity)
         {
             Capacity = capacity;
             
             _dictionary = new Dictionary<int, int>();
+            _additions = new Dictionary<int, int>();
+
+            _currentAddition = int.MinValue;
         }
 
         public int Capacity { get; }
@@ -27,6 +34,26 @@ namespace Cache
 
         public void Put(int key, int value)
         {
+            if (_dictionary.Count() >= Capacity)
+            {
+                var minKey = _additions.Keys.First();
+                var minAddition = _additions[minKey];
+
+                foreach (var (additionKey, additionValue) in _additions)
+                {
+                    if (additionValue <= minAddition)
+                    {
+                        minKey = additionKey;
+                    }
+                }
+
+                _additions.Remove(minKey);
+                _dictionary.Remove(minKey);
+            }
+
+            _additions[key] = _currentAddition;
+            _currentAddition++;
+            
             _dictionary[key] = value;
         }
     }
