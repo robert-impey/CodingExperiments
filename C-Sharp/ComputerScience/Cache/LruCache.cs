8e821a48dff6758c817a4adc687e717c84f681ab
Robert Impey <robert.impey@gmail.com>
2020-04-12 15:52:48 +0100

    C#: Incompleted LRU cache

--- C-Sharp/ComputerScience/Cache/LruCache.cs
+++ C-Sharp/ComputerScience/Cache/LruCache.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Cache
 {
@@ -7,10 +8,22 @@ namespace Cache
     {
         private readonly int _capacity;
         private readonly IDictionary<int, int> _values;
+        private readonly IDictionary<int, int> _additions;
+        private readonly IDictionary<int, int> _usages;
+
+        private int _currentAddition;
+        private int _currentUsage;
+        
         public LruCache(int capacity)
         {
             _capacity = capacity;
+            
             _values = new Dictionary<int, int>();
+            _additions = new Dictionary<int, int>();
+            _usages = new Dictionary<int, int>();
+
+            _currentAddition = int.MinValue;
+            _currentUsage = int.MinValue;
         }
         
         public int Get(int key)
@@ -20,12 +33,63 @@ namespace Cache
                 return -1;
             }
 
+            _usages[key] = _currentUsage;
+            _currentUsage++;
+
             return _values[key];
         }
 
         public void Put(int key, int value)
         {
+            // Possibly evict the least recently used
+            if (_values.Count >= _capacity)
+            {
+                if (_usages.Any())
+                {
+                    var currentMin = int.MaxValue;
+                    int minKey = _usages.Keys.First();
+
+                    foreach (var (usageKey, usageValue) in _usages)
+                    {
+                        if (usageValue <= currentMin)
+                        {
+                            minKey = usageKey;
+                        }
+                    }
+
+                    _usages.Remove(minKey);
+                    _additions.Remove(minKey);
+                    _values.Remove(minKey);
+                }
+                else
+                {
+                    if (_additions.Any())
+                    {
+                        var currentMin = int.MaxValue;
+                        int minKey = _additions.Keys.First();
+
+                        foreach (var (additionKey, additionValue) in _additions)
+                        {
+                            if (additionValue <= currentMin)
+                            {
+                                minKey = additionKey;
+                            }
+                        }
+
+                        _additions.Remove(minKey);
+                        _values.Remove(minKey);
+                    }
+                    else
+                    {
+                        throw new Exception("No additions when over capacity!");
+                    }
+                }
+            }
+
             _values[key] = value;
+
+            _additions[key] = _currentAddition;
+            _currentAddition++;
         }
     }
 }
