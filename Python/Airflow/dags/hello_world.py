e127329c81202a3289846d9eee6c0799c8a4bc1e
Robert Impey <robert.impey@gmail.com>
2020-07-09 18:41:23 +0100

    Py: Hello World for Airflow DAGs

---
+++ Python/Airflow/dags/hello_world.py
@@ -0,0 +1,20 @@
+# See https://technofob.com/2019/05/30/get-started-developing-workflows-with-apache-airflow/
+
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+def print_hello():
+    return 'Hello, World!'
+
+dag = DAG('hello_world', description='Simple tutorial DAG',
+        schedule_interval='0 12 * * *',
+        start_date=datetime(2020, 7, 9), catchup=False)
+
+dummy_operator = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
+
+hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+
+dummy_operator >> hello_operator
+
