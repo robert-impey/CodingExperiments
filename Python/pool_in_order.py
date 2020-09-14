e2fa777c435dbe130dca705de845803efd6c69d7
Robert Impey <robert.impey@gmail.com>
2020-09-14 11:54:00 +0100

    PY: Test for pools returning in order

---
+++ Python/pool_in_order.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# See https://docs.python.org/3/library/multiprocessing.html
+
+from multiprocessing import Pool
+
+def f(x):
+    return x*x
+
+def in_order(xs):
+    if len(xs) > 1:
+        previous = xs[0]
+
+        for x in xs[1:]:
+            if x < previous:
+                return False
+            previous = x
+
+    return True
+
+if __name__ == '__main__':
+    with Pool(5) as p:
+        squares = p.map(f, range(1000))
+        print(in_order(squares))
