9a21eb86721760630a230721de4402b4ba45ce87
Robert Impey <robert-impey@users.noreply.github.com>
2020-12-28 22:06:49 +0000

    SW: Iterating through dictionary

--- swift/ControlFlow.swift
+++ swift/ControlFlow.swift
@@ -39,3 +39,18 @@ switch vegetable {
     default:
         print("Everything tastes good in soup.")
 }
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+for (_, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+print(largest)
