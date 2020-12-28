5af4503296bd7b472425538cf31c62ad222a5608
Robert Impey <robert-impey@users.noreply.github.com>
2020-12-28 22:23:41 +0000

    SW: while, repeat, for loops

--- swift/ControlFlow.swift
+++ swift/ControlFlow.swift
@@ -61,3 +61,21 @@ if case let lk? = largestKind {
 } else {
     print("No largest kind found!")
 }
+
+var n = 2
+while n < 100 {
+    n *= 2
+}
+print(n)
+
+var m = 2
+repeat {
+    m *= 2
+} while m < 100
+print(m)
+
+var total = 0
+for i in 0..<4 {
+    total += i
+}
+print(total)
