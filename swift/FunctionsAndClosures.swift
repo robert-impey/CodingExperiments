374cf6ac8e891b2179a98061ed26041136bfcc10
Robert Impey <robert-impey@users.noreply.github.com>
2020-12-29 15:35:02 +0000

    SW: First class functions

--- swift/FunctionsAndClosures.swift
+++ swift/FunctionsAndClosures.swift
@@ -46,3 +46,26 @@ func returnFifteen() -> Int {
     return y
 }
 print(returnFifteen())
+
+func makeIncrementer() -> ((Int) -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
+    }
+    return addOne
+}
+var increment = makeIncrementer()
+print(increment(7))
+
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+var numbers = [20, 19, 7, 12]
+print(hasAnyMatches(list: numbers, condition: lessThanTen))
