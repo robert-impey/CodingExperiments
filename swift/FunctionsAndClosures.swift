da13dbe1ca3d5d0d45d7b9437bc9a6bf4da2056c
Robert Impey <robert-impey@users.noreply.github.com>
2020-12-29 15:24:58 +0000

    SW: Return tuple

--- swift/FunctionsAndClosures.swift
+++ swift/FunctionsAndClosures.swift
@@ -14,3 +14,25 @@ func greet3(_ person: String, on day: String) -> String {
     return "Hello \(person), today is \(day)."
 }
 print(greet3("John", on: "Wednesday"))
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+
+    return (min, max, sum)
+}
+let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
+print(statistics.sum)
+print(statistics.2)
+print(statistics.min)
+print(statistics.max)
