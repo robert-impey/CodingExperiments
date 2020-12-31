d3e1bdd12b202800af003101bc2c599c81cf76b1
Robert Impey <robert.impey@gmail.com>
2020-12-31 14:36:54 +0000

    SW: willSet properties

--- swift/ObjectsAndClasses.swift
+++ swift/ObjectsAndClasses.swift
@@ -106,3 +106,28 @@ print(triangle.perimeter)
 triangle.perimeter = 9.3
 print(triangle.sideLength)
 print(triangle.perimeter)
+
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+
+    init (size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+
+var triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+print(triangleAndSquare.triangle.sideLength)
