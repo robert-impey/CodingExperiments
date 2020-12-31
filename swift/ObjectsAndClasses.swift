68c11029b1f5e9f51812804578c7ff8d3091b517
Robert Impey <robert.impey@gmail.com>
2020-12-31 14:28:37 +0000

    SW: Property get and set

--- swift/ObjectsAndClasses.swift
+++ swift/ObjectsAndClasses.swift
@@ -77,3 +77,32 @@ class Circle: NamedShape {
 let testCircle = Circle(radius: 3.4, name: "my test circle")
 print(testCircle.area())
 print(testCircle.simpleDescription())
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+
+    init (sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+}
+
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.perimeter)
+triangle.perimeter = 9.3
+print(triangle.sideLength)
+print(triangle.perimeter)
