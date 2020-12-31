56585b557fcdbb8cf4f17f90a067bbe263697119
Robert Impey <robert.impey@gmail.com>
2020-12-31 14:03:12 +0000

    SW: Class inheritance

--- swift/ObjectsAndClasses.swift
+++ swift/ObjectsAndClasses.swift
@@ -33,3 +33,25 @@ class NamedShape {
 
 var namedShape = NamedShape(name: "Fred")
 print(namedShape.name)
+
+class Square: NamedShape {
+    var sideLength: Double
+
+    init (sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+}
+
+let test = Square(sideLength: 5.2, name: "my test square")
+print(test.area())
+print(test.simpleDescription())
