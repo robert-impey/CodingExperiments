e3e8a7fbcfa2a0921bd339de9dd6ccefa900c46d
Robert Impey <robert.impey@gmail.com>
2020-12-31 13:56:15 +0000

    SW: init for class

--- swift/ObjectsAndClasses.swift
+++ swift/ObjectsAndClasses.swift
@@ -17,3 +17,19 @@ var shape = Shape()
 shape.numberOfSides = 7
 var shapeDescription = shape.simpleDescription()
 print(shapeDescription)
+
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+var namedShape = NamedShape(name: "Fred")
+print(namedShape.name)
