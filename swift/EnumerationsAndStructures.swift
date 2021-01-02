bdf6972023c07b6f748e6d044bee302151f2aae7
Robert Impey <robert-impey@users.noreply.github.com>
2021-01-02 16:09:02 +0000

    SW: Enum switching

--- swift/EnumerationsAndStructures.swift
+++ swift/EnumerationsAndStructures.swift
@@ -72,3 +72,18 @@ print(heartsDescription)
 
 print(Suit.hearts.colour())
 print(Suit.clubs.colour())
+
+enum ServerResponse {
+    case result(String, String)
+    case failure(String)
+}
+
+let success = ServerResponse.result("6:00 am", "8.09 pm")
+let failure = ServerResponse.failure("Out of cheese")
+
+switch success {
+    case let .result(sunrise, sunset):
+        print("Sunrise is at \(sunrise) and sunset is at \(sunset)")
+    case let .failure(message):
+        print("Failure... \(message)")
+}
