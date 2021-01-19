d3fcfbf3335514885a1d2e100003115b39eea2e4
Robert Impey <robert.impey@gmail.com>
2021-01-19 22:05:01 +0000

    SW: Non-mutating func added to protocol

--- swift/ProtocolsAndExtensions.swift
+++ swift/ProtocolsAndExtensions.swift
@@ -3,6 +3,7 @@
 protocol ExampleProtocol {
     var simpleDescription: String { get }
     mutating func adjust()
+    func augmentedDescription () -> String
 }
 
 class SimpleClass: ExampleProtocol {
@@ -10,18 +11,26 @@ class SimpleClass: ExampleProtocol {
     var anotherPropery: Int = 69105
     func adjust() {
         simpleDescription += " Now 100% adjusted."
-    }    
+    }
+    func augmentedDescription() -> String {
+        return simpleDescription + " Augmented."
+    }
 }
 
 var a = SimpleClass()
 a.adjust()
 let aDescription = a.simpleDescription
 print(aDescription)
+print(a.augmentedDescription())
+print(a.simpleDescription)
 
 struct SimpleStructure: ExampleProtocol {
     var simpleDescription: String = "A simple structure"
     mutating func adjust() {
         simpleDescription += " (adjusted)"
+    }
+    func augmentedDescription() -> String {
+        return simpleDescription + " (aug'd)"
     }
 }
 
@@ -29,3 +38,5 @@ var b = SimpleStructure()
 b.adjust()
 let bDescription = b.simpleDescription
 print(bDescription)
+print(b.augmentedDescription())
+print(b.simpleDescription)
