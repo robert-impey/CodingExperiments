2e26c616168cdb69ced7314aa5a29aabe125befb
robert-impey <robert-impey@users.noreply.github.com>
2021-01-25 22:12:41 +0000

    PY: WS in MT Pool Exp

--- Python/pool_in_order.py
+++ Python/pool_in_order.py
@@ -4,8 +4,10 @@
 
 from multiprocessing import Pool
 
+
 def f(x):
     return x*x
+
 
 def in_order(xs):
     if len(xs) > 1:
@@ -17,6 +19,7 @@ def in_order(xs):
             previous = x
 
     return True
+
 
 if __name__ == '__main__':
     with Pool(5) as p:
