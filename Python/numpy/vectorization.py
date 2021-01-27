58f564ca378bbffe253085b9aeb6a1f7a71cc050
robert-impey <robert-impey@users.noreply.github.com>
2021-01-27 15:08:51 +0000

    PY: Timing for loop vs vectorized version

--- Python/numpy/vectorization.py
+++ Python/numpy/vectorization.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import time
 
 a = np.arange(10)
 
@@ -14,3 +15,25 @@ for an_a in a:
 print(sum_of_a)
 
 print(np.sum(a))
+
+size = 1000000
+a = np.random.rand(size)
+b = np.random.rand(size)
+
+start = time.time()
+c = np.dot(a, b)
+finish = time.time()
+
+print(c)
+print(f'Vectorized version: {1000 * (finish - start)} ms')
+
+c = 0
+start = time.time()
+
+for i in range(size):
+    c += a[i] * b[i]
+
+finish = time.time()
+
+print(c)
+print(f'For loop version: {1000 * (finish - start)} ms')
