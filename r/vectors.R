de432e8f35494e387a1669c9d558f79616ce7260
robert-impey <robert-impey@users.noreply.github.com>
2021-02-27 23:19:14 +0000

    R: Vectors

---
+++ r/vectors.R
@@ -0,0 +1,12 @@
+# See Chapter 5, Schaum's Outlines Discrete Mathematics, 2nd Ed. by Lipschutz and Lipson
+
+u <- c(2, 3, -4)
+v <- c(1, -5, 8)
+
+u + v
+5 * u
+-1 * v
+2 * u - 3 * v
+
+library(geometry)
+dot(u, v, d = TRUE)
