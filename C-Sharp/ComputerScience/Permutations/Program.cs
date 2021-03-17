f8bd203894baec14a4a2fd656d46e6395a3b5da9
robert-impey <robert-impey@users.noreply.github.com>
2021-03-17 15:20:30 +0000

    CS: Options for counting permutations

--- C-Sharp/ComputerScience/Permutations/Program.cs
+++ C-Sharp/ComputerScience/Permutations/Program.cs
@@ -6,10 +6,42 @@ namespace Permutations
     {
         static void Main(string[] args)
         {
-            var fraction = new ThreeUnequalPairsPermutationCounter().Count();
-
-            WriteLine($"Matching Count: {fraction.Matching}");
-            WriteLine($"Total Count: {fraction.Total}");
+            if (args.Length > 0)
+            {
+                if (args[0] == "count")
+                {
+                    if (args.Length > 0)
+                    {
+                        Fraction? fraction = null;
+                        if (args[1] == "three-unequal-pairs")
+                        {
+                            fraction = new ThreeUnequalPairsPermutationCounter().Count();
+                        }
+                        
+                        if (fraction.HasValue)
+                        {
+                            WriteLine($"Matching Count: {fraction.Value.Matching}");
+                            WriteLine($"Total Count: {fraction.Value.Total}");
+                        }
+                        else
+                        {
+                            WriteLine("Unrecognised thing to count!");
+                        }
+                    }
+                    else
+                    {
+                        WriteLine("What do you want me to count?");
+                    }
+                }
+                else
+                {
+                    WriteLine("Unrecognised action!");
+                }
+            }
+            else
+            {
+                WriteLine("Tell me what to do!");
+            }
         }       
     }
 }
