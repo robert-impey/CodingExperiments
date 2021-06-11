ee0fd5b58c33c5a573e8cfc41ffe45a56c8aa142
robert-impey <robert-impey@users.noreply.github.com>
2021-06-11 16:40:21 +0100

    C++: Egyptian multiplication

--- C++/FromMathsToGenProg/FromMathsToGenProg/FromMathsToGenProg.cpp
+++ C++/FromMathsToGenProg/FromMathsToGenProg/FromMathsToGenProg.cpp
@@ -5,8 +5,14 @@
 
 using namespace std;
 
+int multiply0(int n, int a)
+{
+	if (n == 1) return a;
+	return multiply0(n - 1, a) + a;
+}
+
 int main()
 {
-	cout << "Hello CMake." << endl;
+	cout << multiply0(5, 10) << endl;
 	return 0;
 }
