15ededb65370bb9cd859b082a06362160e871d31
robert-impey <robert-impey@users.noreply.github.com>
2021-06-11 16:52:44 +0100

    C++: Alternative algorithm

--- C++/FromMathsToGenProg/FromMathsToGenProg/FromMathsToGenProg.cpp
+++ C++/FromMathsToGenProg/FromMathsToGenProg/FromMathsToGenProg.cpp
@@ -1,4 +1,6 @@
 ﻿// FromMathsToGenProg.cpp : Defines the entry point for the application.
+// See Chapter 1 of "From Mathematics to Generic Programming"
+// by Alexander Stepanov and Daniel Rose
 //
 
 #include "FromMathsToGenProg.h"
@@ -11,8 +13,27 @@ int multiply0(int n, int a)
 	return multiply0(n - 1, a) + a;
 }
 
+bool odd(int n) { return n & 0x1; }
+int half(int n) { return n >> 1; }
+
+int multiply1(int n, int a)
+{
+	if (n == 1) return a;
+	int result = multiply1(half(n), a + a);
+	if (odd(n)) result = result + a;
+	return result;
+}
+
 int main()
 {
 	cout << multiply0(5, 10) << endl;
+
+	for (auto i = 1; i < 10; i++)
+	{
+		cout << odd(i) << " " << half(i) << endl;
+	}
+
+	cout << multiply1(5, 10) << endl;
+
 	return 0;
 }
