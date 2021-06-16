65d698cebd921fe0ed0141e5df3d5a7e3294ae27
Robert Impey <robert.impey@gmail.com>
2021-06-16 16:08:40 +0100

    OC: Second multiply function

--- OCaml/multiply.ml
+++ OCaml/multiply.ml
@@ -1,4 +1,16 @@
+let half n = n / 2
+let odd n = n mod 2 == 1
+
 let rec multiply0 n a =
     match n with
     | 1 -> a
     | _ -> a + multiply0 (n - 1) a
+
+let rec multiply1 n a =
+    match n with
+    | 1 -> a
+    | _ ->
+            begin
+                let r = multiply1 (half n) (a + a) in
+                if odd n then r + a else r
+            end
