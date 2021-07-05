30b65ff7bfa8b5b98ac31b5b30c08b94a6cb34a5
Robert Impey <robert.impey@gmail.com>
2021-07-05 11:35:43 +0100

    OC: Fmt fib

--- OCaml/fib.ml
+++ OCaml/fib.ml
@@ -1,7 +1,7 @@
-let rec fib n =
-    if n = 0 || n = 1 then 1
-    else fib(n - 1) + fib(n - 2);;
+let rec fib n = if n = 0 || n = 1 then 1 else fib (n - 1) + fib (n - 2)
 
-print_int (fib 5);;
-print_newline ();;
+;;
+print_int (fib 5)
 
+;;
+print_newline ()
