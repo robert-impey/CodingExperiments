2d5fbe9e630a9efafe8393bbc8e0711fdba3b843
Robert Impey <robert.impey@gmail.com>
2021-07-26 15:22:53 +0100

    OC: Fmt

--- OCaml/fact.ml
+++ OCaml/fact.ml
@@ -1,7 +1,5 @@
-let rec fact x = if x <= 1 then 1 else x * fact (x - 1)
+let rec fact x = if x <= 1 then 1 else x * fact (x - 1);;
 
-;;
-print_int (fact 5)
+print_int (fact 5);;
 
-;;
 print_newline ()
