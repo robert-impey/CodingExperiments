5edae4b971b8d613c1777a56d860327e590c4158
Robert Impey <robert.impey@gmail.com>
2021-07-28 13:49:01 +0100

    OC: Variations on factorial
    See:
    
    https://ocaml.org/learn/tutorials/a_first_hour_with_ocaml.html#Pattern-matching

--- OCaml/fact.ml
+++ OCaml/fact.ml
@@ -1,5 +1,9 @@
-let rec fact x = if x <= 1 then 1 else x * fact (x - 1);;
+(* https://ocaml.org/learn/tutorials/a_first_hour_with_ocaml.html#Pattern-matching *)
 
-print_int (fact 5);;
+let rec fact0 x = if x <= 1 then 1 else x * fact0 (x - 1)
 
-print_newline ()
+let rec fact1 n = match n with 0 | 1 -> 1 | x -> x * fact1 (x - 1)
+
+let rec fact2 n = match n with 0 | 1 -> 1 | _ -> n * fact2 (n - 1)
+
+let rec fact3 = function 0 | 1 -> 1 | n -> n * fact3 (n - 1)
