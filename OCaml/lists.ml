3f807eb742505602f367546c4117697215e3a1e7
Robert Impey <robert.impey@gmail.com>
2021-07-29 11:45:58 +0100

    OC: Comment on resources

--- OCaml/lists.ml
+++ OCaml/lists.ml
@@ -1,4 +1,7 @@
-(* https://en.wikipedia.org/wiki/OCaml *)
+(* 
+ * https://en.wikipedia.org/wiki/OCaml 
+ * https://ocaml.org/learn/tutorials/a_first_hour_with_ocaml.html#Lists
+ *)
 
 let rec sum integers =
   match integers with [] -> 0 | first :: rest -> first + sum rest
