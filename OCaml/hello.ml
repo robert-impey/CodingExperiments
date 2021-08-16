63800b461d7811bee2b13ba67b9ad1dc1fdc5ccf
Robert Impey <robert.impey@gmail.com>
2021-08-16 14:28:34 +0100

    OC: Fmt

--- OCaml/hello.ml
+++ OCaml/hello.ml
@@ -1,1 +1,2 @@
+;;
 print_endline "Bonjour, le monde!"
--- OCaml/sum.ml
+++ OCaml/sum.ml
@@ -2,6 +2,6 @@
 
 let rec sum integers =
   match integers with [] -> 0 | first :: rest -> first + sum rest
-;;
 
+;;
 Printf.printf "Sum: %d\n" (sum [ 1; 2; 3; 4; 5 ])
