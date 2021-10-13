48918dbc451a0796632eb6d9fa2232050fec587f
robert-impey <robert-impey@users.noreply.github.com>
2021-10-13 16:04:29 +0100

    DN: Graph generator

---
+++ dotnet/C-Sharp/Graphs/GenerateAdjacencyList/Program.cs
@@ -0,0 +1,34 @@
+﻿using Graphs;
+using System;
+
+namespace GenerateAdjacencyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Please give me a size and threshold!");
+                return;
+            }
+
+            int size;
+            if (!int.TryParse(args[0], out size))
+            {
+                Console.WriteLine($"Can't parse {args[0]}");
+                return;
+            }
+
+            double threshold;
+            if (!double.TryParse(args[1], out threshold))
+            {
+                Console.WriteLine($"Can't parse {args[1]}");
+                return;
+            }
+
+            var graph = GraphGenerator.Generate(size, threshold);
+            DotPrinter.PrintOneWay(graph);
+        }
+    }
+}
---
+++ dotnet/C-Sharp/Graphs/Graphs/GraphGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Graphs
+{
+    public static class GraphGenerator
+    {
+        public static Graph Generate(int size, double threshold)
+        {
+            var graph = new Graph();
+
+            var rand = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                var vertex = new Vertex($"{i}");
+
+                for (int j = 0; j < size; j++)
+                {
+                    if (i == j  || rand.NextDouble() > threshold) continue;
+
+                    vertex.AddNeighbour(new Vertex($"{j}"));
+                }
+
+                graph.AddVertex(vertex);
+            }
+
+            return graph;
+        }
+    }
+}
