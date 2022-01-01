1a7d951a5d9923dcff4dcc1167814496d3f5ddf4
robert-impey <robert-impey@users.noreply.github.com>
2022-01-01 17:22:09 +0000

    PY: User input for sum

--- Python/sum_1000.py
+++ Python/sum_1000.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 sum_of_numbers = 0
-max_number = 1000
+max_number = int(input("How many numbers?"))
 
 for i in range(1, max_number + 1):
     print("Adding %d to %d" % (i, sum_of_numbers))
