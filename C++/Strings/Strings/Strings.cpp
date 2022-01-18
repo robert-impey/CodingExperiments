f3e666e470636177f1f15138e92f7c392962dcd1
robert-impey <robert-impey@users.noreply.github.com>
2022-01-18 17:06:27 +0000

    C++: Restart strings

---
+++ C++/Strings/Strings/Strings.cpp
@@ -0,0 +1,31 @@
+﻿// Strings.cpp : Defines the entry point for the application.
+//
+
+#include "Strings.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	if (argc <= 1) {
+		cerr << "Tell me what to do." << endl;
+		return 1;
+	}
+
+	string task(argv[1]);
+
+	if (task == "list") {
+		if (argc <= 2) {
+			cerr << "Please tell me the file name to list!" << endl;
+			return 1;
+		}
+
+		string file_name(argv[2]);
+
+		cout << "Listing " << file_name << endl;
+
+		return 0;
+	}
+
+	cerr << "Unrecognized task - " << task << endl;
+	return 1;
+}
---
+++ C++/Strings/Strings/Strings.h
@@ -0,0 +1,8 @@
+﻿// Strings.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
