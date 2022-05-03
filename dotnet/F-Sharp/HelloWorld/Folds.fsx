9fb4caa860aca364ca11790ff4830760fed84562
Robert Impey <robert-impey@users.noreply.github.com>
2022-05-03 22:50:15 +0100

    F#: Focus is fold not multiply

---
+++ dotnet/F-Sharp/HelloWorld/Folds.fsx
@@ -0,0 +1,14 @@
+﻿open System.Text
+
+let multiply n a = [1 .. n ] |> List.fold (fun a' _ -> a' + a) 0
+
+multiply 5 10
+
+let sumOfFirst n = [1 .. n] |> List.fold (fun a n' -> a + n') 0
+
+sumOfFirst 10
+
+let numbers = ["one"; "two"; "three"]
+
+let forwardSb = numbers |> List.fold (fun (a: StringBuilder) w -> a.Append(w)) (StringBuilder())
+forwardSb.ToString()
--- dotnet/F-Sharp/HelloWorld/Multiply.fsx
+++
@@ -1,7 +0,0 @@
-﻿let multiply n a =
-    [1 .. n ]
-    |> List.fold (fun a' _ ->
-       a' + a 
-    ) 0
-
-multiply 5 10
