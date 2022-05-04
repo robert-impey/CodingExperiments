3631bfb07f14f96ee934b080005db79816811e4e
robert-impey <robert-impey@users.noreply.github.com>
2022-05-04 17:00:12 +0100

    EX: More list functions

--- elixir/programming-elixir/lists.exs
+++ elixir/programming-elixir/lists.exs
@@ -1,4 +1,10 @@
 defmodule MyList do
     def len([]), do: 0
     def len([_|tail]), do: 1 + len(tail)
+    
+    def square([]), do: []
+    def square([head|tail]), do: [head * head | square(tail) ]
+    
+    def add_1([]), do: []
+    def add_1([h|t]), do: [h + 1 | add_1(t)]
 end
