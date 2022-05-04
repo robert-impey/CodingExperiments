ca6fee467399b1609d9cd2bbfde776d3d3830064
robert-impey <robert-impey@users.noreply.github.com>
2022-05-04 18:09:24 +0100

    EX: Reduce a list

--- elixir/programming-elixir/lists.exs
+++ elixir/programming-elixir/lists.exs
@@ -13,4 +13,12 @@ defmodule MyList do
     
     def square_v2(l), do: map(l, &(&1 * &1))
     def add_1_v2(l), do: map(l, &(&1 + 1))
+    
+    def reduce([], v, _f), do: v
+    def reduce([h|t], v, f) do
+        reduce(t, f.(h, v), f)
+    end
+    
+    def sum_of(l), do: reduce(l, 0, &(&1 + &2))
+    def product_of(l), do: reduce(l, 1, &(&1 * &2))
 end
