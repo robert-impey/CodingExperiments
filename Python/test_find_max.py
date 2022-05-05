3836231d0600c4e1d3dd99bfc2381f702fad47a9
robert-impey <robert-impey@users.noreply.github.com>
2022-05-05 18:20:25 +0100

    PY: Rename variable to avoid shadow

--- Python/test_find_max.py
+++ Python/test_find_max.py
@@ -4,5 +4,5 @@ from find_max import find_max
 def test_lists():
     expectations = [([], None), ([1], 1), ([1, 5, 3, 4, 2], 5)]
 
-    for (input, exp) in expectations:
-        assert find_max(input) == exp
+    for (test_input, exp) in expectations:
+        assert find_max(test_input) == exp
