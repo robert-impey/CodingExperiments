8253720c3df3fd42cc0e79bca216123c052ac822
Robert Impey <robert-impey@users.noreply.github.com>
2022-05-05 22:31:44 +0100

    PY: Max temp with Pandas DataFrame

--- Python/find_max_temp.py
+++ Python/find_max_temp.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
-import csv
-import argparse
+import pandas as pd
+import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--file", action="store_const", const=csv_file)
-args = parser.parse_args()
-
-print(args.csv_file)
+for arg in sys.argv[1:]:
+    csv_file_name = arg
+    df = pd.read_csv(csv_file_name)
+    
+    max_id = df["Average Temperature"].idxmax()
+    
+    max_year = df["Year"][max_id]
+    max_temp = df["Average Temperature"][max_id]
+    
+    print(f'In {max_year} the average temperature in December was {max_temp} C')
