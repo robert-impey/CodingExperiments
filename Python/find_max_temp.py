c06212b5955a42e48241eb7033f14d15108553e2
robert-impey <robert-impey@users.noreply.github.com>
2022-05-06 11:17:47 +0100

    PY: Reformat pandas file

--- Python/find_max_temp.py
+++ Python/find_max_temp.py
@@ -6,10 +6,10 @@ import sys
 for arg in sys.argv[1:]:
     csv_file_name = arg
     df = pd.read_csv(csv_file_name)
-    
+
     max_id = df["Average Temperature"].idxmax()
-    
+
     max_year = df["Year"][max_id]
     max_temp = df["Average Temperature"][max_id]
-    
-    print(f'In {max_year} the average temperature in December was {max_temp} C')
+
+    print(f"In {max_year} the average temperature in December was {max_temp} C")
