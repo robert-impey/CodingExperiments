aa18c498f38b3f155b1e40ad03cc4d1d2f8148b8
robert-impey <robert-impey@users.noreply.github.com>
2022-05-19 21:10:09 +0100

    C#: Swap by deconstruction

--- dotnet/C-Sharp/DataStructures/Swapper.cs
+++ dotnet/C-Sharp/DataStructures/Swapper.cs
@@ -4,9 +4,7 @@
     {
         public static void Swap(int[] ary, int firstIndex, int secondIndex)
         {
-            var temp = ary[firstIndex];
-            ary[firstIndex] = ary[secondIndex];
-            ary[secondIndex] = temp;
+            (ary[firstIndex], ary[secondIndex]) = (ary[secondIndex], ary[firstIndex]);
         }
     }
 }
