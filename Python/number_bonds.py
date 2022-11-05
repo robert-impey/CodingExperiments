3a26c87190e8a1f21861eab5c8dd046e235461cb
Robert Impey <robert-impey@users.noreply.github.com>
2022-11-05 14:27:36 +0000

    PY: Number bonds with multiplication

--- Python/number_bonds.py
+++ Python/number_bonds.py
@@ -1,10 +1,16 @@
-number = 10
+number = 1000
 
 print(f'Number is {number}')
+
+count = 0
 
 for n in range(number + 1):
 	b1 = n
 	b2 = number - n
-	total = b1 + b2
+	b3 = number * n
+	total = b1 + b2 + b3
+	count += 1
 	
-	print(f'{b1} + {b2} = {total}')
+	print(f'{b1} + {b2} + {b3} = {total}')
+
+print(f'count is {count}')
