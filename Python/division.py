36a181b5fdd4e7afb18eda4a9aa8a195211d1d7c
Robert Impey <robert-impey@users.noreply.github.com>
2022-11-05 14:58:09 +0000

    PY: Division

---
+++ Python/division.py
@@ -0,0 +1,6 @@
+number = 56789
+
+print(f'number is {number}')
+
+for n in range(1, number + 1):
+	print(f'{number} / {n} = {number / n}')
--- Python/number_bonds.py
+++ Python/number_bonds.py
@@ -8,9 +8,15 @@ for n in range(number + 1):
 	b1 = n
 	b2 = number - n
 	b3 = number * n
-	total = b1 + b2 + b3
+	
+	if n == 0:
+		b4 = 0
+	else:
+		b4 = number / n
+		
+	total = b1 + b2 + b3 + b4
 	count += 1
 	
-	print(f'{b1} + {b2} + {b3} = {total}')
+	print(f'{b1} + {b2} + {b3} + {b4} = {total}')
 
 print(f'count is {count}')
