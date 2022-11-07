181df5d80f6467d4a6eff04f85e9b5f84c41925a
Robert Impey <robert-impey@users.noreply.github.com>
2022-11-07 21:00:17 +0000

    C++: Counting chars

--- C++/HelloWorld/CountChars/CountChars.cpp
+++ C++/HelloWorld/CountChars/CountChars.cpp
@@ -2,16 +2,34 @@
 //
 
 #include <iostream>
+#include <map>
 
 using std::cout;
 using std::endl;
 
 int main()
 {
-    std::string text("Alice in Wonderland");
+	std::string text("Alice's Adventures in Wonderland");
+	std::map<char, int> counts;
 
-    for (auto i = 0; i < text.size(); i++)
-    {
-        cout << text[i] << endl;
-    }
+	for (auto i = 0; i < text.size(); i++)
+	{
+		auto cur_char = text[i];
+
+		cout << cur_char << endl;
+
+		if (counts.count(cur_char))
+		{
+			counts[cur_char]++;
+		}
+		else
+		{
+			counts[cur_char] = 1;
+		}
+	}
+
+	for (auto& count : counts)
+	{
+		cout << count.first << " " << count.second << endl;
+	}
 }
