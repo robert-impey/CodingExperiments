dd045c275463b32f7bf182f1b313e5b00ddcc993
Robert Impey <robert-impey@users.noreply.github.com>
2022-11-09 11:55:39 +0000

    PY: Rearrange file to clarify

--- Python/modify_bytearray.py
+++ Python/modify_bytearray.py
@@ -2,14 +2,14 @@
 
 from bytes import get_bytes
 
+def modify_bytearray(ba: bytearray) -> None:
+	g_bytes = get_bytes("g")
+	ba[0] = g_bytes[0]
+	print(f'In method - {foo_bytes.decode("utf-8")}')
+
 foo_bytes = get_bytes("foo")
 
 print(f'In main scope - {foo_bytes.decode("utf-8")}')
-
-def modify_bytearray(ba: bytearray) -> None:
-	g_bytes = get_bytes("g")
-	foo_bytes[2] = g_bytes[0]
-	print(f'In method - {foo_bytes.decode("utf-8")}')
 
 modify_bytearray(foo_bytes)
 
