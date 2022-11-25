00b05ba7164f37c34767c6d5927d6f37737f6031
Robert Impey <robert-impey@users.noreply.github.com>
2022-11-25 11:20:40 +0000

    C++: Fix script for processing files

--- C++/FiveLetters/FiveLetters/Make-AllDifferent.ps1
+++ C++/FiveLetters/FiveLetters/Make-AllDifferent.ps1
@@ -3,19 +3,20 @@ param($exe, $dataDir)
 $dictionaryFile = "$($dataDir)\british-english.txt"
 
 $fiveLetterWordsFile = "$($dataDir)\five-letter-words.txt"
-Start-Process $exe -ArgumentList 'find_5_letter_words' -RedirectStandardInput $dictionaryFile -RedirectStandardOutput $fiveLetterWordsFile
+
+Start-Process $exe -NoNewWindow -Wait -ArgumentList 'find_5_letter_words' -RedirectStandardInput $dictionaryFile -RedirectStandardOutput $fiveLetterWordsFile
 
 $allLatinFile = "$($dataDir)\five-letter-words-all-latin.txt"
-Start-Process $exe -ArgumentList 'remove_words_with_non_latin' -RedirectStandardInput $fiveLetterWordsFile -RedirectStandardOutput $allLatinFile
+Start-Process $exe -NoNewWindow -Wait -ArgumentList 'remove_words_with_non_latin' -RedirectStandardInput $fiveLetterWordsFile -RedirectStandardOutput $allLatinFile
 
 $upperFile = "$($dataDir)\five-letter-words-all-latin-upper.txt"
-Start-Process $exe -ArgumentList 'to_upper' -RedirectStandardInput $allLatinFile -RedirectSC:\Users\rober\OneDrive\datatandardOutput $upperFile
+Start-Process $exe -NoNewWindow -Wait -ArgumentList 'to_upper' -RedirectStandardInput $allLatinFile -RedirectStandardOutput $upperFile
 
 $sortedFile = "$($dataDir)\sorted.txt"
-Start-Process $exe -ArgumentList 'sort' -RedirectStandardInput $upperFile -RedirectStandardOutput $sortedFile
+Start-Process $exe -NoNewWindow -Wait -ArgumentList 'sort' -RedirectStandardInput $upperFile -RedirectStandardOutput $sortedFile
 
 $uniqueFile = "$($dataDir)\unique-five-letter-words-all-latin-upper.txt"
-Start-Process $exe -ArgumentList 'remove_duplicates' -RedirectStandardInput $sortedFile -RedirectStandardOutput $uniqueFile
+Start-Process $exe -NoNewWindow -Wait -ArgumentList 'remove_duplicates' -RedirectStandardInput $sortedFile -RedirectStandardOutput $uniqueFile
 
 $allDifferentFile = "$($dataDir)\all-different.txt"
-Start-Process $exe -ArgumentList 'all_different' -RedirectStandardInput $uniqueFile -RedirectStandardOutput $allDifferentFile
+Start-Process $exe -NoNewWindow -Wait -ArgumentList 'all_different' -RedirectStandardInput $uniqueFile -RedirectStandardOutput $allDifferentFile
