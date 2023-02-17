da3fd8f180c1ac78de2c280a5e60d40f4e5bb9d4
Robert Impey <robert-impey@users.noreply.github.com>
2023-02-17 20:49:31 +0000

    Py: Finding the max

--- Python/find_max.py
+++ Python/find_max.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
 
-def find_max(l):
-    if len(l) == 0:
+def find_max(the_list):
+    if len(the_list) == 0:
         return
 
-    max = l[0]
+    max_found = the_list[0]
 
-    for i in range(1, len(l)):
-        if l[i] > max:
-            max = l[i]
+    for i in range(1, len(the_list)):
+        if the_list[i] > max_found:
+            max_found = the_list[i]
 
-    return max
+    return max_found
+
+
+my_list = [1, 2, 3, 2]
+
+print(find_max(my_list))
