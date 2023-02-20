6b74398aeed729d006cde988bf0ea2789f40a7c4
Robert Impey <robert-impey@users.noreply.github.com>
2023-02-20 15:39:38 +0000

    EX: Map and lists

--- elixir/programming-elixir/ListsAndRecursion.exs
+++ elixir/programming-elixir/ListsAndRecursion.exs
@@ -7,4 +7,31 @@ defmodule MyList do
     IO.puts len([1, 2, 3])
     IO.puts len(["dogs", "cats"])
   end
+
+  def square([]), do: []
+  def square([head|tail]), do: [ head * head | square(tail) ]
+
+  def test_square() do
+    IO.puts square([])
+    IO.puts square([0, 1, 2, 3, 4, 5, 6, 7])
+  end
+
+  def add_1([]), do: []
+  def add_1([head|tail]), do: [ head + 1 | add_1(tail) ]
+
+  def test_add_1() do
+    IO.puts add_1([])
+    IO.puts add_1([0, 1, 2, 3, 4, 5, 6, 7])
+  end
+
+  def map([], _func), do: []
+  def map([head|tail], func), do: [ func.(head) | map(tail, func) ]
+
+  def test_map() do
+    IO.puts map([], &(&1 * &1))
+    IO.puts map([0, 1, 2, 3, 4, 5, 6, 7], &(&1 * &1))
+
+    IO.puts map([], &(&1 + 1))
+    IO.puts map([0, 1, 2, 3, 4, 5, 6, 7], &(&1 + 1))
+  end
 end
