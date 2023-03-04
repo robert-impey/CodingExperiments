3d535ead8dc11f7c15d8b0e8b2e9f00879edc448
Robert Impey <robert-impey@users.noreply.github.com>
2023-03-04 21:13:46 +0000

    EX: Recursive definition of odd

--- elixir/multiply.exs
+++ elixir/multiply.exs
@@ -1,4 +1,18 @@
 defmodule Multiply do
   def multiply(a, 1), do: a
   def multiply(a, n), do: a + multiply(a, n - 1)
+
+  def odd_(0, false), do: true
+  def odd_(0, true), do: false
+  def odd_(n, switch), do: odd_(n - 1, not switch)
+
+  def odd(n), do: odd_(n, true)
+
+  def test_odd() do
+    IO.puts(odd(0))
+    IO.puts(odd(1))
+    IO.puts(odd(2))
+    IO.puts(odd(37))
+    IO.puts(odd(100))
+  end
 end
