6de8e30e2bcaeeeff881a962e3ed4c33c4eb7f36
Robert Impey <robert-impey@users.noreply.github.com>
2023-03-09 23:49:11 +0000

    EX: List contains

---
+++ elixir/lists.exs
@@ -0,0 +1,14 @@
+defmodule MyLists do
+  def contains(_,[]), do: false
+  def contains(h, [h|_]), do: true
+  def contains(i, [_|t]), do: contains(i, t)
+
+  def test_contains() do
+    IO.puts(contains(1, []))
+    IO.puts(contains(1, [1]))
+    IO.puts(contains(1, [2]))
+    IO.puts(contains(1, [1,2]))
+    IO.puts(contains(1, [2,3]))
+    IO.puts(contains(1, [1,2,3]))
+    IO.puts(contains(1, [2,3,4]))  end
+end
