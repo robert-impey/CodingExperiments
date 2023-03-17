60b838a0fec176d908349ec5a23413c2d40f414d
Robert Impey <robert-impey@users.noreply.github.com>
2023-03-17 16:45:26 +0000

    EX: Keyword lists

---
+++ elixir/programming-elixir/keyword_lists.exs
@@ -0,0 +1,19 @@
+defmodule Canvas do
+  @defaults [ fg: "black", bg: "white", font: "Merriweather"]
+
+  def draw_text(text, options \\ []) do
+    options = Keyword.merge(@defaults, options)
+
+    IO.puts "Drawing text #{inspect(text)}"
+    IO.puts "Foreground: #{options[:fg]}"
+    IO.puts "Background: #{Keyword.get(options, :bg)}"
+    IO.puts "Font: #{Keyword.get(options, :font)}"
+    IO.puts "Pattern: #{Keyword.get(options, :pattern, "solid")}"
+    IO.puts "Style: #{inspect Keyword.get_values(options, :style)}"
+  end
+
+  def test_draw_text do
+    Canvas.draw_text("hello", fg: "red", style: "italic", style: "bold")
+    Canvas.draw_text("hello", fg: "blue", pattern: "dashed")
+  end
+end
