5616f375243ce21070ab2e80fcfd840e2cdb52c5
Robert Impey <robert-impey@users.noreply.github.com>
2023-03-25 12:27:17 +0000

    Ex: Map comprehension

---
+++ elixir/programming-elixir/people.exs
@@ -0,0 +1,17 @@
+defmodule People do
+  @people [
+    %{name: "Grumpy", height: 1.24},
+    %{name: "Rob", height: 1.83},
+    %{name: "Dopey", height: 1.32},
+    %{name: "Shaquille", height: 2.16},
+    %{name: "Sneezy", height: 1.28}
+  ]
+
+  def inspect_people do
+    IO.inspect(@people)
+  end
+
+  def inspect_tall_people do
+    IO.inspect(for person = %{height: height} <- @people, height > 1.5, do: person)
+  end
+end
