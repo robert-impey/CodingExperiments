f4d79ca1fdb25b59710d7873ade2e95c0409d57c
Robert Impey <robert-impey@users.noreply.github.com>
2023-06-30 22:24:00 +0100

    C#: Calculate magnitude of vector

---
+++ dotnet/C-Sharp/Vectors/Program.cs
@@ -0,0 +1,10 @@
+﻿// See Page 30 of "Linear Algebra for Dummies"
+// Calculate the magnitude of a vector.
+
+var xs = new[] { 3.0, 2.0, 4.0 };
+
+var sum = xs.Sum(x => x * x);
+
+var mag = Math.Sqrt(sum);
+
+Console.WriteLine(mag);
