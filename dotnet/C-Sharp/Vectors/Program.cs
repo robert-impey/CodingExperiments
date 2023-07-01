4624d0949bae1995d656fc1ae2f8df8b0a960739
Robert Impey <robert-impey@users.noreply.github.com>
2023-07-01 08:36:24 +0100

    C#: Sum of vectors

--- dotnet/C-Sharp/Vectors/Program.cs
+++ dotnet/C-Sharp/Vectors/Program.cs
@@ -8,3 +8,20 @@ var sum = xs.Sum(x => x * x);
 var mag = Math.Sqrt(sum);
 
 Console.WriteLine(mag);
+
+// Sum of Vectors (page 28)
+var a = new[] { 2.0, 4.0};
+var b = new[] { 3.0, 1.0};
+
+var sumOfAAndB = new double[a.Length];
+
+for (var i = 0; i < a.Length; i++)
+{
+    sumOfAAndB[i] += a[i] + b[i];
+}
+
+foreach (var element in sumOfAAndB)
+{
+    Console.WriteLine(element);
+}
+
