f5b0592393dde6a1d592df1db0b2100483e53b42
Robert Impey <robert-impey@users.noreply.github.com>
2023-07-03 22:09:54 +0100

    RB: Sum of vectors with Zip

--- ruby/lang/vectors.rb
+++ ruby/lang/vectors.rb
@@ -5,7 +5,7 @@
 
 xs = [3.0, 2.0, 4.0]
 
-xs.each { | x | puts x }
+xs.each { |x| puts x }
 
 sum_of_squares = 0.0
 
@@ -14,3 +14,16 @@ xs.each { |x| sum_of_squares += x * x }
 mag = Math.sqrt(sum_of_squares)
 
 puts mag
+
+# Sum of Vectors
+
+a = [2.0, 4.0]
+b = [3.0, 1.0]
+
+sum_of_a_and_b = []
+
+a.zip(b).each do |element_of_a, element_of_b|
+  sum_of_a_and_b.append(element_of_a + element_of_b)
+end
+
+sum_of_a_and_b.each { |element| puts element }
