6449d95286512b753b4964f8364a798ccd251d95
Robert Impey <robert-impey@users.noreply.github.com>
2023-07-05 17:12:39 +0100

    DT: Sum of vectors

--- dart/vectors.dart
+++ dart/vectors.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 void main() {
+    // Magnitude of a Vector
     var xs = [3.0, 2.0, 4.0];
 
     for (final x in xs) {
@@ -16,4 +17,17 @@ void main() {
     final mag = sqrt(sumOfSquares);
 
     print(mag);
+
+    // Sum of Vectors
+    var a = [2.0, 4.0];
+    var b = [3.0, 1.0];
+
+    var sum = [];
+    for (var i = 0; i < a.length; i++) {
+        sum.add(a[i] + b[i]);
+    }
+
+    for (final elementInSum in sum) {
+        print(elementInSum);
+    }
 }
