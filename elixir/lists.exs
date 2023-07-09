d7585f670f36906d3913f2bbeb0f750c05f29685
Robert Impey <robert-impey@users.noreply.github.com>
2023-07-09 19:47:50 +0100

    EX: More test cases for list contains

--- elixir/lists.exs
+++ elixir/lists.exs
@@ -4,12 +4,14 @@ defmodule MyLists do
   def contains(i, [_|t]), do: contains(i, t)
 
   def test_contains() do
-    IO.puts(contains(1, []))
-    IO.puts(contains(1, [1]))
-    IO.puts(contains(1, [2]))
-    IO.puts(contains(1, [1,2]))
-    IO.puts(contains(1, [2,3]))
-    IO.puts(contains(1, [1,2,3]))
-    IO.puts(contains(1, [2,3,4]))
+    IO.puts("contains(1, []) - #{contains(1, [])}")
+    IO.puts("contains(1, [1]) - #{contains(1, [1])}")
+    IO.puts("contains(1, [2]) - #{contains(1, [2])}")
+    IO.puts("contains(1, [1,2]) - #{contains(1, [1,2])}")
+    IO.puts("contains(1, [2,3]) - #{contains(1, [2,3])}")
+    IO.puts("contains(1, [1,2,3]) - #{contains(1, [1,2,3])}")
+    IO.puts("contains(2, [1,2,3]) - #{contains(2, [1,2,3])}")
+    IO.puts("contains(3, [1,2,3]) - #{contains(3, [1,2,3])}")
+    IO.puts("contains(1, [2,3,4]) - #{contains(1, [2,3,4])}")
   end
 end
