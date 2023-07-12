f1e6e3c2aca78dbf040376d2df49c00f187779a1
Robert Impey <robert-impey@users.noreply.github.com>
2023-07-12 23:06:32 +0100

    DT: Print a missing env var

--- dart/env_vars.dart
+++ dart/env_vars.dart
@@ -4,4 +4,12 @@ void main() {
   var localScripts = Platform.environment["LOCAL_SCRIPTS"];
 
   print(localScripts);
+
+  var dev = Platform.environment["DEV"];
+
+  if (dev == null) {
+    print("DEV not set!");
+  } else {
+    print(dev);
+  }
 }
