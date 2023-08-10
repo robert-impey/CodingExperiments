e6b10a99ccfde3b0a6a313a90d593ac54346e641
Robert Impey <robert-impey@users.noreply.github.com>
2023-08-09 21:13:24 -0400

    JV: Sum Tail Recursive

--- Java/Experiments/src/com/robertimpey/SumIf357.java
+++ Java/Experiments/src/com/robertimpey/SumIf357.java
@@ -6,7 +6,7 @@ public class SumIf357 {
         int[] ns = new int[] { 0, 1, 3, 5, 7, 10, 50, 100 };
 
         for (int n : ns) {
-            System.out.printf("n = %d, sum = %d\n", n, findSum(n));
+            System.out.printf("n = %d, sum = %d, sum TR = %d\n", n, findSum(n), findSumTr(n));
         }
     }
 
@@ -27,5 +27,26 @@ public class SumIf357 {
         }
 
         return findSumAcc(n - 1, acc);
+    }
+
+    private static int findSumTr(int n)
+    {
+        return findSumAccTr(n, 0);
+    }
+
+    private static int findSumAccTr(int n, int acc)
+    {
+        if (n <= 0) {
+            return acc;
+        }
+
+        int nextAcc = acc;
+
+        if (n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
+        {
+            nextAcc += n;
+        }
+
+        return findSumAcc(n - 1, nextAcc);
     }
 }
