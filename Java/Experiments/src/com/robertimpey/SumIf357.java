c1a0a13205e81501c812818577ae28ddc01e4a8d
Robert Impey <robert-impey@users.noreply.github.com>
2023-09-16 23:40:38 +0100

    JV: Reformat

--- Java/Experiments/src/com/robertimpey/SumIf357.java
+++ Java/Experiments/src/com/robertimpey/SumIf357.java
@@ -1,9 +1,8 @@
 package com.robertimpey;
 
 public class SumIf357 {
-    public static void main(String[] args)
-    {
-        int[] ns = new int[] { 0, 1, 3, 5, 7, 10, 50, 100 };
+    public static void main(String[] args) {
+        int[] ns = new int[]{0, 1, 3, 5, 7, 10, 50, 100};
 
         for (int n : ns) {
             System.out.printf("n = %d, sum = %d, sum TR = %d, sum WL = %d\n",
@@ -14,46 +13,39 @@ public class SumIf357 {
         }
     }
 
-    private static int findSum(int n)
-    {
+    private static int findSum(int n) {
         return findSumAcc(n, 0);
     }
 
-    private static int findSumAcc(int n, int acc)
-    {
+    private static int findSumAcc(int n, int acc) {
         if (n <= 0) {
             return acc;
         }
 
-        if (testN(n))
-        {
+        if (testN(n)) {
             return findSumAcc(n - 1, acc + n);
         }
 
         return findSumAcc(n - 1, acc);
     }
 
-    private static int findSumTr(int n)
-    {
+    private static int findSumTr(int n) {
         return findSumAccTr(n, 0);
     }
 
-    private static int findSumAccTr(int n, int acc)
-    {
+    private static int findSumAccTr(int n, int acc) {
         if (n <= 0) {
             return acc;
         }
 
-        if (testN(n))
-        {
+        if (testN(n)) {
             acc += n;
         }
 
         return findSumAccTr(n - 1, acc);
     }
 
-    private static int findSumWl(int n)
-    {
+    private static int findSumWl(int n) {
         int sum = 0;
         while (n > 0) {
             if (testN(n)) {
@@ -65,8 +57,7 @@ public class SumIf357 {
         return sum;
     }
 
-    private static boolean testN(int n)
-    {
+    private static boolean testN(int n) {
         return n % 3 == 0 || n % 5 == 0 || n % 7 == 0;
     }
 }
