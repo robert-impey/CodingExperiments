a9e3813727dbc1ed4de1bca0b443a6b6e9fa9658
Robert Impey <robert-impey@users.noreply.github.com>
2024-01-17 21:10:38 +0000

    DN: Async methods awaited

---
+++ dotnet/C-Sharp/HelloAsync/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        await Hello();
+        Goodbye();
+    }
+
+    async static Task Hello()
+    {
+        await Task.Delay(TimeSpan.FromMilliseconds(50));
+        Console.WriteLine("Hello");
+    }
+
+    static void Goodbye()
+    {
+        Console.WriteLine("Goodbye");
+    }
+}
