9e381ccce365879d93165d4dcc506254bdc3a9c0
Robert Impey <robert-impey@users.noreply.github.com>
2024-03-03 16:16:07 +0000

    C#: Count up and down

---
+++ dotnet/C-Sharp/CountUpCountDown/Program.cs
@@ -0,0 +1,30 @@
+﻿// See p. 139 of
+// Power Maths Year 3
+// Practice Book 3B
+
+var danny = 2.0;
+var dannyStep = 1.0 / 4.0;
+
+var aki = 4.0;
+var akiStep = 1.0 / 8.0;
+
+var steps = 0;
+
+while (danny <= aki)
+{
+    PrintState();
+
+    danny += dannyStep;
+    aki -= akiStep;
+
+    steps++;
+}
+
+Console.WriteLine("They have passed each other.");
+
+PrintState();
+
+void PrintState()
+{
+    Console.WriteLine($"After {steps} steps Danny is at {danny} and Aki is at {aki}.");
+}
