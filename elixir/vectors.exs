ce306eb92886766a6c0bd6655c88ec0c4f51b6df
Robert Impey <robert-impey@users.noreply.github.com>
2024-04-10 23:04:15 +0100

    EX: Using private functions

--- elixir/vectors.exs
+++ elixir/vectors.exs
@@ -11,30 +11,32 @@ defmodule MyVec do
     IO.puts(Enum.join(v, ", "))
   end
 
-  def add(a, b) when length(a) != length(b), do: {:error, [] }
-  def add(a, b), do: add_(a, b, [])
-  def add_([], [], v), do: {:ok, Enum.reverse v }
+  def add(a, b) when length(a) != length(b), do: {:error, []}
+  def add(a, b), do: add(a, b, [])
+  defp add([], [], v), do: {:ok, Enum.reverse(v)}
 
   # These should never match because of the when check above.
-  def add_(_a, [], _), do: {:error, [] }
-  def add_([], _b, _), do: {:error, [] }
+  defp add(_a, [], _), do: {:error, []}
+  defp add([], _b, _), do: {:error, []}
 
-  def add_([h1|t1], [h2|t2], v) do
-    add_(t1, t2, [h1 + h2 | v])
+  defp add([h1 | t1], [h2 | t2], v) do
+    add(t1, t2, [h1 + h2 | v])
+  end
+
+  def test_add() do
+    a = [2, 4]
+    b = Enum.map(a, &(&1 * 2))
+
+    MyVec.print(a)
+    MyVec.print(b)
+
+    c = [3, 5]
+    MyVec.print(c)
+
+    d = MyVec.add(a, c)
+    MyVec.print(d)
+
+    MyVec.print(MyVec.add([1, 2], [1, 2, 3]))
+    MyVec.print(MyVec.add([1, 2, 3], [1, 2]))
   end
 end
-
-a = [2, 4]
-b = Enum.map(a, &(&1 * 2))
-
-MyVec.print(a)
-MyVec.print(b)
-
-c = [3, 5]
-MyVec.print(c)
-
-d = MyVec.add(a, c)
-MyVec.print(d)
-
-MyVec.print(MyVec.add([1, 2], [1, 2, 3]))
-MyVec.print(MyVec.add([1, 2, 3], [1, 2]))
