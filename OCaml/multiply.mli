be7bc093ced0676bf4d7771d7533ddbd8dfe2870
Robert Impey <robert-impey@users.noreply.github.com>
2024-07-27 21:51:54 +0100

    OC: Interface file for multiply

---
+++ OCaml/multiply.mli
@@ -0,0 +1,6 @@
+val half : int -> int
+val odd : int -> bool
+val multiply0 : int -> int -> int
+val multiply1 : int -> int -> int
+val mult_acc0 : int -> int -> int -> int
+val mult_acc1 : int -> int -> int -> int
