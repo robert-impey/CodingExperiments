76cca148283e02a82532b6838cbf864f24e51922
Robert Impey <robert-impey@users.noreply.github.com>
2024-08-07 01:08:01 +0200

    FS: Added a test for reading an XML file

--- F-Sharp/FSharp.Data.Exp/FSharp.Data.Exp/ReadLastFm.fsx
+++
@@ -1,7 +0,0 @@
-﻿#r @"bin\Debug\FSharp.Data.dll"
-
-open FSharp.Data
-
-type LastFm = HtmlProvider<"http://www.last.fm/user/robert_impey">
-
-let lastFm = LastFm()
---
+++ dotnet/F-Sharp/FSharp.Data.Exp/Program.fs
@@ -0,0 +1,17 @@
+﻿open FSharp.Data
+open System
+
+let dataDir = Environment.GetEnvironmentVariable("DATA")
+
+type BaltimoreRestaurants = XmlProvider<"BaltimoreRestaurantsSample.xml">
+
+let restaurantsFile = IO.Path.Join(dataDir, "BaltimoreRestaurants.xml")
+
+let restaurants = BaltimoreRestaurants.Load(restaurantsFile)
+
+let names = restaurants.Rows |> Array.map (fun r -> r.Name)
+
+for n in names do
+  printfn "Name: %s" n
+
+printfn "There are %d restaurants in the list" restaurants.Rows.Length
