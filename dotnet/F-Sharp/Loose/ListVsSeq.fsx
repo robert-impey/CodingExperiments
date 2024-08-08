144e3443e1220c33a9e510633c950fa6f6a7d6f1
Robert Impey <robert-impey@users.noreply.github.com>
2024-08-08 18:18:23 +0100

    F#: Comparing a list to a seq

--- dotnet/F-Sharp/Loose/ListVsSeq.fsx
+++ dotnet/F-Sharp/Loose/ListVsSeq.fsx
@@ -1,12 +1,11 @@
 // Comparing a list to a seq
-// Robert Impey 2013-08-21
 
-let max = pown 10 3
+let max = pown 10 2
 
 let p x = printfn "%d" x 
 
 let firstUpToMaxList = [1..max]
-List.iter (printfn "%d") firstUpToMaxList
+firstUpToMaxList |> List.iter p
 
 let firstUpToMaxSeq = seq { for i in 1 .. max do yield i }
-Seq.iter p firstUpToMaxSeq
+firstUpToMaxSeq |> Seq.iter p
