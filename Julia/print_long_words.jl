d1c645e350dd90aaf99604d395c65526f89a844e
Robert Impey <robert-impey@users.noreply.github.com>
2025-01-10 18:39:02 +0000

    JL: Count long words

---
+++ Julia/print_long_words.jl
@@ -0,0 +1,36 @@
+function find_long_words(dictionary_file :: String)
+    long_words = []
+    
+    # Read the dictionary file
+    fin = open(dictionary_file, "r") do fin
+
+
+        for line in eachline(fin)
+            if length(line) > 20
+                push!(long_words, line)
+            end
+        end
+    end
+
+    return long_words
+end
+
+function print_long_words(dictionary_file :: String)
+    long_words = find_long_words(dictionary_file)
+
+    for word in long_words
+        println(word)
+    end
+
+    println("-----")
+
+    println("Number of long words: ", length(long_words))
+end
+
+data_dir = abspath(ENV["DATA"])
+
+fn = "british-english.txt"
+
+dict_file = joinpath(data_dir, fn)
+
+print_long_words(dict_file)
