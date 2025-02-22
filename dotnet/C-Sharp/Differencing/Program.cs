fc1c60b48fc322da4cf37afaf57287ff0a93f101
Robert Impey <robert-impey@users.noreply.github.com>
2025-02-22 17:34:56 +0000

    C#: Differencing in list using indices

---
+++ dotnet/C-Sharp/Differencing/Program.cs
@@ -0,0 +1,6 @@
+﻿var data = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+for (var i = 1; i < data.Count; i++)
+{
+    Console.WriteLine($"{data[i]} - {data[i-1]} = {data[i] - data[i - 1]}");
+}
