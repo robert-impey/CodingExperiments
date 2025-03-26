6a6e4d99d9d56351623c6e0ce82a492137de4caf
Robert Impey <robert-impey@users.noreply.github.com>
2025-03-26 11:22:42 +0000

    C#: Index based palindrome checker

---
+++ dotnet/C-Sharp/PalindromeChecker/PalindromeChecker.Lib/IndexPalindromeChecker.cs
@@ -0,0 +1,19 @@
+﻿namespace PalindromeChecker.Lib;
+
+public class IndexPalindromeChecker : IPalindromeChecker
+{
+    public bool IsPalindrome(string possiblePalindrome)
+    {
+        if (string.IsNullOrWhiteSpace(possiblePalindrome))
+            return false;
+
+        var i = 0;
+        for (; i < possiblePalindrome.Length / 2; i++)
+        {
+            if (possiblePalindrome[i] != possiblePalindrome[possiblePalindrome.Length - i - 1])
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ dotnet/C-Sharp/PalindromeChecker/PalindromeChecker.Tests/IndexPalindromeCheckerTest.cs
@@ -0,0 +1,12 @@
+﻿using PalindromeChecker.Lib;
+
+namespace PalindromeChecker.Tests;
+
+public class IndexPalindromeCheckerTest
+{
+    [Fact]
+    public void TestPalindromeChecker()
+    {
+        PalindromeCheckerTestHelper.TestPalindromeChecker(new IndexPalindromeChecker());
+    }
+}
