5858250238fa3a1f3f2357f3fbabba9ab1c0bd5a
Robert Impey <robert-impey@users.noreply.github.com>
2025-05-04 21:11:52 +0100

    JL: Find unique all Latin words ignoring case

--- Julia/words_matching_pred.jl
+++ Julia/words_matching_pred.jl
@@ -27,22 +27,29 @@ function print_long_words(dict)
     println("Number of long words: ", length(long_words))
 end
 
-function main()
-    println("-----")
-
-    println("Finding Words with 5 characters:")
-    println("-----")
-    five_letter_words = filter(x -> 5 == length(x), dict)
-    println("Number of words with 5 letters: ", length(five_letter_words))
-end
-
 data_dir = abspath(ENV["DATA"])
 
 fn = "british-english.txt"
 
 dict_file = joinpath(data_dir, fn)
 
-dict = read_dictionary(dict_file)
+function main()
+    dict = read_dictionary(dict_file)
+
+    println("-----")
+
+    println("Finding Words with 5 characters:")
+    println("-----")
+    five_letter_words = filter(x -> 5 == length(x), dict)
+    println("Number of words with 5 letters: ", length(five_letter_words))
+
+    all_latin = filter(x -> occursin(r"^[A-Za-z]{5}$", x), five_letter_words)
+    println("Number of 5 letter words that are all Latin: ", length(all_latin))
+
+    all_latin_uppercase = unique(map(uppercase, all_latin))
+    println("Number of unique 5 letter words that are all Latin ignoring case: ",
+        length(all_latin_uppercase))
+end
 
 if abspath(PROGRAM_FILE) == @__FILE__
     main()
