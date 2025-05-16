ac8e8fb8ed70517616ff22ec838695eb83eec8c6
Robert Impey <robert-impey@users.noreply.github.com>
2025-05-16 11:21:33 +0100

    JV: FL: Find the five letter words

--- Java/Experiments/src/com/robertimpey/FiveLetters.java
+++ Java/Experiments/src/com/robertimpey/FiveLetters.java
@@ -1,11 +1,12 @@
 package com.robertimpey;
 
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FiveLetters {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String dataEnvVar = "DATA";
         String dataDirectory = System.getenv(dataEnvVar);
 
@@ -18,11 +19,43 @@ public class FiveLetters {
 
             if (Files.exists(dictionaryPath)) {
                 System.out.printf("Data dictionary file %s exists\n", dictionaryPath);
+
+                Path outputRelativePath = Paths.get("FiveLetters/Java");
+                Path outputPath = dataPath.resolve(outputRelativePath);
+
+                if (!Files.exists(outputPath)) {
+                    Files.createDirectory(outputPath);
+                }
+
+                fivel(dictionaryPath, outputPath);
             } else {
                 System.out.printf("Data dictionary file %s does not exist\n", dictionaryPath);
             }
         } else {
             System.out.printf("Environment variable '%s' is not set.", dataEnvVar);
         }
+    }
+
+    public static void fivel(Path dictionaryPath, Path outputPath) throws IOException {
+        Path fiveLetterWordsPath = outputPath.resolve(Paths.get("five-letter-words.txt"));
+
+        if (!Files.exists(fiveLetterWordsPath)) {
+            find5LetterWords(dictionaryPath, fiveLetterWordsPath);
+        }
+    }
+
+    private static void find5LetterWords(Path dictionaryPath, Path fiveLetterWordsPath) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(dictionaryPath.toFile())));
+        FileWriter writer = new FileWriter(fiveLetterWordsPath.toFile());
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (5 == line.length()) {
+                writer.write(line);
+                writer.write(System.lineSeparator());
+            }
+        }
+
+        writer.close();
     }
 }
