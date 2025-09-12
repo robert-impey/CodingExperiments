f74b17bd73c9c2ae1a243532a06bc59bb05059d7
Robert Impey <robert-impey@users.noreply.github.com>
2025-09-12 22:36:38 +0100

    DN: Updated framework targets

--- dotnet/C-Sharp/RabbitMQ/PubSub/EmitLog/Program.cs
+++ dotnet/C-Sharp/RabbitMQ/PubSub/EmitLog/Program.cs
@@ -1,23 +1,25 @@
 ﻿using RabbitMQ.Client;
 using System;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace EmitLog
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
 
-            using var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
+            await using var connection = await factory.CreateConnectionAsync();
+            await using var channel = await connection.CreateChannelAsync();
 
-            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+            await channel.ExchangeDeclareAsync(exchange: "logs", type: ExchangeType.Fanout);
 
             var message = GetMessage(args);
             var body = Encoding.UTF8.GetBytes(message);
-            channel.BasicPublish(exchange: "logs",
+            await channel.BasicPublishAsync(
+                exchange: "logs",
                 routingKey: "",
                 basicProperties: null,
                 body: body);
