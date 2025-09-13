23fbc1dadea137d45bcc983c46a8637ab8199781
Robert Impey <robert-impey@users.noreply.github.com>
2025-09-13 22:29:32 +0100

    CS: Fixed the rabbit demo for workers and tasks

--- dotnet/C-Sharp/RabbitMQ/WorkQueues/NewTask/Program.cs
+++ dotnet/C-Sharp/RabbitMQ/WorkQueues/NewTask/Program.cs
@@ -5,38 +5,39 @@ namespace NewTask
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (IConnection connection = factory.CreateConnection())
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            await using var connection = await factory.CreateConnectionAsync();
+            await using var channel = await connection.CreateChannelAsync();
+            await channel.QueueDeclareAsync(
+                queue: "task_queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var message = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            var properties = new BasicProperties
             {
-                using (IModel channel = connection.CreateModel())
-                {
-                    channel.QueueDeclare(queue: "task_queue",
-                        durable: true,
-                        exclusive: false,
-                        autoDelete: false,
-                        arguments: null);
+                Persistent = true
+            };
 
-                    var message = GetMessage(args);
-                    var body = Encoding.UTF8.GetBytes(message);
+            //await channel.BasicQosAsync(0, 1, false);
 
-                    IBasicProperties properties = channel.CreateBasicProperties();
-                    properties.Persistent = true;
-
-                    channel.BasicQos(0, 1, false);
-
-                    channel.BasicPublish(exchange: "",
-                        routingKey: "task_queue",
-                        basicProperties: properties,
-                        body: body);
-                }
-            }
+            await channel.BasicPublishAsync(
+                exchange: string.Empty,
+                routingKey: "task_queue",
+                mandatory: true,
+                basicProperties: properties,
+                body: body);
         }
 
         private static string GetMessage(string[] args)
         {
-            return (args.Length > 0) ? string.Join(" ", args) : "red 4";
+            return args.Length > 0 ? string.Join(" ", args) : "Red 3";
         }
     }
 }
--- dotnet/C-Sharp/RabbitMQ/WorkQueues/Worker/Program.cs
+++ dotnet/C-Sharp/RabbitMQ/WorkQueues/Worker/Program.cs
@@ -1,68 +1,56 @@
 ﻿using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
-using System;
 using System.Text;
-using System.Threading;
+using RabbitMQ.Client.Events;
 
-namespace Worker
+var factory = new ConnectionFactory { HostName = "localhost" };
+await using var connection = await factory.CreateConnectionAsync();
+await using var channel = await connection.CreateChannelAsync();
+await channel.QueueDeclareAsync(
+    queue: "task_queue",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.ReceivedAsync += async (model, ea) =>
 {
-    internal class Program
+    var body = ea.Body;
+    var message = Encoding.UTF8.GetString(body.ToArray());
+    Console.WriteLine(" [x] Received {0}", message);
+
+    var parts = message.Split(" ");
+
+    if (parts.Length == 2)
     {
-        private static void Main(string[] args)
+        Console.WriteLine(parts[0]);
+
+        if (int.TryParse(parts[1], out var work))
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (IConnection connection = factory.CreateConnection())
+            for (var i = 0; i < work; i++)
             {
-                using (IModel channel = connection.CreateModel())
-                {
-                    channel.QueueDeclare(queue: "task_queue",
-                        durable: true,
-                        exclusive: false,
-                        autoDelete: false,
-                        arguments: null);
-                    var consumer = new EventingBasicConsumer(channel);
-                    consumer.Received += (model, ea) =>
-                    {
-                        var body = ea.Body;
-                        var message = Encoding.UTF8.GetString(body.ToArray());
-                        Console.WriteLine(" [x] Received {0}", message);
-
-                        var parts = message.Split(" ");
-
-                        if (parts.Length == 2)
-                        {
-                            Console.WriteLine(parts[0]);
-
-                            if (int.TryParse(parts[1], out var work))
-                            {
-                                for (var i = 0; i < work; i++)
-                                {
-                                    Console.Write(".");
-                                    Thread.Sleep(TimeSpan.FromSeconds(1));
-                                }
-
-                                Console.WriteLine();
-                            }
-                            else
-                            {
-                                Console.WriteLine($"Unable to parse '{parts[1]}' to an int.");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Unable to split '{message}'.");
-                        }
-
-                        Console.WriteLine(" [x] Done");
-
-                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
-                    };
-                    channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
-
-                    Console.WriteLine(" Press [enter] to exit.");
-                    Console.ReadLine();
-                }
+                Console.Write(".");
+                Thread.Sleep(TimeSpan.FromSeconds(1));
             }
+
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine($"Unable to parse '{parts[1]}' to an int.");
         }
     }
-}
+    else
+    {
+        Console.WriteLine($"Unable to split '{message}'.");
+    }
+
+    Console.WriteLine(" [x] Done");
+
+    await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+
+await channel.BasicConsumeAsync(queue: "task_queue", autoAck: false, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
