8a7cc668cd8b5f2907b188b1066725b92bab7c89
Robert Impey <robert-impey@users.noreply.github.com>
2025-09-13 18:24:23 +0100

    C#: Fixed the Send/Receive RabbitMQ async demo

--- dotnet/C-Sharp/RabbitMQ/SendReceive/Receive/Program.cs
+++ dotnet/C-Sharp/RabbitMQ/SendReceive/Receive/Program.cs
@@ -2,36 +2,29 @@
 using RabbitMQ.Client.Events;
 using System.Text;
 
-namespace Receive
+var factory = new ConnectionFactory { HostName = "localhost" };
+await using var connection = await factory.CreateConnectionAsync();
+await using var channel = await connection.CreateChannelAsync();
+await channel.QueueDeclareAsync(
+    queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += (model, ea) =>
 {
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (IConnection connection = factory.CreateConnection())
-            {
-                using (IModel channel = connection.CreateModel())
-                {
-                    channel.QueueDeclare(queue: "hello",
-                        durable: false,
-                        exclusive: false,
-                        autoDelete: false,
-                        arguments: null);
-                    var consumer = new EventingBasicConsumer(channel);
-                    consumer.Received += (model, ea) =>
-                    {
-                        var body = ea.Body;
-                        var message = Encoding.UTF8.GetString(body.ToArray());
-                        Console.WriteLine(" [x] Received {0}", message);
-                    };
-                    channel.BasicConsume(queue: "hello",
-                        autoAck: true,
-                        consumer: consumer);
-                    Console.WriteLine(" Press [enter] to exit.");
-                    Console.ReadLine();
-                }
-            }
-        }
-    }
-}
+    var body = ea.Body;
+    var message = Encoding.UTF8.GetString(body.ToArray());
+    Console.WriteLine(" [x] Received {0}", message);
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(
+    queue: "hello",
+    autoAck: true,
+    consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
--- dotnet/C-Sharp/RabbitMQ/SendReceive/Send/Program.cs
+++ dotnet/C-Sharp/RabbitMQ/SendReceive/Send/Program.cs
@@ -1,31 +1,18 @@
 ﻿using RabbitMQ.Client;
 using System.Text;
 
-namespace Send
-{
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (IConnection connection = factory.CreateConnection())
-            {
-                using (IModel channel = connection.CreateModel())
-                {
-                    channel.QueueDeclare(queue: "hello",
-                        durable: false,
-                        exclusive: false,
-                        autoDelete: false,
-                        arguments: null);
-                    var message = "Hello World!";
-                    var body = Encoding.UTF8.GetBytes(message);
-                    channel.BasicPublish(exchange: "",
-                        routingKey: "hello",
-                        basicProperties: null,
-                        body: body);
-                    Console.WriteLine(" [x] Sent {0}", message);
-                }
-            }
-        }
-    }
-}
+var factory = new ConnectionFactory { HostName = "localhost" };
+await using var connection = await factory.CreateConnectionAsync();
+await using var channel = await connection.CreateChannelAsync();
+await channel.QueueDeclareAsync(queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+const string message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+await channel.BasicPublishAsync(
+    exchange: string.Empty,
+    routingKey: "hello",
+    body: body);
+Console.WriteLine(" [x] Sent {0}", message);
