f675611cea208f7e23c903ffaaea8aee31826e78
Robert Impey <robert-impey@users.noreply.github.com>
2025-09-16 11:12:18 +0100

    C#: Building an array from a permutation

---
+++ dotnet/C-Sharp/BuildArrayFromPermutation.Test/SolutionTest.cs
@@ -0,0 +1,30 @@
+﻿using Shouldly;
+
+namespace BuildArrayFromPermutation.Test;
+
+public class SolutionTest
+{
+    [Fact]
+    public void Ex1()
+    {
+        var input = new[] { 0, 2, 1, 5, 3, 4 };
+        var expected = new[] { 0, 1, 2, 4, 5, 3 };
+
+        var solution = new Solution();
+        var output = solution.BuildArray(input);
+
+        output.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void Ex2()
+    {
+        var input = new[] { 5, 0, 1, 2, 3, 4 };
+        var expected = new[] { 4, 5, 0, 1, 2, 3 };
+
+        var solution = new Solution();
+        var output = solution.BuildArray(input);
+
+        output.ShouldBe(expected);
+    }
+}
