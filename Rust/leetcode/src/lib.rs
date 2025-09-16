47b5829f782c7c29735bcd48dcac72e71517af63
Robert Impey <robert-impey@users.noreply.github.com>
2025-09-16 14:12:24 +0100

    Rs: Max achievable number

---
+++ Rust/leetcode/src/lib.rs
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/find-the-maximum-achievable-number/?envType=problem-list-v2&envId=vpmy63kg
+fn the_maximum_achievable_x(num: i32, t: i32) -> i32 {
+    num + (2 * t)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn the_maximum_achievable_x_ex1() {
+        let result = the_maximum_achievable_x(4, 1);
+        assert_eq!(result, 6);
+    }
+
+    #[test]
+    fn the_maximum_achievable_x_ex2() {
+        let result = the_maximum_achievable_x(3, 2);
+        assert_eq!(result, 7);
+    }
+}
