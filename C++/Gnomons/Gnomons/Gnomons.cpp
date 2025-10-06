f1ba6b5c1a8447a2607cfeb9eed718a0983b7ba8
Robert Impey <robert-impey@users.noreply.github.com>
2025-10-06 15:58:37 +0100

    C++: Gnomons: User input

--- C++/Gnomons/Gnomons/Gnomons.cpp
+++ C++/Gnomons/Gnomons/Gnomons.cpp
@@ -3,10 +3,31 @@
 
 #include "Gnomons.h"
 
+#include <iostream>
+#include <string>
+
 using namespace std;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        cerr << "Please set the max!" << endl;
+    }
+    else {
+        string input(argv[1]);
+
+        try {
+            auto max = stoi(input);
+
+            if (max < 1) {
+                cerr << "Please set an integer greater than 0!" << endl;
+            }
+
+            for (auto i = 1; i <= max; i++) {
+                cout << i << endl;
+            }
+        }
+        catch (const std::invalid_argument& ia) {
+            cerr << "Unable to parse input - \"" << input << "\"!" << endl;
+        }
+    }
 }
