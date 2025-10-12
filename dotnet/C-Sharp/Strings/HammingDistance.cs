70e4956cb81d13397305df4c5506878190339fc8
Robert Impey <robert-impey@users.noreply.github.com>
2025-10-12 09:37:01 +0100

    C#: Hamming Distance

---
+++ dotnet/C-Sharp/Strings/HammingDistance.cs
@@ -0,0 +1,29 @@
+﻿namespace Strings;
+
+public static class HammingDistance
+{
+    // Pascal Style
+    // That is, no jumps
+    public static int? Distance(string a, string b)
+    {
+        int? distance;
+
+        if (a.Length == b.Length && a.Length > 0)
+        {
+            distance = 0;
+            for (var i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i])
+                {
+                    distance++;
+                }
+            }
+        }
+        else
+        {
+            distance = null;
+        }
+        
+        return distance;
+    }
+}
