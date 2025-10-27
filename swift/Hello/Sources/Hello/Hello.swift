e30b5a7343370dfdf3ccd7c2a673e02fe1c8b9b3
Robert Impey <robert-impey@users.noreply.github.com>
2025-10-27 22:11:10 +0000

    Sw: Mv HW from tour to CLI app

---
+++ swift/Hello/Sources/Hello/Hello.swift
@@ -0,0 +1,53 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct Hello {
+    static func main() {
+        // See https://docs.swift.org/swift-book/GuidedTour/GuidedTour.html#
+
+        print("Hello, world!")
+
+        let float4: Float = 4
+
+        print(float4)
+
+        let label = "The width is "
+        let width = 94
+        let widthLabel = label + String(width)
+        print(widthLabel)
+
+        let apples = 3
+        let oranges = 5
+        let appleSummary = "I have \(apples) apples."
+        let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+        print(appleSummary)
+        print(fruitSummary)
+
+        let quotation = """
+            I said "I have \(apples) apples."
+            Then I said "I have \(apples + oranges) pieces of fruit."
+            """
+        print(quotation)
+
+        var shoppingList = ["catfish", "water", "tulips"]
+        print(shoppingList[1])
+        shoppingList[1] = "bottle of water"
+        print(shoppingList[1])
+
+        shoppingList.append("blue paint")
+        print(shoppingList)
+
+        var occupations = [
+            "Malcolm": "Captain",
+            "Kaylee": "Mechanic",
+        ]
+        occupations["Jayne"] = "Public Relations"
+        print(occupations)
+
+        let emptyArray = [String]()
+        let emptyDictionary = [String: Float]()
+        print(emptyArray)
+        print(emptyDictionary)
+    }
+}
