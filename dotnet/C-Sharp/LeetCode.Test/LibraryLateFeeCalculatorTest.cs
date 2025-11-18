01250d8c24db5e6dabd70de4d9c30d5b06dd4f7b
Robert Impey <robert-impey@users.noreply.github.com>
2025-11-18 12:29:02 +0000

    C#: LC: Library Late Fee

---
+++ dotnet/C-Sharp/LeetCode.Test/LibraryLateFeeCalculatorTest.cs
@@ -0,0 +1,30 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class LibraryLateFeeCalculatorTest
+{
+    [Fact]
+    public void Ex1()
+    {
+        var input = new[] { 5, 1, 7};
+        var expected = 32;
+
+        var solution = new LibraryLateFeeCalculator();
+        var output = solution.LateFee(input);
+
+        output.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void Ex2()
+    {
+        var input = new[] { 1, 1 };
+        var expected = 2;
+
+        var solution = new LibraryLateFeeCalculator();
+        var output = solution.LateFee(input);
+
+        output.ShouldBe(expected);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/LibraryLateFeeCalculator.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode;
+
+public class LibraryLateFeeCalculator
+{
+    public int LateFee(int[] daysLate)
+    {
+        var lateFee = 0;
+
+        foreach (var days in daysLate)
+        {
+            if (days == 1)
+            {
+                lateFee += 1;
+            }
+            else if (days <= 5)
+            {
+                lateFee += 2 * days;
+            }
+            else if (days > 5)
+            {
+                lateFee += 3 * days;
+            }
+        }
+
+        return lateFee;
+    }
+}
