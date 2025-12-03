e45e883392da19aea0c9d42c12afd71423564859
Robert Impey <robert-impey@users.noreply.github.com>
2025-12-03 15:37:43 +0000

    C#: Min ops for array sum to be divisible by K

---
+++ dotnet/C-Sharp/LeetCode.Test/MinOpArySumDivKTest.cs
@@ -0,0 +1,45 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class MinOpArySumDivKTest
+{
+    [Fact]
+    public void Ex1()
+    {
+        var input = new[] { 3, 9, 7};
+        var k = 5;
+        var expected = 4;
+
+        var solution = new MinOpArySumDivK();
+        var output = solution.MinOperations(input, k);
+
+        output.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void Ex2()
+    {
+        var input = new[] { 4, 1, 3 };
+        var k = 4;
+        var expected = 0;
+
+        var solution = new MinOpArySumDivK();
+        var output = solution.MinOperations(input, k);
+
+        output.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void Ex3()
+    {
+        var input = new[] { 3, 2 };
+        var k = 6;
+        var expected = 5;
+
+        var solution = new MinOpArySumDivK();
+        var output = solution.MinOperations(input, k);
+
+        output.ShouldBe(expected);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/MinOpArySumDivK.cs
@@ -0,0 +1,8 @@
+﻿namespace LeetCode;
+
+// https://leetcode.com/problems/minimum-operations-to-make-array-sum-divisible-by-k
+
+public class MinOpArySumDivK
+{
+    public int MinOperations(int[] nums, int k) => nums.Sum() % k;
+}
