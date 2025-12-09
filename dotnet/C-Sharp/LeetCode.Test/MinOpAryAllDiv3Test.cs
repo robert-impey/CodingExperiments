a4f4ec68a97b44f11eeac23bdaeb51ccb23f2a1f
Robert Impey <robert-impey@users.noreply.github.com>
2025-12-09 10:57:03 +0000

    C#: Min ops to make all array div 3

---
+++ dotnet/C-Sharp/LeetCode.Test/MinOpAryAllDiv3Test.cs
@@ -0,0 +1,30 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class MinOpAryAllDiv3Test
+{
+    [Fact]
+    public void Ex1()
+    {
+        var nums = new[] { 1, 2, 3, 4 };
+        var expected = 3;
+
+        var solution = new MinOpAryAllDiv3();
+        var output = solution.MinimumOperations(nums);
+
+        output.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void Ex2()
+    {
+        var nums = new[] { 3, 6, 9 };
+        var expected = 0;
+
+        var solution = new MinOpAryAllDiv3();
+        var output = solution.MinimumOperations(nums);
+
+        output.ShouldBe(expected);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/MinOpAryAllDiv3.cs
@@ -0,0 +1,8 @@
+﻿namespace LeetCode;
+
+// https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three
+
+public class MinOpAryAllDiv3
+{
+    public int MinimumOperations(int[] nums) => nums.Sum(x => x % 3 == 0 ? 0 : 1);
+}
