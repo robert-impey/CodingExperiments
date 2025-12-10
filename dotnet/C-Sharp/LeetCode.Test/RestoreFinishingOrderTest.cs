87260dc9b66ae5e1967824bc6dbc5456d10a1660
Robert Impey <robert-impey@users.noreply.github.com>
2025-12-10 17:00:50 +0000

    C#: LT: Recover Finishing Order

---
+++ dotnet/C-Sharp/LeetCode.Test/RestoreFinishingOrderTest.cs
@@ -0,0 +1,32 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class RestoreFinishingOrderTest
+{
+    [Fact]
+    public void Ex1()
+    {
+        var order = new[] { 3, 1, 2, 5, 4 };
+        var friends = new[] { 1, 3, 4 };
+        var expected = new[] { 3, 1, 4 };
+
+        var solution = new RestoreFinishingOrder();
+        var output = solution.RecoverOrder(order, friends);
+
+        output.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void Ex2()
+    {
+        var order = new[] { 1, 4, 5, 3, 2 };
+        var friends = new[] { 2, 5 };
+        var expected = new[] { 5, 2 };
+
+        var solution = new RestoreFinishingOrder();
+        var output = solution.RecoverOrder(order, friends);
+
+        output.ShouldBe(expected);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/RestoreFinishingOrder.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode;
+
+// https://leetcode.com/problems/restore-finishing-order/
+
+public class RestoreFinishingOrder
+{
+    public int[] RecoverOrder(int[] order, int[] friends)
+    {
+        var recovered = new int[friends.Length];
+
+        var counter = 0;
+
+        for (var i = 0; i < order.Length; i++)
+        {
+            if (counter >= friends.Length)
+            {
+                break;
+            }
+
+            for (var j = 0; j < friends.Length; j++)
+            {
+                if (order[i] == friends[j])
+                {
+                    recovered[counter] = friends[j];
+                    counter++;
+                    break;
+                }
+            }
+        }
+
+        return recovered;
+    }
+}
