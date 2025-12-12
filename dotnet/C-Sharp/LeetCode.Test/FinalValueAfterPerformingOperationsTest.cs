e3cbf857d0cc7835d1495a32852c1c8b620c8e6f
Robert Impey <robert-impey@users.noreply.github.com>
2025-12-12 14:31:30 +0000

    C#: LT: Final value after applying operations

---
+++ dotnet/C-Sharp/LeetCode.Test/FinalValueAfterPerformingOperationsTest.cs
@@ -0,0 +1,30 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class FinalValueAfterPerformingOperationsTest
+{
+    [Fact]
+    public void Ex1()
+    {
+        var oparations = new[] { "--X", "X++", "X++" };
+        var expected = 1;
+
+        var solution = new FinalValueAfterPerformingOperations();
+        var output = solution.FinalValueAfterOperations(oparations);
+
+        output.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void Ex2()
+    {
+        var oparations = new[] { "++X", "++X", "X++" };
+        var expected = 3;
+
+        var solution = new FinalValueAfterPerformingOperations();
+        var output = solution.FinalValueAfterOperations(oparations);
+
+        output.ShouldBe(expected);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/FinalValueAfterPerformingOperations.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode;
+
+// https://leetcode.com/problems/final-value-of-variable-after-performing-operations
+
+public class FinalValueAfterPerformingOperations
+{
+    public int FinalValueAfterOperations(string[] operations)
+    {
+        var x = 0;
+
+        for (var i = 0; i < operations.Length; i++)
+        {
+            switch (operations[i])
+            {
+                case "++X":
+                case "X++":
+                    x++;
+                    break;
+                case "--X":
+                case "X--":
+                    x--;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        return x;
+    }
+}
