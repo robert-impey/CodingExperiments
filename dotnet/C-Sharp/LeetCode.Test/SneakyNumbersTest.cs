de6cf677022f90bebc08e6b2a6a36d618214848e
Robert Impey <robert-impey@users.noreply.github.com>
2026-01-08 10:28:51 +0000

    C#: Sneaky Numbers

---
+++ dotnet/C-Sharp/LeetCode.Test/SneakyNumbersTest.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Test;
+
+public class SneakyNumbersTest
+{
+    [Fact]
+    public void Ex1()
+    {
+        var input = new[] { 0, 1, 1, 0 };
+        var expected = new[] { 0, 1 };
+
+        var solution = new SneakyNumbers();
+        var output = solution.GetSneakyNumbers(input);
+
+        ListHelpers.OutcomeListMatches(expected, output);
+    }
+
+
+    [Fact]
+    public void Ex2()
+    {
+        var input = new[] { 0, 3, 2, 1, 3, 2 };
+        var expected = new[] { 2, 3 };
+
+        var solution = new SneakyNumbers();
+        var output = solution.GetSneakyNumbers(input);
+
+        ListHelpers.OutcomeListMatches(expected, output);
+    }
+
+
+    [Fact]
+    public void Ex3()
+    {
+        var input = new[] { 7, 1, 5, 4, 3, 4, 6, 0, 9, 5, 8, 2 };
+        var expected = new[] { 4, 5 };
+
+        var solution = new SneakyNumbers();
+        var output = solution.GetSneakyNumbers(input);
+
+        ListHelpers.OutcomeListMatches(expected, output);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/SneakyNumbers.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode;
+
+public class SneakyNumbers
+{
+    private const int MaxValue = 100;
+    private const int MaxSneaky = 2;
+
+    public int[] GetSneakyNumbers(int[] nums)
+    {
+        var sneaky = new int[MaxSneaky];
+
+        var foundDuplicates = 0;
+
+        var seen = new int[MaxValue];
+
+        foreach (var num in nums)
+        {
+            seen[num]++;
+            if (seen[num] == 2)
+            {
+                sneaky[foundDuplicates] = num;
+                foundDuplicates++;
+                if (foundDuplicates == MaxSneaky)
+                {
+                    break;
+                }
+            }
+        }
+
+        return sneaky;
+    }
+}
