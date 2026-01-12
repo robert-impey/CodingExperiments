4efd6202911897546d90d3ddf14dbf00f9c03663
Robert Impey <robert-impey@users.noreply.github.com>
2026-01-12 09:56:00 +0000

    C#: LT: Transform Array by Parity

---
+++ dotnet/C-Sharp/LeetCode.Test/TransformArrayByParityTest.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Test;
+
+public class TransformArrayByParityTest
+{
+    [Fact]
+    public void Ex1()
+    {
+        var input = new[] { 4, 3, 2, 1 };
+        var expected = new[] { 0, 0, 1, 1 };
+
+        var solution = new TransformArrayByParity();
+        var output = solution.TransformArray(input);
+
+        ListHelpers.OutcomeListMatches(expected, output);
+    }
+
+    [Fact]
+    public void Ex2()
+    {
+        var input = new[] { 1, 5, 1, 4, 2 };
+        var expected = new[] { 0, 0, 1, 1, 1 };
+
+        var solution = new TransformArrayByParity();
+        var output = solution.TransformArray(input);
+
+        ListHelpers.OutcomeListMatches(expected, output);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/TransformArrayByParity.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode;
+
+// https://leetcode.com/problems/transform-array-by-parity/
+
+public class TransformArrayByParity
+{
+    public int[] TransformArray(int[] nums)
+    {
+        var result = new int[nums.Length];
+
+        var countOdd = 0;
+
+        foreach (var num in nums)
+        {
+            countOdd += num % 2;
+        }
+
+        for (var i = nums.Length - countOdd; i < nums.Length; i++)
+        {
+            result[i] = 1;
+        }
+
+        return result;
+    }
+}
