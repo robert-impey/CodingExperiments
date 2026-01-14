2120635a71e3c90585ead6bf927cdce022a5ab98
Robert Impey <robert-impey@users.noreply.github.com>
2026-01-14 11:17:00 +0000

    C#: LT: Jewels and Stones

---
+++ dotnet/C-Sharp/LeetCode.Test/JewelsAndStonesTest.cs
@@ -0,0 +1,16 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class JewelsAndStonesTest
+{
+    [Theory]
+    [InlineData("aA", "aAAbbbb", 3)]
+    [InlineData("z", "ZZ", 0)]
+    public void Examples(string jewels, string stones, int expected)
+    {
+        var sln = new JewelsAndStones();
+
+        sln.NumJewelsInStones(jewels, stones).ShouldBe(expected);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/JewelsAndStones.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode;
+
+// https://leetcode.com/problems/jewels-and-stones/description/
+
+public class JewelsAndStones
+{
+    public int NumJewelsInStones(string jewels, string stones)
+    {
+        var count = 0;
+
+        var jewelsSet = new HashSet<char>();
+
+        foreach (var j in jewels)
+        {
+            jewelsSet.Add(j);
+        }
+
+        foreach (var s in stones)
+        {
+            if (jewelsSet.Contains(s))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
