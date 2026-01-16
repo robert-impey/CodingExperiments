ba8c279af73449b02f6a52a1bdbf5749a7819798
Robert Impey <robert-impey@users.noreply.github.com>
2026-01-16 09:44:59 +0000

    C#: Lt: Prefix reversing

---
+++ dotnet/C-Sharp/LeetCode.Test/PrefixReverserTest.cs
@@ -0,0 +1,17 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class PrefixReverserTest
+{
+    [Theory]
+    [InlineData("abcd", 2, "bacd")]
+    [InlineData("xyz", 3, "zyx")]
+    [InlineData("hey", 1, "hey")]
+    public void Examples(string s, int k, string expected)
+    {
+        var sln = new PrefixReverser();
+
+        sln.ReversePrefix(s, k).ShouldBe(expected);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/PrefixReverser.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode;
+
+// https://leetcode.com/problems/reverse-string-prefix
+
+public class PrefixReverser
+{
+    public string ReversePrefix(string s, int k)
+    {
+        if (k == 1)
+        {
+            return s;
+        }
+
+        var result = new char[s.Length];
+
+        var r = 0;
+        for (var i = k - 1; i >= 0; i--)
+        {
+            result[r] = s[i];
+            r++;
+        }
+
+        for (var i = k; i < s.Length; i++)
+        {
+            result[r] = s[i];
+            r++;
+        }
+
+        // The following line fixes CS8603 by converting the char array to a string.
+        return new string(result);
+    }
+}
