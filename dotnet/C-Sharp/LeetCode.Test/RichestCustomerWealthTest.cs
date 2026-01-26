a829c8b43ec93b59a03c48de8358c6cb8a442d09
Robert Impey <robert-impey@users.noreply.github.com>
2026-01-26 15:03:08 +0000

    C#: Lt: Richest Customer Wealth

---
+++ dotnet/C-Sharp/LeetCode.Test/RichestCustomerWealthTest.cs
@@ -0,0 +1,42 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class RichestCustomerWealthTest
+{
+    [Fact]
+    public void Ex1()
+    {
+        var input = new int[][] { [1, 2, 3], [3, 2, 1] };
+        var expected = 6;
+
+        var solution = new RichestCustomerWealth();
+        var output = solution.MaximumWealth(input);
+
+        output.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void Ex2()
+    {
+        var input = new int[][] { [1, 5], [7, 3], [3, 5] };
+        var expected = 10;
+
+        var solution = new RichestCustomerWealth();
+        var output = solution.MaximumWealth(input);
+
+        output.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void Ex3()
+    {
+        var input = new int[][] { [2, 8, 7], [7, 1, 3], [1, 9, 5] };
+        var expected = 17;
+
+        var solution = new RichestCustomerWealth();
+        var output = solution.MaximumWealth(input);
+
+        output.ShouldBe(expected);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/RichestCustomerWealth.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode;
+
+// https://leetcode.com/problems/richest-customer-wealth/
+
+public class RichestCustomerWealth
+{
+    public int MaximumWealth(int[][] accounts)
+    {
+        int max = 0;
+
+        for (var i = 0; i < accounts.Length; i++)
+        {
+            var sum = 0;
+
+            for (var j = 0; j < accounts[i].Length; j++)
+            {
+                sum += accounts[i][j];
+            }
+
+            if (sum > max)
+            {
+                max = sum;
+            }
+        }
+
+        return max;
+    }
+}
