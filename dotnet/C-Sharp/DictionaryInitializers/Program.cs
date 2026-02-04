7d315fa9dd9a0641525345b1a2da54f0ec787ae1
Robert Impey <robert-impey@users.noreply.github.com>
2026-02-04 16:25:00 +0000

    C#: Mv Dictionary Initializers to Core project

---
+++ dotnet/C-Sharp/DictionaryInitializers/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+namespace DictionaryInitializers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var dictFromCs3 = new Dictionary<string, string>
+        {
+            { "duck", "quack" },
+            { "cow", "moo" }
+        };
+
+        WriteLine($"Cows say '{dictFromCs3["cow"]}'");
+
+        var dictFromCs6 = new Dictionary<string, string>
+        {
+            ["English"] = "Hello!",
+            ["French"] = "Bonjour!"
+        };
+
+        WriteLine($"In France, say '{dictFromCs6["French"]}'");
+    }
+}
