170afd5d1ba07b1ac5df18e32817cd8bd52bc46d
Robert Impey <robert-impey@users.noreply.github.com>
2026-02-23 10:30:44 +0000

    Lt: C#: Left and Right Sum diff
    Tricky to avoid off by one and arithmetic errors

---
+++ dotnet/C-Sharp/LeetCode.Test/LeftRightSumDiffTest.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Test;
+
+public class LeftRightSumDiffTest
+{
+    [Fact]
+    public void Ex1()
+    {
+        var input = new[] { 10, 4, 8, 3 };
+        var expected = new[] { 15, 1, 11, 22 };
+
+        var solution = new LeftRightSumDiff();
+        var output = solution.LeftRightDifference(input);
+
+        ListHelpers.OutputListContent(output, expected);
+    }
+
+    [Fact]
+    public void Ex2()
+    {
+        var input = new[] { 1 };
+        var expected = new[] { 0 };
+
+        var solution = new LeftRightSumDiff();
+        var output = solution.LeftRightDifference(input);
+
+        ListHelpers.OutputListContent(output, expected);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/LeftRightSumDiff.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode;
+
+// https://leetcode.com/problems/left-and-right-sum-differences/
+
+public class LeftRightSumDiff
+{
+    public int[] LeftRightDifference(int[] nums)
+    {
+        var leftSum = new int[nums.Length];
+        var rightSum = new int[nums.Length];
+        var answer = new int[nums.Length];
+
+        var sum = 0;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            sum += nums[i - 1];
+
+            leftSum[i] = sum;
+        }
+
+        var total = sum + nums[nums.Length - 1];
+        sum = total;
+
+        for (var i = rightSum.Length - 2; i >= 0; i--)
+        {
+            sum -= nums[i + 1];
+
+            rightSum[i] = total - sum;
+        }
+
+        for (var i = 0; i < answer.Length; i++)
+        {
+            var diff = leftSum[i] - rightSum[i];
+
+            if (diff >= 0)
+            {
+                answer[i] = diff;
+            }
+            else
+            {
+                answer[i] = -1 * diff;
+            }
+        }
+
+        return answer;
+    }
+}
