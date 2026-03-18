01f8b312d608b3b1fa8dfcca3ddb7a248df5eae9
Robert Impey <robert-impey@users.noreply.github.com>
2026-03-18 09:53:30 +0000

    Py: Use unittest lib for palindrome tests

--- Python/is_palindrome.py
+++ Python/is_palindrome.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 
+import unittest
+
 
 def is_palindrome(string: str) -> bool:
-    for i in range(int(len(string) / 2)):
-        if string[i] != string[len(string) - (1 + i)]:
-            return False
-
-    return True
+    return string == string[::-1]
 
 
-def test_palindromes():
-    tests = [
-        ("", True),
-        ("a", True),
-        ("aa", True),
-        ("aba", True),
-        ("abba", True),
-        ("ab", False),
-        ("abab", False),
-    ]
+class TestPalindrome(unittest.TestCase):
+    def test_palindromes(self):
+        tests = [
+            ("", True),
+            ("a", True),
+            ("aa", True),
+            ("aba", True),
+            ("abba", True),
+            ("ab", False),
+            ("abab", False),
+        ]
 
-    for test_input, expectation in tests:
-        assert is_palindrome(test_input) == expectation
+        for test_input, expectation in tests:
+            with self.subTest(test_input=test_input):
+                self.assertEqual(is_palindrome(test_input), expectation)
+
+
+if __name__ == "__main__":
+    unittest.main()
