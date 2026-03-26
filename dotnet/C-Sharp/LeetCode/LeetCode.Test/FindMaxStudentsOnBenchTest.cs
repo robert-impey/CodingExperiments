e24060ed99b1608dc12cb9b869de2e7865363883
Robert Impey <robert-impey@users.noreply.github.com>
2026-03-26 11:21:03 +0000

    C#: LC: Find max students sitting on a bench

---
+++ dotnet/C-Sharp/LeetCode/LeetCode.Test/FindMaxStudentsOnBenchTest.cs
@@ -0,0 +1,77 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class FindMaxStudentsOnBenchTest
+{
+    [Fact]
+    public void Ex1()
+    {
+        // Input: students = [[1,2],[2,2],[3,3],[1,3],[2,3]]
+        var students = new[]
+        {
+            new[] {1, 2 },
+            new[] {2, 2 },
+            new[] {3, 3 },
+            new[] {1, 3 },
+            new[] {2, 3 },
+            new[] { 2, 3 }
+        };
+
+        var solution = new FindMaxStudentsOnBench();
+        var result = solution.MaxStudentsOnBench(students);
+
+        // Output: 3
+        result.ShouldBe(3);
+    }
+
+    [Fact]
+    public void Ex2()
+    {
+        // Input: students = [[1, 1], [2, 1], [3, 1], [4, 2], [5, 2]]
+        var students = new[]
+        {
+            new[] {1, 2 },
+            new[] {2, 1 },
+            new[] {3, 1 },
+            new[] {4, 2 },
+            new[] {5, 2 }
+        };
+
+        var solution = new FindMaxStudentsOnBench();
+        var result = solution.MaxStudentsOnBench(students);
+
+        // Output: 3
+        result.ShouldBe(3);
+    }
+
+    [Fact]
+    public void Ex3()
+    {
+        // Input: students = [[1,1],[1,1]]
+        var students = new[]
+                {
+            new[] {1, 1 },
+            new[] {1, 1 }
+        };
+
+        var solution = new FindMaxStudentsOnBench();
+        var result = solution.MaxStudentsOnBench(students);
+
+        // Output: 1
+        result.ShouldBe(1);
+    }
+
+    [Fact]
+    public void Ex4()
+    {
+        // Input: students = []
+        var students = Array.Empty<int[]>();
+
+        var solution = new FindMaxStudentsOnBench();
+        var result = solution.MaxStudentsOnBench(students);
+
+        // Output: 0
+        result.ShouldBe(0);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/LeetCode/FindMaxStudentsOnBench.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode;
+
+public class FindMaxStudentsOnBench
+{
+    public int MaxStudentsOnBench(int[][] students)
+    {
+        var maxStudentsOnBench = 0;
+
+        var benches = new Dictionary<int, HashSet<int>>();
+
+        for (var i = 0; i < students.Length; i++)
+        {
+            if (2 == students[i].Length)
+            {
+                var studentId = students[i][0];
+                var benchId = students[i][1];
+
+                if (!benches.ContainsKey(benchId))
+                {
+                    benches[benchId] = new HashSet<int>();
+                }
+
+                benches[benchId].Add(studentId);
+            }
+        }
+
+        foreach (var bench in benches.Values)
+        {
+            var benchCount = bench.Count();
+            if (benchCount > maxStudentsOnBench)
+            {
+                maxStudentsOnBench = benchCount;
+            }
+        }
+
+        return maxStudentsOnBench;
+    }
+}
