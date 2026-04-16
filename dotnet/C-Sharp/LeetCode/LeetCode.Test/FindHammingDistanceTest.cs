04328de1e2f5f5c1344cc7f1e8530f3e0dbb93ec
Robert Impey <robert-impey@users.noreply.github.com>
2026-04-16 16:00:37 +0100

    C#: LT: Hamming Distance of Ints

---
+++ dotnet/C-Sharp/LeetCode/LeetCode.Test/FindHammingDistanceTest.cs
@@ -0,0 +1,17 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class FindHammingDistanceTest
+{
+    [Theory]
+    [InlineData(1, 4, 2)]
+    [InlineData(3, 1, 1)]
+    [InlineData(10, 7, 3)]
+    [InlineData(3, 4, 3)]
+    public void Ex1(int x, int y, int expected)
+    {
+        var sln = new FindHammingDistance();
+        sln.HammingDistance(x, y).ShouldBe(expected);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/LeetCode/FindHammingDistance.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode;
+
+public class FindHammingDistance
+{
+    public int HammingDistance(int x, int y)
+    {
+        var distance = 0;
+
+        var numBits = 32;
+        var checkBit = 0x80000000;
+
+        for (int i = 0; i < numBits; i++)
+        {
+            if ((checkBit & x) != (checkBit & y))
+            {
+                distance++;
+            }
+
+            x = x << 1;
+            y = y << 1;
+        }
+
+        return distance;
+    }
+}
