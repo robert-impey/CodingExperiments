65284d614904f84457cd8129a7745ba93fa5e790
Robert Impey <robert-impey@users.noreply.github.com>
2026-04-28 11:40:37 +0100

    C#: LT - Count employees hitting the target

---
+++ dotnet/C-Sharp/LeetCode/LeetCode.Test/EmployeesWhoMetTargetTest.cs
@@ -0,0 +1,28 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class EmployeesWhoMetTargetTest
+{
+    [Fact]
+    public void Ex1()
+    {
+        var hours = new[] { 0, 1, 2, 3, 4 };
+
+        var solution = new EmployeesWhoMetTarget();
+        var result = solution.NumberOfEmployeesWhoMetTarget(hours, 2);
+
+        result.ShouldBe(3);
+    }
+
+    [Fact]
+    public void Ex2()
+    {
+        var hours = new[] { 5, 1, 4, 2, 2 };
+
+        var solution = new EmployeesWhoMetTarget();
+        var result = solution.NumberOfEmployeesWhoMetTarget(hours, 6);
+
+        result.ShouldBe(0);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/LeetCode/EmployeesWhoMetTarget.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode;
+
+public class EmployeesWhoMetTarget
+{
+    public int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+    {
+        var count = 0;
+
+        foreach (var hour in hours)
+        {
+            if (hour >= target)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
