23c184b675580ee316bf36ecf58674b49218644f
Robert Impey <robert-impey@users.noreply.github.com>
2026-04-28 17:12:55 +0100

    VB: Combine HW experiments

--- dotnet/VB/HelloWorld/Program.vb
+++ dotnet/VB/HelloWorld/Program.vb
@@ -1,5 +1,12 @@
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
+
+        For counter = 0 To 9
+            For counter2 = 0 To 9
+                Console.Write(counter2)
+            Next
+            Console.WriteLine(counter)
+        Next
     End Sub
 End Module
