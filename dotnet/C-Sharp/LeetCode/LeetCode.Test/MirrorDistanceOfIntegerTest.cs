67bcb7eae86898e9228ca8b8550015694609cbe3
Robert Impey <robert-impey@users.noreply.github.com>
2026-05-07 11:32:31 +0100

    LC: C#: Mirror Distance of an Integer

---
+++ dotnet/C-Sharp/LeetCode/LeetCode.Test/MirrorDistanceOfIntegerTest.cs
@@ -0,0 +1,30 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class MirrorDistanceOfIntegerTest
+{
+    private readonly MirrorDistanceOfInteger _solution = new();
+
+    [Theory]
+    [InlineData(25, 27)]
+    [InlineData(10, 9)]
+    [InlineData(7, 0)]
+    public void Ex(int n, int expected)
+    {
+        _solution.MirrorDistance(n).ShouldBe(expected);
+    }
+
+    [Theory]
+    [InlineData(7, 7)]
+    [InlineData(10, 1)]
+    [InlineData(25, 52)]
+    [InlineData(1_002_000, 2001)]
+    [InlineData(1, 1)]
+    [InlineData(1_000_000_000, 1)]
+    [InlineData(123456789, 987654321)]
+    public void Reverse(int n, int expected)
+    {
+        MirrorDistanceOfInteger.Reverse(n).ShouldBe(expected);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/LeetCode/MirrorDistanceOfInteger.cs
@@ -0,0 +1,54 @@
+﻿// https://leetcode.com/problems/mirror-distance-of-an-integer/
+
+namespace LeetCode;
+
+public class MirrorDistanceOfInteger
+{
+    public int MirrorDistance(int n)
+    {
+        var diff = n - Reverse(n);
+
+        if (diff >= 0)
+        {
+            return diff;
+        }
+
+        return -1 * diff;
+    }
+
+    public static int Reverse(int n)
+    {
+        var reverse = 0;
+
+        var exp = 1_000_000_000;
+        var revExp = 1;
+        var inNumber = false;
+
+        while (exp >= 1)
+        {
+            var addend = 0;
+
+            if (n >= exp)
+            {
+                if (!inNumber)
+                {
+                    inNumber = true;
+                }
+
+                var div = n / exp;
+                addend = div * revExp;
+            }
+
+            if (inNumber)
+            {
+                n %= exp;
+                revExp *= 10;
+            }
+
+            reverse += addend;
+            exp /= 10;
+        }
+
+        return reverse;
+    }
+}
