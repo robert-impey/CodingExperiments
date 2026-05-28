181edf908eb78aed0830b42a0986dc5f99e73de5
Robert Impey <robert-impey@users.noreply.github.com>
2026-05-28 15:30:31 +0100

    C#: LC: Count Pairs whose Sum is less than Target

---
+++ dotnet/C-Sharp/LeetCode/LeetCode.Test/CountPairsSumLessThanTargetTest.cs
@@ -0,0 +1,28 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class CountPairsSumLessThanTargetTest
+{
+    [Fact]
+    public void Ex1()
+    {
+        var nums = new[] { -1, 1, 2, 3, 1 };
+
+        var solution = new CountPairsSumLessThanTarget();
+        var result = solution.CountPairs(nums, 2);
+
+        result.ShouldBe(3);
+    }
+
+    [Fact]
+    public void Ex2()
+    {
+        var nums = new[] { -6, 2, 5, -2, -7, -1, 3 };
+
+        var solution = new CountPairsSumLessThanTarget();
+        var result = solution.CountPairs(nums, -2);
+
+        result.ShouldBe(10);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/LeetCode/CountPairsSumLessThanTarget.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode;
+
+public class CountPairsSumLessThanTarget
+{
+    public int CountPairs(IList<int> nums, int target)
+    {
+        var count = 0;
+        var n = nums.Count();
+
+        for (var i = 0; i < n - 1; i++)
+        {
+            for (var j = i + 1; j < n; j++)
+            {
+                if (nums[i] + nums[j] < target)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
