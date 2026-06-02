46192319971e96170b5ba3b74fa0e8a1e37a384e
Robert Impey <robert-impey@users.noreply.github.com>
2026-06-02 12:21:07 +0100

    C#: LC: Concat Array with Reverse

---
+++ dotnet/C-Sharp/LeetCode/LeetCode.Test/ConcatArrayWithReverseTest.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Test;
+
+public class ConcatArrayWithReverseTest
+{
+    [Fact]
+    public void Ex1()
+    {
+        var nums = new[] { 1, 2, 3 };
+        var expected = new[] { 1, 2, 3, 3, 2, 1 };
+
+        var solution = new ConcatArrayWithReverse();
+        var output = solution.ConcatWithReverse(nums);
+
+        ListHelpers.MatchesInOrder(output, expected);
+    }
+
+    [Fact]
+    public void Ex2()
+    {
+        var nums = new[] { 1 };
+        var expected = new[] { 1, 1 };
+
+        var solution = new ConcatArrayWithReverse();
+        var output = solution.ConcatWithReverse(nums);
+
+        ListHelpers.MatchesInOrder(output, expected);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/LeetCode/ConcatArrayWithReverse.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode;
+
+// https://leetcode.com/problems/concatenate-array-with-reverse/
+
+public class ConcatArrayWithReverse
+{
+    public int[] ConcatWithReverse(int[] nums)
+    {
+        var concat = new int[nums.Length * 2];
+        var concatLast = concat.Length - 1;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            concat[i] = nums[i];
+            concat[concatLast - i] = nums[i];
+        }
+
+        return concat;
+    }
+}
