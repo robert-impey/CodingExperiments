11903764d9850f93e21ae6e66b8ff97a43f04ba6
Robert Impey <robert-impey@users.noreply.github.com>
2026-06-15 16:06:42 +0100

    Drt: Runner for arithmetic experiment

---
+++ dart/bin/adder.dart
@@ -0,0 +1,5 @@
+import 'package:dart_experiments/arithmetic.dart';
+
+void main() {
+  print('2 + 3 = ${add(2, 3)}');
+}
--- dart/test/arithmetic_test.dart
+++ dart/test/arithmetic_test.dart
@@ -1,5 +1,4 @@
 import 'package:test/test.dart';
-// Replace 'dart' with the name defined in your pubspec.yaml 'name' field
 import 'package:dart_experiments/arithmetic.dart';
 
 void main() {
