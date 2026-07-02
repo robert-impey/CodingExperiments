4903dfa6246715ce6746e8768576aec33034969e
Robert Impey <robert-impey@users.noreply.github.com>
2026-07-02 14:40:53 +0100

    C#: LC: Translated Go intToDigits

---
+++ dotnet/C-Sharp/LeetCode/LeetCode.Test/IntegerExtensionsTest.cs
@@ -0,0 +1,41 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class IntegerExtensionsTest
+{
+    [Fact]
+    public void Zero()
+    {
+        var digits = 0.ToDigits(1);
+
+        digits.ShouldNotBeNull();
+        digits.Length.ShouldBe(1);
+        digits[0].ShouldBe(0);
+    }
+
+    [Fact]
+    public void Ascending()
+    {
+        var digits = 1234.ToDigits(4);
+
+        digits.ShouldNotBeNull();
+        digits.Length.ShouldBe(4);
+        digits[0].ShouldBe(1);
+        digits[1].ShouldBe(2);
+        digits[2].ShouldBe(3);
+        digits[3].ShouldBe(4);
+    }
+
+    [Fact]
+    public void AllSame()
+    {
+        var digits = 555.ToDigits(3);
+
+        digits.ShouldNotBeNull();
+        digits.Length.ShouldBe(3);
+        digits[0].ShouldBe(5);
+        digits[1].ShouldBe(5);
+        digits[2].ShouldBe(5);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/LeetCode/IntegerExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode;
+
+public static class IntegerExtensions
+{
+    public static int[] ToDigits(this int num, int numberOfDigits)
+    {
+        var digits = new int[numberOfDigits];
+
+        int finalIndex = numberOfDigits - 1;
+
+        for (int i = 0; i < numberOfDigits; i++)
+        {
+            int rem = num % 10;
+            digits[finalIndex - i] = rem;
+
+            if (num < 10)
+            {
+                break;
+            }
+
+            num /= 10;
+        }
+
+        return digits;
+    }
+}
