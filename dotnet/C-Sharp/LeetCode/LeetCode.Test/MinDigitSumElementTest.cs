d6d0bfa7ecdf4746621cecea997587045aa94a22
Robert Impey <robert-impey@users.noreply.github.com>
2026-07-03 16:51:57 +0100

    C#: LC: Min digit sum

---
+++ dotnet/C-Sharp/LeetCode/LeetCode.Test/MinDigitSumElementTest.cs
@@ -0,0 +1,42 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class MinDigitSumElementTest
+{
+    [Fact]
+    public void Ex1()
+    {
+        var input = new[] { 10, 12, 13, 14 };
+        var expected = 1;
+
+        var solution = new MinDigitSumElement();
+        var output = solution.MinElement(input);
+
+        output.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void Ex2()
+    {
+        var input = new[] { 1, 2, 3, 4 };
+        var expected = 1;
+
+        var solution = new MinDigitSumElement();
+        var output = solution.MinElement(input);
+
+        output.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void Ex3()
+    {
+        var input = new[] { 999, 19, 199 };
+        var expected = 10;
+
+        var solution = new MinDigitSumElement();
+        var output = solution.MinElement(input);
+
+        output.ShouldBe(expected);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/LeetCode/MinDigitSumElement.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode;
+
+public class MinDigitSumElement
+{
+    public int MinElement(int[] nums)
+    {
+        var min = 46;
+
+        foreach (var num in nums)
+        {
+            var digits = num.ToDigits(5);
+
+            var sum = digits.Sum();
+
+            if (sum < min)
+            {
+                min = sum;
+            }
+        }
+        
+        return min;
+    }
+}
