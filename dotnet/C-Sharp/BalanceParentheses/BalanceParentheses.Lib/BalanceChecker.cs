e549e2c6b18228f6f7d3f8e53d8df420f0704c90
Robert Impey <robert-impey@users.noreply.github.com>
2026-07-18 18:04:45 +0100

    C#: Modernize Balance Parens

---
+++ dotnet/C-Sharp/BalanceParentheses/BalanceParentheses.Lib/BalanceChecker.cs
@@ -0,0 +1,23 @@
+﻿namespace BalanceParentheses.Lib;
+
+public class BalanceChecker
+{
+    public static bool IsStringBalanced(string beingChecked)
+    {
+        var unbalancedParenthesesCount = 0;
+
+        foreach (var c in beingChecked)
+        {
+            if (c == '(')
+                unbalancedParenthesesCount++;
+
+            if (c == ')')
+                unbalancedParenthesesCount--;
+
+            if (unbalancedParenthesesCount < 0)
+                return false;
+        }
+
+        return unbalancedParenthesesCount == 0;
+    }
+}
---
+++ dotnet/C-Sharp/BalanceParentheses/BalanceParentheses.Test/BalanceCheckerTest.cs
@@ -0,0 +1,22 @@
+﻿using BalanceParentheses.Lib;
+using Shouldly;
+
+namespace BalanceParentheses.Test;
+
+public class BalanceCheckerTest
+{
+    [Theory]
+    [InlineData("", true)]
+    [InlineData("()", true)]
+    [InlineData("(", false)]
+    [InlineData(")", false)]
+    [InlineData("(())", true)]
+    [InlineData("(()())", true)]
+    [InlineData("(foo)(bar)", true)]
+    [InlineData("(()", false)]
+    [InlineData(")(", false)]
+    public void IsStringBalanced(string input, bool expected)
+    {
+        BalanceChecker.IsStringBalanced(input).ShouldBe(expected);
+    }
+}
