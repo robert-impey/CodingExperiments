f0b94f68c386ddb41b46f0bce0aba7f5d337bc12
Robert Impey <robert-impey@users.noreply.github.com>
2026-08-19 09:27:25 -0700

    VB: Bool conv -> dotnet

--- dotnet/VB/BooleanConversions/Program.vb
+++ dotnet/VB/BooleanConversions/Program.vb
@@ -1,18 +1,16 @@
-﻿Module Module1
-
+Module Program
     Sub Main()
         Dim myBln As Boolean = True
         Dim myInt As Integer = 10
 
-        'Dim myVar = myBln * myInt 'If option strict is 'Off'.
-        Dim myVar = CInt(myBln) * myInt 'If option strict is 'On'.
+        Dim myVar = myBln * myInt 'If option strict is 'Off'.
+        'Dim myVar = CInt(myBln) * myInt 'If option strict is 'On'.
 
         Console.WriteLine(myVar)
 
         myBln = False
-        myVar = CInt(myBln) * myInt
+        myVar = myBln * myInt
 
         Console.WriteLine(myVar)
     End Sub
-
 End Module
