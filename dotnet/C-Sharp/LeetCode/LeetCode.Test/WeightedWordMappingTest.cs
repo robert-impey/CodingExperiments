8ff01e0c418ddab947a6485d7ec01bdf79d76334
Robert Impey <robert-impey@users.noreply.github.com>
2026-08-27 20:58:45 -0700

    C#: LC: Tests for Weighted Word Mapping

---
+++ dotnet/C-Sharp/LeetCode/LeetCode.Test/WeightedWordMappingTest.cs
@@ -0,0 +1,39 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class WeightedWordMappingTest
+{
+    [Fact]
+    public void Ex1()
+    {
+        var words = new[] { "abcd", "def", "xyz" };
+        var weights = new[] { 5, 3, 12, 14, 1, 2, 3, 2, 10, 6, 6, 9, 7, 8, 7, 10, 8, 9, 6, 9, 9, 8, 3, 7, 7, 2 };
+
+        var sln = new WeightedWordMapping();
+        var output = sln.MapWordWeights(words, weights);
+        output.ShouldBe("rij");
+    }
+
+    [Fact]
+    public void Ex2()
+    {
+        var words = new[] { "a", "b", "c" };
+        var weights = new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+        var sln = new WeightedWordMapping();
+        var output = sln.MapWordWeights(words, weights);
+        output.ShouldBe("yyy");
+    }
+
+    [Fact]
+    public void Ex3()
+    {
+        var words = new[] { "abcd" };
+        var weights = new[] { 7, 5, 3, 4, 3, 5, 4, 9, 4, 2, 2, 7, 10, 2, 5, 10, 6, 1, 2, 2, 4, 1, 3, 4, 4, 5 };
+
+        var sln = new WeightedWordMapping();
+        var output = sln.MapWordWeights(words, weights);
+        output.ShouldBe("g");
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/LeetCode/WeightedWordMapping.cs
@@ -0,0 +1,18 @@
+﻿// https://leetcode.com/problems/weighted-word-mapping/
+
+namespace LeetCode;
+
+public class WeightedWordMapping
+{
+    public string MapWordWeights(string[] words, int[] weights)
+    {
+        ArgumentNullException.ThrowIfNull(words);
+        ArgumentNullException.ThrowIfNull(weights);
+
+        var letters = new char[words.Length];
+
+
+
+        return new string(letters);
+    }
+}
