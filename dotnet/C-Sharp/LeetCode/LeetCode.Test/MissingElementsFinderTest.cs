335b7d434c33534b3707df53660b8cc0ab590a8d
Robert Impey <robert-impey@users.noreply.github.com>
2026-09-11 12:36:36 +0100

    C#: LC: Find Missing Elements

---
+++ dotnet/C-Sharp/LeetCode/LeetCode.Test/MissingElementsFinderTest.cs
@@ -0,0 +1,43 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class MissingElementsFinderTest
+{
+    [Fact]
+    public void Ex1()
+    {
+        var input = new[] { 1, 4, 2, 5 };
+        var expected = new[] { 3 };
+
+        var solution = new MissingElementsFinder();
+        var output = solution.FindMissingElements(input).ToArray();
+
+        output.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void Ex2()
+    {
+        var input = new[] { 7, 8, 6, 9 };
+
+        var expected = Array.Empty<int>();
+
+        var solution = new MissingElementsFinder();
+        var output = solution.FindMissingElements(input).ToArray();
+
+        output.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void Ex3()
+    {
+        var input = new[] { 5, 1 };
+        var expected = new[] { 2, 3, 4 };
+
+        var solution = new MissingElementsFinder();
+        var output = solution.FindMissingElements(input).ToArray();
+
+        output.ShouldBe(expected);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/LeetCode/MissingElementsFinder.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode;
+
+public class MissingElementsFinder
+{
+    public IList<int> FindMissingElements(int[] nums)
+    {
+        var missingElements = new List<int>();
+
+        Array.Sort(nums);
+
+        var min = nums[0];
+        var max = nums[nums.Length - 1];
+
+        for (var i = min + 1; i < max; i++)
+        {
+            if (!nums.Contains(i))
+            {
+                missingElements.Add(i);
+            }
+        }
+
+        return missingElements;
+    }
+}
