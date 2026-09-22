24f96a6edc0dd05c9dc7c80c166c2cd8f98ac537
Robert Impey <robert-impey@users.noreply.github.com>
2026-09-22 15:13:15 +0100

    C#: LC: GCD of Odd and Even Sums

---
+++ dotnet/C-Sharp/LeetCode/LeetCode.Test/GcdOfOddEvenSumsFinderTest.cs
@@ -0,0 +1,16 @@
+﻿using Shouldly;
+
+namespace LeetCode.Test;
+
+public class GcdOfOddEvenSumsFinderTest
+{
+    private readonly GcdOfOddEvenSumsFinder _solution = new();
+
+    [Theory]
+    [InlineData(4, 4)]
+    [InlineData(5, 5)]
+    public void Ex(int n, int expected)
+    {
+        _solution.GcdOfOddEvenSums(n).ShouldBe(expected);
+    }
+}
---
+++ dotnet/C-Sharp/LeetCode/LeetCode/GcdOfOddEvenSumsFinder.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode;
+
+// https://leetcode.com/problems/gcd-of-odd-and-even-sums
+
+public class GcdOfOddEvenSumsFinder
+{
+    public int GcdOfOddEvenSums(int n)
+    {
+        const int addend = 2;
+
+        var odd = 1;
+        var even = 2;
+        var oddSum = odd;
+        var evenSum = even;
+        for (var i = 2; i <= n; i++)
+        {
+            odd += addend;
+            even += addend;
+
+            oddSum += odd;
+            evenSum += even;
+        }
+
+        return Gcd(oddSum, evenSum);
+    }
+
+    private int Gcd(int a, int b)
+    {
+        var larger = a;
+        var smaller = b;
+
+        if (b > a)
+        {
+            larger = b;
+            smaller = a;
+        }
+
+        if (smaller == 0)
+        {
+            return larger;
+        }
+
+        return Gcd(larger - smaller, smaller);
+    }
+}
